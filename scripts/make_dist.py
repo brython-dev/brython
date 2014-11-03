f9e709cab126f8f7ce55f6dd885f9dad73d2e54b
Pierre Quentel <pierre.quentel@gmail.com>
2014-11-03 22:07:09 +0100

    Change version to 3.0.0 final

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -24,7 +24,7 @@ except ImportError:
 pdir = os.path.dirname(os.getcwd())
 # version info
 version = [3, 3, 0, "alpha", 0]
-implementation = [3, 0, 0, 'rc', 1]
+implementation = [3, 0, 0, 'final', 0]
 
 def custom_minify(src):
     _res, pos = '', 0
