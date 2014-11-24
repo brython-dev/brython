115c66ee27fd67755d11bfe203e115810afcf24e
pierrequentel <pierre.quentel@gmail.com>
2014-11-24 22:06:24 +0100

    Minor change in pystone.py

--- site/speed/pystone.py
+++ site/speed/pystone.py
@@ -250,19 +250,4 @@ def Func3(EnumParIn):
     if EnumLoc == Ident3: return TRUE
     return FALSE
 
-if __name__ == '__main__':
-    import sys
-    def error(msg):
-        print >>sys.stderr, msg,
-        print >>sys.stderr, "usage: %s [number_of_loops]" % sys.argv[0]
-        sys.exit(100)
-    nargs = len(sys.argv) - 1
-    if nargs > 1:
-        error("%d arguments are too many;" % nargs)
-    elif nargs == 1:
-        try: loops = int(sys.argv[1])
-        except ValueError:
-            error("Invalid argument %r;" % sys.argv[1])
-    else:
-        loops = LOOPS
-    main(loops)
+main(LOOPS)
