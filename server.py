1a9bf504fcc4bddcec474a652ff72cc4f23ff422
juancarlospaco <juancarlospaco@gmail.com>
2014-11-25 09:29:34 -0300

    Remove print from server.py, Add proper Logging

--- server.py
+++ server.py
@@ -5,8 +5,10 @@
 """Simple HTTP Server for Brython Development."""
 
 
-import os
+import logging as log
 import sys
+from ctypes import byref, cdll, create_string_buffer
+from os import nice
 from webbrowser import open_new_tab
 
 try:
@@ -17,16 +19,24 @@ except:
     from CGIHTTPServer import CGIHTTPRequestHandler
 
 
+log.basicConfig(level=-1)  # logs
 try:
-    os.nice(19)
-except Exception as error:
-    print(error)
+    nice(19)  # smooth cpu priority
+    libc = cdll.LoadLibrary("libc.so.6")  # set process name
+    buff = create_string_buffer(len("brython-dev-server") + 1)
+    buff.value = bytes("brython-dev-server".encode("utf-8"))
+    libc.prctl(15, byref(buff), 0, 0, 0)
+except Exception as reason:
+    log.warning(reason, exc_info=True)
 
 
+log.info(__doc__)
 server_address, handler = ('', 8000), CGIHTTPRequestHandler
-httpd = server.HTTPServer(server_address, handler)
-print(__doc__)
-print(("Server running on port http://localhost:{}.".format(server_address[1])))
-print("Press CTRL+C to Quit.")
+log.info(("Server running on http://localhost:{}.".format(server_address[1])))
+log.info("Press CTRL+C to Quit.")
 open_new_tab("http://localhost:{}/site/".format(server_address[1]))
-httpd.serve_forever()
+try:
+    httpd = server.HTTPServer(server_address, handler)
+    httpd.serve_forever()
+except Exception as reason:
+    log.error(reason, exc_info=True)
