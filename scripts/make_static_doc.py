c539ebef359daf9ddb77ad35518830fe9d9a2cd0
PierreQuentel <pierre.quentel@gmail.com>
2014-11-26 22:11:25 +0100

    Revert "Add Multiprocessing to make_static_doc.py if run standalone"
    This reverts commit d5755c3bca279dc0c3b461bd76287bd9ed515e57.

--- scripts/make_static_doc.py
+++ scripts/make_static_doc.py
@@ -6,10 +6,8 @@
 
 import os
 import re
-from multiprocessing import Pool
 
 
-list_of_languages = ('fr', 'en', 'es', 'pt')
 folder = os.path.join(os.path.dirname(os.getcwd()), 'site', 'doc')
 loader = """
 import time
@@ -44,25 +42,17 @@ def repl(mo):
     return 'href="?page=%s">' % mo.groups()[0].replace('/', '%2F')
 
 
-def build_docs(lang):
-    print("Process {} finished writing Docs on file: {}.".format(
-        os.getpid(), os.path.join(folder, lang, 'static_index.html')))
+for lang in ['fr', 'en', 'es', 'pt']:
     with open(os.path.join(folder, lang, 'index.html')) as index_html_file:
         dyn = index_html_file.read()
     with open(os.path.join(folder, lang, 'static_index.html'), 'wb') as static:
         pattern = 'href="#" onClick="load\(\'(.+)\.md\',\'content\'\)">'
         res = re.sub(pattern, repl, dyn)
         script_pattern = '<script type="text/python3?">.*?</script>'
-        res = re.sub(script_pattern, '<script type="text/python3">%s</script>'
-                     % loader, res, flags=re.S)
+        res = re.sub(script_pattern,
+                     '<script type="text/python3">%s</script>' % loader, res,
+                     flags=re.S)
         res = res.replace(
-            '<body onload="brython(1);load(\'intro.md\',\'content\')">',
-            '<body onload="brython(1)">')
+            '<body onLoad="brython(1);load(\'intro.md\',\'content\')">',
+            '<body onLoad="brython(1)">')
         static.write(res)
-        return True
-
-
-if __name__ in '__main__':
-    Pool(len(list_of_languages)).map(build_docs, list_of_languages)
-else:
-    map(build_docs, list_of_languages)  # No multiprocess, its module
