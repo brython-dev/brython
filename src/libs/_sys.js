880c3bd7b832844f536782019023a4d2a1cd0566
Billy Earney <billy.earney@gmail.com>
2014-12-31 23:05:21 -0600

    abbr brython builtin variable

--- src/libs/_sys.js
+++ src/libs/_sys.js
@@ -1,21 +1,20 @@
 var $module=(function($B){
-
+    var _b_=$B.builtins
     return {
         modules :
-            {'__get__':function(){return $B.builtins.dict($B.JSObject($B.imported))},
+            {'__get__':function(){return _b_.dict($B.JSObject($B.imported))},
              '__set__':0 // data descriptor, to force use of __get__
             },
         stderr : {
             __get__:function(){return $B.stderr},
             __set__:function(self, obj, value){$B.stderr = value},
-            write:function(data){$B.builtins.getattr($B.stderr,"write")(data)}
+            write:function(data){_b_.getattr($B.stderr,"write")(data)}
             },
         stdout : {
             __get__:function(){return $B.stdout},
             __set__:function(self, obj, value){$B.stdout = value},
-            write:function(data){$B.builtins.getattr($B.stdout,"write")(data)}
+            write:function(data){_b_.getattr($B.stdout,"write")(data)}
             },
         stdin : $B.stdin
     }
 })(__BRYTHON__)
-
