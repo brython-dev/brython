6ae75dba6c3e5321f4c5aa3afa233e5e56b21b19
Pierre Quentel <pierre.quentel@gmail.com>
2015-01-13 10:39:11 +0100

    Update version_info.js with compiled_date

--- src/version_info.js
+++ src/version_info.js
@@ -1,6 +1,7 @@
 __BRYTHON__.implementation = [3, 0, 3, 'alpha', 0]
 __BRYTHON__.__MAGIC__ = "3.0.3"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
+__BRYTHON__.compiled_date = "2015-01-13 10:31:34.553000"
 __BRYTHON__.builtin_module_names = ["posix","__random",
     "_ajax",
     "_browser",
