7cf8aedf8aedeb0bb5a67e293fae674eb1213681
James Hutchison <v-jahutc@microsoft.com>
2015-01-14 02:14:23 -0800

    update with brython-dev

--- src/libs/json.js
+++ src/libs/json.js
@@ -1,14 +1,17 @@
 var $module = (function($B){
 
-var _b_ = $B.builtins
-var $s=[]
-for(var $b in _b_) $s.push('var ' + $b +'=_b_["'+$b+'"]')
-eval($s.join(';'))
-
-//for(var $py_builtin in _b_) eval("var "+$py_builtin+"=_b_[$py_builtin]")
+eval($B.InjectBuiltins())
 
 function _py(obj){
-    if(obj===null){return None}
+    switch(obj) {
+      case null:
+        return None
+      case true:
+        return True
+      case false:
+        return False
+    }
+
     if(isinstance(obj,list)){
         var res = []
         for(var i=0, _len_i = obj.length; i < _len_i;i++){
@@ -16,14 +19,21 @@ function _py(obj){
         }
         return res
     }
+
     if(obj.__class__!==undefined){
         if(obj.__class__===list){
-            for(var i=0, _len_i = obj.length; i < _len_i;i++){
-                obj[i] = _py(obj[i])
-            }
+          for(var i=0, _len_i = obj.length; i < _len_i;i++){
+              obj[i] = _py(obj[i])
+          }
+          return obj
         }
         return obj
     }
+
+    if(obj._type_ === 'iter') { // this is an iterator
+       return _b_.iter(obj.data)
+    }
+
     if(typeof obj==='object' && obj.__class__===undefined){
         // transform JS object into a Python dict
         var res = dict()
@@ -32,14 +42,21 @@ function _py(obj){
         }
         return res
     }
+
     return $B.JSObject(obj)
 }
 function _js(obj){
     // obj is a Python object
-    if (isinstance(obj,[int,str])) return obj
-    if(obj===None) return null
-    if(obj===True) return true
-    if(obj===False) return false
+    switch(obj) {
+      case None:
+        return null
+      case True:
+        return true
+      case False:
+        return false
+    }
+
+    if(isinstance(obj,[int,str])) return obj
     if(isinstance(obj,float)) return obj.value
     if(isinstance(obj,[list,tuple])){
         var res = []
@@ -47,12 +64,33 @@ function _js(obj){
         return res
     }
     if(isinstance(obj,dict)){
-        var res = new Object()
+        var res = {}
         var items = _b_.list(_b_.dict.$dict.items(obj))
         for(var i=0, _len_i = items.length; i < _len_i;i++){
             res[_js(items[i][0])]=_js(items[i][1])
         }
         return res
+    }
+
+    if (hasattr(obj, '__iter__')) {
+       // this is an iterator..
+       var _a=[]
+       while(1) {
+          try {
+            _a.push(_js(next(obj)))
+          } catch(err) {
+            if (err.__name__ !== "StopIteration") throw err
+            break
+          }
+       }
+       return {'_type_': 'iter', data: _a}
+    }
+
+    if (hasattr(obj, '__getstate__')) {
+       return getattr(obj, '__getstate__')()
+    }
+    if (hasattr(obj, '__dict__')) {
+       return _js(getattr(obj, '__dict__'))
     }
     throw _b_.TypeError(str(obj)+' is not JSON serializable')
 }
