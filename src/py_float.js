cc3a6647cf6cd67ebc615b75418aedb9fd896b92
Billy Earney <billy.earney@gmail.com>
2015-01-14 19:38:15 -0600

    allow objects with __float__ function to be converted to floats properly

--- src/py_float.js
+++ src/py_float.js
@@ -427,6 +427,9 @@ var float = function (value){
     if(isinstance(value,_b_.bytes)) {
       return new $FloatClass(parseFloat(getattr(value,'decode')('latin-1')))
     }
+    if(hasattr(value, '__float__')) {
+      return new $FloatClass(getattr(value, '__float__')())
+    }
     if (typeof value == 'string') {
        value = value.trim()   // remove leading and trailing whitespace
        switch(value.toLowerCase()) {
