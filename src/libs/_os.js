b4818e5f3c22da17253de7e07bb25310d589bd6f
PierreQuentel <pierre.quentel@gmail.com>
2015-01-31 16:38:39 +0100

    Remove _os.js

--- src/libs/_os.js
+++
@@ -1,8 +0,0 @@
-var $module=(function($B){
-
-    var _b_ = $B.builtins
-    return {
-        random:function(){return _b_.float(Math.random())},
-        randint:function(a,b){return _b_.int(Math.floor(Math.random()*(b-a)+a))}
-    }
-})(__BRYTHON__)
