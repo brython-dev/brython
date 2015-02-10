bebf1755e012af9ee1ca902b9f2aa7347dca5efd
Billy Earney <billy.earney@gmail.com>
2015-02-09 19:33:09 -0600

    allow sync loading of py_*.js files

--- src/brython_loader.js
+++ src/brython_loader.js
@@ -1,48 +1,81 @@
 (function() {
-    var scripts = document.getElementsByTagName("script")
-    //get last script that was executed (ie, this one!!!!!) and read 
-    // data-brython-options if it exists..
-    var options = scripts[scripts.length-1].getAttribute("data-brython-options")
 
-    //figure out "root" path so we know where brython source file are located
+    
+    function import_file(path, pos) {
+     
+       // we will "import" each script individually so things can
+       // be debugged much easier by programmers/developers.
+
+       var _files=['brython_builtins', 'version_info', 'identifiers_re',
+                'py2js', 'py_object', 'py_type', 'py_utils',
+                'py_generator', 'py_builtin_functions',
+                'py_bytes', 'py_set', 'js_objects', 'stdlib_paths',
+                'py_import', 'py_string', 'py_int', 'py_float', 
+                'py_complex', 'py_dict', 'py_list', 'py_dom']
+
+       if (pos >= _files.length) return
+      
+       var _s = document.createElement('script')
+       //_s.setAttribute('type', 'text/javascript')
+       _s.setAttribute('src', path + _files[pos] + '.js')
+       _s.onload=function() {import_file(path, pos+1)}
+
+       document.head.appendChild(_s)
+    }
+
+    function parse_options(json_string) {
+       if (json_string === undefined || json_string == null || json_string == '') {
+          return {}
+       }
+          
+       try {
+         return JSON.parse(json_string)
+       } catch(e) {
+         console.log(e)   // display an error to show that options cannot be parsed.
+       }
+
+       return {}
+    }
+
+    var scripts = document.getElementsByTagName("script")
+    //get last script that was executed (ie, this one!!!!!)
+    var script = scripts[scripts.length-1]
+
+    // read data-brython-options (or data-loader-options) if it exists..
+    var _loader=parse_options(script.getAttribute("data-loader-options"))
+    var _brython=parse_options(script.getAttribute("data-brython-options"))
+
+    //figure out "root" path so we know where brython source file(s) are located
     var src = scripts[scripts.length-1].getAttribute("src")
     var _path =src.split('/')
     _path.pop()
     _path = _path.join('/') + '/'
 
-    var _obj
 
-    if (options == null || options == '') {
-       _obj={debug:0}
-    } else {
-       try {
-         _obj=JSON.parse(options)
-       } catch(e) {
-         console.log(e)   // display an error to show that options cannot be parsed.
-       }
-    }
-
-    if (_obj.debug == 0 || _obj.debug==1) {
+    if (_loader.dist == true) {
        var _s = document.createElement('script');
        _s.src = _path + "brython_dist.js";
-       _s.onload = function() { brython(_obj) };
+       _s.onload= function() { brython(_brython) };
        document.head.appendChild(_s);
-    } else {  // we will "import" each script individually so things can
-              // be debugged much easier by programmers/developers.
+       return
+    }
 
-       var _files=['brython_builtins', 'version_info', 'identifiers_re',
-                   'py2js', 'py_object', 'py_type', 'py_utils',
-                   'py_generator', 'py_builtin_functions',
-                   'py_bytes', 'py_set', 'js_objects', 'stdlib_paths',
-                   'py_import', 'py_string', 'py_int', 'py_float', 
-                   'py_complex', 'py_dict', 'py_list', 'py_dom']
+    if (_loader.debug === undefined || _loader.debug==0) {  // just load brython.js
+       var _s = document.createElement('script');
+       _s.src = _path + "brython.js";
+       document.head.appendChild(_s);
+    } else {
+       import_file(_path, 0)
+    }
 
-       for (var i=0; i < _files.length; i++) {
-           var _s = document.createElement('script');
-           _s.src = _path + _files[i] + '.js'
-           document.head.appendChild(_s);
-       }
+    // look at other loader options to see if we can/should do something else.
 
-       document.body.onload = function() { brython(_obj) };
-    } // end of if-else
+    if (_loader.VFS && _loader.VFS == true) {
+       var _s = document.createElement('script');
+       _s.src = _path + 'py_VFS.js'
+       document.head.appendChild(_s);
+    }
+
+    window.onload= function() { brython(_brython)}
+
 })();
