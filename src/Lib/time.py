6ed990f238200f83c131593028ba7f0906f264fc
kikocorreoso <kikocorreoso@gmail.com>
2015-02-12 00:46:54 +0100

    added full CPython 3.4 functionality to time.py module

--- src/Lib/time.py
+++ src/Lib/time.py
@@ -4,9 +4,7 @@ import javascript
 # use Javascript Date constructor
 date = javascript.JSConstructor(window.Date)
 
-tzname = tuple(['', ''])
-
-daylight = 0 # fix me.. returns Non zero if DST timezone is defined
+#daylight = 0 # fix me.. returns Non zero if DST timezone is defined
 
 def _get_day_of_year(arg):
     ml = [31,28,31,30,31,30,31,31,30,31,30,31]
@@ -20,6 +18,38 @@ def _get_day_of_year(arg):
     yday += arg[2]
     arg[7] = yday
     return struct_time(arg)
+    
+def _is_dst(secs = None):
+    d = date()
+    if secs is not None:
+        d = date(secs*1000)
+    # calculate if we are in daylight savings time or not.
+    # borrowed from http://stackoverflow.com/questions/11887934/check-if-daylight-saving-time-is-in-effect-and-if-it-is-for-how-many-hours
+    jan = date(d.getFullYear(), 0, 1)
+    jul = date(d.getFullYear(), 6, 1)
+    dst = int(d.getTimezoneOffset() < max(abs(jan.getTimezoneOffset()), abs(jul.getTimezoneOffset())))
+    return dst
+    
+daylight = _is_dst()
+timezone = date().getTimezoneOffset() * 60
+
+def _get_tzname():
+	d = date()
+	d = d.toTimeString()
+	d = d.split('(')[1].split(')')[0]
+	return (d, 'NotAvailable')
+
+tzname = _get_tzname()
+
+def _set_altzone():
+    d = date()
+    jan = date(d.getFullYear(), 0, 1)
+    jul = date(d.getFullYear(), 6, 1)
+    result = timezone - (jan.getTimezoneOffset() - jul.getTimezoneOffset()) * 60
+    return result
+    
+altzone = _set_altzone() if daylight else timezone
+
 
 def asctime(t = None):
     if t and isinstance(t, struct_time) and len(t.args) == 9:
@@ -42,12 +72,30 @@ def asctime(t = None):
         weekdays[t[6]], months[t[1]], t[2], t[3], t[4], t[5], t[0])
     return result
 
+# All the clock_xx machinery shouldn't work in the browser so some
+# NotImplementedErrors or messages are shown
+_clock_msg = """Browser cannot access CPU. See '%s'"""
+def _clock_xx(url):
+    raise NotImplementedError(_clock_msg % url)
+clock = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.clock")
+clock_getres = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.clock_getres")
+clock_gettime = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.clock_gettime")
+clock_settime = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.clock_settime")
+CLOCK_HIGHRES = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_HIGHRES"
+CLOCK_MONOTONIC = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_MONOTONIC"
+CLOCK_MONOTONIC_RAW = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_MONOTONIC_RAW"
+CLOCK_PROCESS_CPUTIME_ID = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_PROCESS_CPUTIME_ID"
+CLOCK_REALTIME = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_REALTIME"
+CLOCK_THREAD_CPUTIME_ID = _clock_msg % "https://docs.python.org/3/library/time.html#time.CLOCK_THREAD_CPUTIME_ID"
+
 def ctime(timestamp=None):
     if timestamp is None:
         timestamp = int(date().getTime()/1000)
     d = date(0)
     d.setUTCSeconds(timestamp)
     return d.toUTCString()
+
+get_clock_info = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.get_clock_info")
 
 def gmtime(secs = None):
     d = date()
@@ -60,19 +108,11 @@ def gmtime(secs = None):
         wday, 0, 0])
     return _get_day_of_year(tmp.args)
 
-def perf_counter():
-    return float(date().getTime()/1000.0)
-
-def localtime(secs=None):
+def localtime(secs = None):
    d = date()
    if secs is not None:
        d = date(secs*1000)
-
-   # calculate if we are in daylight savings time or not.
-   # borrowed from http://stackoverflow.com/questions/11887934/check-if-daylight-saving-time-is-in-effect-and-if-it-is-for-how-many-hours
-   jan = date(d.getFullYear(), 0, 1)
-   jul = date(d.getFullYear(), 6, 1)
-   dst = int(d.getTimezoneOffset() < max(jan.getTimezoneOffset(), jul.getTimezoneOffset()))
+   dst = _is_dst(secs)
    wday = d.getDay() - 1 if d.getDay() - 1 >= 0 else 6
    tmp = struct_time([d.getFullYear(), 
        d.getMonth()+1, d.getDate(),
@@ -82,11 +122,24 @@ def localtime(secs=None):
    #return struct_time([d.getFullYear(), d.getMonth()+1, d.getDate(), d.getHours(),
    #             d.getMinutes(), d.getSeconds(), d.getDay(), 0, dst])
 
+def mktime():
+	raise NotImplementedError('TODO')
+
+def monotonic():
+	return javascript.JSObject(window.performance.now)()/1000.
+
+def perf_counter():
+    return float(date().getTime()/1000.0)
+
+process_time = lambda: _clock_xx("https://docs.python.org/3/library/time.html#time.process_time")
+
 def time():
     return float(date().getTime()/1000)
 
 def sleep(secs):
-    pass
+	raise NotImplementedError(
+    "Javascript is single-thread event-based model."
+    "Check browser.timer.set_timeout.")
 
 def strftime(_format,arg=None):
     def ns(arg,nb):
@@ -204,3 +257,6 @@ def to_struct_time(ptuple):
 def strptime(string, _format):
     import _strptime
     return struct_time([_strptime._strptime_datetime(to_struct_time, string, _format)])
+
+def tzset():
+	raise NotImplementedError()
