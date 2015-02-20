66fa4882170fcc9196cec67ad6c6392d44bc24b3
PierreQuentel <pierre.quentel@gmail.com>
2015-02-20 15:32:31 +0100

    Update module "dis"

--- src/libs/dis.js
+++ src/libs/dis.js
@@ -2,6 +2,7 @@ var $module=(function($B){
 
 var mod = {
     dis:function(src){
+        $B.$py_module_path['__main__'] = $B.brython_path
         return __BRYTHON__.py2js(src,'__main__','__main__','__builtins__').to_js()
     }
 }
