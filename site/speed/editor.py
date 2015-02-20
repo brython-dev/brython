6ba1e50cf293f4c0661f5648f1439194dfc81026
PierreQuentel <pierre.quentel@gmail.com>
2015-02-20 15:40:00 +0100

    Adapt speed tests editor to new namespace management

--- site/speed/editor.py
+++ site/speed/editor.py
@@ -3,24 +3,32 @@ import time
 import traceback
 import dis
 
-from browser import document as doc
+from browser import document as doc, window, alert
 from javascript import JSObject
 
 # set height of container to 66% of screen
 _height = doc.documentElement.clientHeight
 _s = doc['container']
-_s.style.height = '%spx' % int(_height*0.66)
+_s.style.height = '%spx' % int(_height * 0.66)
 
 has_ace = True
 try:
-    editor=JSObject(ace).edit("editor")
-    editor.getSession().setMode("ace/mode/python")
+    editor = window.ace.edit("editor")
+    session = editor.getSession()
+    session.setMode("ace/mode/python")
+
+    editor.setOptions({
+     'enableLiveAutocompletion': True,
+     'enableSnippets': True,
+     'highlightActiveLine': False,
+     'highlightSelectedWord': True
+    })
 except:
     from browser import html
-    editor = html.TEXTAREA(rows=20,cols=70)
+    editor = html.TEXTAREA(rows=20, cols=70)
     doc["editor"] <= editor
     def get_value(): return editor.value
-    def set_value(x):editor.value=x
+    def set_value(x):editor.value = x
     editor.getValue = get_value
     editor.setValue = set_value
     has_ace = False
@@ -28,39 +36,41 @@ except:
 if sys.has_local_storage:
     from browser.local_storage import storage
 else:
-    storage = False
+    storage = None
 
-__BRYTHON__.debug = int(doc['set_debug'].checked)
+if 'set_debug' in doc:
+    __BRYTHON__.debug = int(doc['set_debug'].checked)
 
 def reset_src():
-    if storage and "py_src" in storage:
-       editor.setValue(storage["py_src"])
+    if storage is not None and "py_src" in storage:
+        editor.setValue(storage["py_src"])
     else:
-       editor.setValue('for i in range(10):\n\tprint(i)')
-
+        editor.setValue('for i in range(10):\n\tprint(i)')
     editor.scrollToRow(0)
     editor.gotoLine(0)
 
 def reset_src_area():
     if storage and "py_src" in storage:
-       editor.value = storage["py_src"]
+        editor.value = storage["py_src"]
     else:
-       editor.value = 'for i in range(10):\n\tprint(i)'
+        editor.value = 'for i in range(10):\n\tprint(i)'
 
-def write(data):
-    doc["console"].value += '%s' % data
+class cOutput:
 
-#sys.stdout = object()    #not needed when importing sys via src/Lib/sys.py
-sys.stdout.write = write
+    def write(self, data):
+        doc["console"].value += str(data)
 
-#sys.stderr = object()    # ditto
-sys.stderr.write = write
+    def flush(self):
+        pass
+
+sys.stdout = cOutput()
+sys.stderr = cOutput()
 
 def to_str(xx):
     return str(xx)
 
 info = sys.implementation.version
-doc['version'].text = '%s.%s.%s' %(info.major,info.minor,info.micro)
+doc['version'].text = '%s.%s.%s' % (info.major, info.minor, info.micro)
 
 output = ''
 
@@ -68,57 +78,40 @@ def show_console(ev):
     doc["console"].value = output
     doc["console"].cols = 60
 
-def clear_text(ev):
-    editor.setValue('')
-    if sys.has_local_storage:
-        storage["py_src"]=''
+# load a Python script
+def load_script(evt):
+    _name = evt.target.value + '?foo=%s' % time.time()
+    editor.setValue(open(_name).read())
 
-    doc["console"].value=''
-
-def run(*args):
+# run a script, in global namespace if in_globals is True
+def run(in_globals=False):
     global output
-    doc["console"].value=''
+    doc["console"].value = ''
     src = editor.getValue()
-    if storage:
-       storage["py_src"]=src
+    if storage is not None:
+       storage["py_src"] = src
 
     t0 = time.perf_counter()
     try:
-        exec(src,globals())
+        if(in_globals):
+            exec(src)
+        else:
+            ns = {}
+            exec(src, ns)
         state = 1
     except Exception as exc:
-        traceback.print_exc()
+        traceback.print_exc(file=sys.stderr)
         state = 0
     output = doc["console"].value
 
-    print('<completed in %6.2f ms>' % ((time.perf_counter()-t0)*1000.0))
+    print('<completed in %6.2f ms>' % ((time.perf_counter() - t0) * 1000.0))
     return state
-
-# load a Python script
-def load(evt):
-    _name=evt.target.value+'?foo=%s' %time.time()
-    editor.setValue(open(_name).read())
 
 def show_js(ev):
     src = editor.getValue()
     doc["console"].value = dis.dis(src)
 
-def change_theme(evt):
-    _theme=evt.target.value
-    editor.setTheme(_theme)
-
-    if storage:
-       storage["ace_theme"]=_theme
-doc["ace_theme"].bind("change",change_theme)
-
-def reset_theme():
-    if storage:
-       if "ace_theme" in storage:
-          editor.setTheme(storage["ace_theme"])
-          doc["ace_theme"].value=storage["ace_theme"]
-
 if has_ace:
     reset_src()
-    reset_theme()
 else:
     reset_src_area()
