7fd036d7757e80bc5a53b0f626a4687a058d9f5c
PierreQuentel <pierre.quentel@gmail.com>
2015-03-23 17:38:36 +0100

    Add test on REMOTE_ADDR in speed.py

--- cgi-bin/speed.py
+++ cgi-bin/speed.py
@@ -1,15 +1,17 @@
 #!c:/python33/python.exe
 # -*- coding: utf-8 -*-
 
-
+import os
 import cgi
 import time
 
 print('Content-type: text/html\n\n')
 
-
-fs = cgi.FieldStorage()
-src = fs['src'].value
-t0 = time.perf_counter()
-exec(src)
-print('CPython: %6.2f ms' % ((time.perf_counter() - t0) * 1000.0))
+if os.environ['REMOTE_ADDR']!='127.0.0.1':
+    print('forbidden access')
+else:
+    fs = cgi.FieldStorage()
+    src = fs['src'].value
+    t0 = time.perf_counter()
+    exec(src)
+    print('CPython: %6.2f ms' % ((time.perf_counter() - t0) * 1000.0))
