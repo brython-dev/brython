56bc6ffb87c65c046c52372b070ef092cbe6bf2a
PierreQuentel <pierre.quentel@gmail.com>
2015-06-06 11:36:13 +0200

    Fixes #205 : Syntax error when trying to take ~ operator as function argument

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 1, 4, 'alpha', 0]
 __BRYTHON__.__MAGIC__ = "3.1.4"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2015-06-05 21:59:04.222000"
+__BRYTHON__.compiled_date = "2015-06-06 11:35:29.134000"
 __BRYTHON__.builtin_module_names = ["posix","sys","errno", "time","_ajax",
     "_browser",
     "_html",
