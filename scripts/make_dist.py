67292d90132eeaa7d432e457cbee8535e48aabac
PierreQuentel <pierre.quentel@gmail.com>
2015-06-12 18:36:35 +0200

    Remove Javascript minifier from make_dist.py

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -11,18 +11,13 @@ import sys
 import tarfile
 import zipfile
 
+import javascript_minifier
+
 if(sys.version_info[0]!=3):
     raise ValueError("This script only works with Python 3")
 
 import make_doc  # lint:ok
 import make_stdlib_list
-
-try:
-    import slimit
-    minify = slimit.minify
-except ImportError:
-    minify = slimit = None
-
 
 # path of parent directory
 pdir = os.path.dirname(os.getcwd())
@@ -34,75 +29,6 @@ implementation = [3, 1, 4, 'alpha', 0]
 vname = '.'.join(str(x) for x in implementation[:3])
 if implementation[3] == 'rc':
     vname += 'rc%s' % implementation[4]
-
-def custom_minify(src):
-    _res, pos = '', 0
-    while pos < len(src):
-        if src[pos] in ('"', "'"):
-            # the end of the string is the next quote if it is not
-            # after an odd number of backslashes
-            start = pos
-            while True:
-                end = src.find(src[pos], start + 1)
-                if end == -1:
-                    line = src[:pos].count('\n')
-                    raise SyntaxError('string not closed in %s line %s : %s' %
-                                      (fname, line, src[pos:pos + 20]))
-                else:
-                    # count number of backslashes before the quote
-                    nb = 0
-                    while src[end-nb-1] == '\\':
-                        nb += 1
-                    if not nb % 2:
-                        break
-                    else:
-                        start = end+1
-            _res += src[pos:end+1]
-            pos = end+1
-        elif src[pos] == '\r':
-            pos += 1
-        elif src[pos] == ' ':
-            if _res and _res[-1] in '({=[)}];|\n':
-                pos += 1
-                continue
-            _res += ' '
-            while pos < len(src) and src[pos] == ' ':
-                pos += 1
-        elif src[pos:pos + 2] == '//':
-            end = src.find('\n', pos)
-            if end == -1:
-                break
-            pos = end
-        elif src[pos:pos + 2] == '/*':
-            end = src.find('*/', pos)
-            if end == -1:
-                break
-            pos = end+2
-        elif src[pos] in '={[(' and _res and _res[-1] == ' ':
-            _res = _res[:-1]+src[pos]
-            pos += 1
-        elif src[pos] == ';' and pos < len(src) - 1 and src[pos + 1] in '\r\n':
-            pos += 1
-        elif src[pos] in '{[,':
-            _res += src[pos]
-            while pos < len(src) - 1 and src[pos + 1] in ' \r\n':
-                pos += 1
-            pos += 1
-        elif src[pos] == '}':
-            _res += src[pos]
-            nxt = pos + 1
-            while nxt < len(src) and src[nxt] in ' \r\n':
-                nxt += 1
-            if nxt < len(src) and src[nxt] == '}':
-                pos = nxt - 1
-            pos += 1
-        else:
-            _res += src[pos]
-            pos += 1
-    while '\n\n' in _res:
-        _res = _res.replace('\n\n', '\n')
-    return _res
-
 
 abs_path = lambda _pth: os.path.join(os.path.dirname(os.getcwd()), 'www', 'src', _pth)
 now = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
@@ -211,13 +137,7 @@ src_size = 0
 for fname in sources:
     src = open(abs_path(fname)+'.js').read() + '\n'
     src_size += len(src)
-    if minify is not None:
-        try:
-            res += minify(src)
-        except Exception as error:
-            print(error)
-    else:
-        res += custom_minify(src)
+    res += javascript_minifier.minify(src)
 
 res = res.replace('context', 'C')
 
