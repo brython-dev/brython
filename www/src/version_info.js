e22e9ac3ecd6bb57b8d3607d2be09f9c1dc0937b
Pierre Quentel <pierre.quentel@gmail.com>
2015-08-23 12:23:28 +0200

    Update generated files, add brython_dist to repository, remove py_unittest.js

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 2, 1, 'alpha', 0]
 __BRYTHON__.__MAGIC__ = "3.2.1"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2015-08-21 11:35:48.829136"
+__BRYTHON__.compiled_date = "2015-08-23 12:21:58.110568"
 __BRYTHON__.builtin_module_names = ["posix","sys","errno", "time","_ajax",
     "_browser",
     "_html",
