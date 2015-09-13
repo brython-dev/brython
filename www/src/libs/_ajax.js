a6538b46df0291d71e458c944b0d8b40a9e8fe20
Pierre Quentel <pierre.quentel@gmail.com>
2015-09-13 16:22:08 +0200

    Rewrite _ajax.js using JSObject

--- www/src/libs/_ajax.js
+++ www/src/libs/_ajax.js
@@ -2,125 +2,106 @@
 var $module = (function($B){
 
 eval($B.InjectBuiltins())
+var $N = $B.builtins.None
 
-var $XMLHttpDict = {__class__:$B.$type,__name__:'XMLHttp'}
-
-$XMLHttpDict.__getattribute__ = function(self,attr){
-    if(['headers','text','xml'].indexOf(attr)>-1){
-        return $XMLHttpDict[attr](self)
-    }
-    return _b_.object.$dict.__getattribute__(self,attr)
-}
-
-$XMLHttpDict.__mro__ = [$XMLHttpDict, _b_.object.$dict]
-
-$XMLHttpDict.__repr__ = function(self){return '<object XMLHttp>'}
-
-$XMLHttpDict.__str__ = $XMLHttpDict.toString = $XMLHttpDict.__repr__
-
-$XMLHttpDict.text = function(self){return self.responseText}
-
-$XMLHttpDict.xml = function(self){return $DomObject(self.responseXML)}
-
-$XMLHttpDict.headers = function(self){
-    return list(self.getAllResponseHeaders().split('\n'))
-}
-
-$XMLHttpDict.get_header = function(){
-    var reqobj = self;
-    return function(header){ return reqobj.getResponseHeader(header) }
-}
-
-var $AjaxDict = {__class__:$B.$type,__name__:'ajax'}
-
-$AjaxDict.__mro__ = [$AjaxDict, _b_.object.$dict]
-
-$AjaxDict.__repr__ = function(self){return '<object Ajax>'}
-
-$AjaxDict.__str__ = $AjaxDict.toString = $AjaxDict.__repr__
-
-$AjaxDict.bind = function(self,evt,func){
-    // req.bind(evt,func) is the same as req.on_evt = func
-    self['on_'+evt]=func
-}
-
-$AjaxDict.open = function(self,method,url,async){
-    self.$xmlhttp.open(method,url,async)
-}
-
-$AjaxDict.send = function(self,params){
-    // params can be Python dictionary or string
-    var res = ''
-    if(!params){
-        self.$xmlhttp.send();
-        return;
-    }else if(isinstance(params,str)){
-        res = params
-    }else if(isinstance(params,dict)){
-        var items = _b_.list(_b_.dict.$dict.items(params))
-        for(var i=0, _len_i = items.length; i < _len_i;i++){
-            var key = encodeURIComponent(str(items[i][0]));
-            if (isinstance(items[i][1],list)) {
-                for (j = 0; j < items[i][1].length; j++) {
-                    res += key +'=' + encodeURIComponent(str(items[i][1][j])) + '&'
-                }
-            } else {
-                res += key + '=' + encodeURIComponent(str(items[i][1])) + '&'
-            }
-        }
-        res = res.substr(0,res.length-1)
-    }else{
-        throw _b_.TypeError("send() argument must be string or dictionary, not '"+str(params.__class__)+"'")
-    }
-    self.$xmlhttp.send(res)
-}
-
-$AjaxDict.set_header = function(self,key,value){
-    self.$xmlhttp.setRequestHeader(key,value)
-}
-
-$AjaxDict.set_timeout = function(self,seconds,func){
-    self.$xmlhttp.$requestTimer = setTimeout(
-        function() {self.$xmlhttp.abort();func()},
-        seconds*1000);
-}
 
 function ajax(){
 
-    var res = {
-        __class__:$AjaxDict
-    }
-
     if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-        var $xmlhttp=new XMLHttpRequest();
+        var xmlhttp=new XMLHttpRequest();
     }else{// code for IE6, IE5
-        var $xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        var xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
-    $xmlhttp.$requestTimer = null
-    $xmlhttp.__class__ = $XMLHttpDict
-
-    $xmlhttp.onreadystatechange = function(){
-        // here, "this" refers to $xmlhttp
+    xmlhttp.onreadystatechange = function(){
+        // here, "this" refers to xmlhttp
         var state = this.readyState
         var req = this.$ajax
+        req.js.text = this.responseText
         var timer = this.$requestTimer
         var obj = this
-        if(state===0 && 'on_uninitialized' in req){req.on_uninitialized(obj)}
-        else if(state===1 && 'on_loading' in req){req.on_loading(obj)}
-        else if(state===2 && 'on_loaded' in req){req.on_loaded(obj)}
-        else if(state===3 && 'on_interactive' in req){req.on_interactive(obj)}
+        if(state===0 && 'on_uninitialized' in req){req.on_uninitialized(req)}
+        else if(state===1 && 'on_loading' in req){req.on_loading(req)}
+        else if(state===2 && 'on_loaded' in req){req.on_loaded(req)}
+        else if(state===3 && 'on_interactive' in req){req.on_interactive(req)}
         else if(state===4 && 'on_complete' in req){
             if(timer !== null){window.clearTimeout(timer)}
-            req.on_complete(obj)
+            req.on_complete(req)
         }
     }
-    $xmlhttp.$ajax = res
-    res.$xmlhttp = $xmlhttp
+    var res = {__class__:ajax.$dict, js:xmlhttp}
+    xmlhttp.$ajax = res
     return res
 }
 
 ajax.__class__ = $B.$factory
-ajax.$dict = $AjaxDict
+
+ajax.$dict = {
+
+    __class__:$B.$type,
+    __name__:'ajax',
+    $factory: ajax,
+    
+    __getattribute__ : function(self, attr){
+        // Special case for send : accept dict as parameters
+        if(attr=='send'){
+            return function(params){
+                return ajax.$dict.send(self, params)
+            }
+        }
+        // Otherwise default to JSObject method
+        return $B.JSObject.$dict.__getattribute__(self, attr)
+    },
+    
+    __repr__ : function(self){return '<object Ajax>'},
+    __str__ : function(self){return '<object Ajax>'},
+    
+    bind : function(self,evt,func){
+        // req.bind(evt,func) is the same as req.on_evt = func
+        self['on_'+evt] = func
+        return $N
+    },
+    
+    send : function(self,params){
+        // params can be Python dictionary or string
+        var res = ''
+        if(!params){
+            self.js.send();
+            return;
+        }else if(isinstance(params,str)){
+            res = params
+        }else if(isinstance(params,dict)){
+            var items = _b_.list(_b_.dict.$dict.items(params))
+            for(var i=0, _len_i = items.length; i < _len_i;i++){
+                var key = encodeURIComponent(str(items[i][0]));
+                if (isinstance(items[i][1],list)) {
+                    for (j = 0; j < items[i][1].length; j++) {
+                        res += key +'=' + encodeURIComponent(str(items[i][1][j])) + '&'
+                    }
+                } else {
+                    res += key + '=' + encodeURIComponent(str(items[i][1])) + '&'
+                }
+            }
+            res = res.substr(0,res.length-1)
+        }else{
+            throw _b_.TypeError("send() argument must be string or dictionary, not '"+str(params.__class__)+"'")
+        }
+        self.js.send(res)
+    },
+    
+    set_header : function(self,key,value){
+        self.js.setRequestHeader(key,value)
+    },
+    
+    set_timeout : function(self,seconds,func){
+        self.js.$requestTimer = setTimeout(
+            function() {self.js.abort();func()},
+            seconds*1000);
+    }
+}
+
+ajax.$dict.__mro__ = [ajax.$dict, $B.JSObject.$dict, _b_.object.$dict]
+
+$B.set_func_names(ajax.$dict)
 
 return {ajax:ajax}
 
