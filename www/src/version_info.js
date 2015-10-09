a747a03c3fe759c9589a6a4741efe8c42cb6a533
PierreQuentel <pierre.quentel@gmail.com>
2015-10-09 18:10:49 +0200

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 2, 3, 'alpha', 0]
 __BRYTHON__.__MAGIC__ = "3.2.3"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2015-10-09 18:05:25.055458"
+__BRYTHON__.compiled_date = "2015-10-09 18:09:40.264458"
 __BRYTHON__.builtin_module_names = ["posix","sys","errno", "time","_ajax",
     "_browser",
     "_html",
