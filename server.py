1e1956e44a52098e85e025104b6c494c1ad977e3
Pierre Quentel <pierre.quentel@orange.com>
2015-11-21 09:12:46 +0100

    Restore server.py to previous version

--- server.py
+++ server.py
@@ -10,9 +10,6 @@ served from subdirectory cgi-bin
 import os
 import sys
 from webbrowser import open_new_tab
-import email.utils
-import urllib.parse
-import datetime
 
 # generate static doc pages if not already present
 if not os.path.exists(os.path.join(os.getcwd(),'www','static_doc')):
@@ -36,67 +33,6 @@ cgi_dir = os.path.join(os.path.dirname(os.getcwd()), 'cgi-bin')
 
 class RequestHandler(CGIHTTPRequestHandler):
 
-    def send_head(self):
-        """Common code for GET and HEAD commands.
-
-        This sends the response code and MIME headers.
-
-        Return value is either a file object (which has to be copied
-        to the outputfile by the caller unless the command was HEAD,
-        and must be closed by the caller under all circumstances), or
-        None, in which case the caller has nothing further to do.
-
-        """
-        path = self.translate_path(self.path)
-        f = None
-        if os.path.isdir(path):
-            parts = urllib.parse.urlsplit(self.path)
-            if not parts.path.endswith('/'):
-                # redirect browser - doing basically what apache does
-                self.send_response(301)
-                new_parts = (parts[0], parts[1], parts[2] + '/',
-                             parts[3], parts[4])
-                new_url = urllib.parse.urlunsplit(new_parts)
-                self.send_header("Location", new_url)
-                self.end_headers()
-                return None
-            for index in "index.html", "index.htm":
-                index = os.path.join(path, index)
-                if os.path.exists(index):
-                    path = index
-                    break
-            else:
-                return self.list_directory(path)
-        ctype = self.guess_type(path)
-        try:
-            f = open(path, 'rb')
-        except OSError:
-            self.send_error(404, "File not found")
-            return None
-        if "If-Modified-Since" in self.headers:
-            ims = email.utils.parsedate(
-                self.headers["If-Modified-Since"])
-            if ims is not None:
-                ims_datetime = datetime.datetime(*ims[:7])
-                ims_dtstring = ims_datetime.strftime("%d %b %Y %H:%M:%S")
-                last_modif = datetime.datetime.utcfromtimestamp(
-                    os.stat(path).st_mtime).strftime("%d %b %Y %H:%M:%S")
-                if last_modif == ims_dtstring:
-                    self.send_response(304)
-                    self.end_headers()
-                    return None
-        try:
-            self.send_response(200)
-            self.send_header("Content-type", ctype)
-            fs = os.fstat(f.fileno())
-            self.send_header("Content-Length", str(fs[6]))
-            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-            self.end_headers()
-            return f
-        except:
-            f.close()
-            raise
-
     def translate_path(self, path):
         """For paths starting with /cgi-bin/, serve from cgi_dir"""
         elts = path.split('/')
@@ -111,3 +47,4 @@ print(("Server running on port http://localhost:{}.".format(server_address[1])))
 print("Press CTRL+C to Quit.")
 open_new_tab("http://localhost:{}/".format(server_address[1]))
 httpd.serve_forever()
+
