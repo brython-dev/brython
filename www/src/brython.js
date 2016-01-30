7d13e57af5cac27a735cfdfde7e358d3aeb27679
Pierre Quentel <pierre.quentel@orange.com>
2016-01-30 11:58:08 +0100

    Fixes #369 : time.ctime hard coded to +2:00 UTC

--- www/src/brython.js
+++ www/src/brython.js
@@ -60,7 +60,7 @@ return $B.frames_stack[$B.frames_stack.length-1][3]}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,2,5,'alpha',0]
 __BRYTHON__.__MAGIC__="3.2.5"
 __BRYTHON__.version_info=[3,3,0,'alpha',0]
-__BRYTHON__.compiled_date="2016-01-30 10:36:32.034740"
+__BRYTHON__.compiled_date="2016-01-30 11:57:09.522049"
 __BRYTHON__.builtin_module_names=["posix","sys","errno","time","_ajax","_browser","_html","_jsre","_multiprocessing","_posixsubprocess","_svg","_sys","builtins","dis","hashlib","javascript","json","long_int","math","modulefinder","random","_abcoll","_codecs","_collections","_csv","_functools","_imp","_io","_random","_socket","_sre","_string","_struct","_sysconfigdata","_testcapi","_thread","_warnings","_weakref"]
 
 ;(function($B){var js,$pos,res,$op
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 2, 5, 'alpha', 0]
 __BRYTHON__.__MAGIC__ = "3.2.5"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2016-01-30 10:36:32.034740"
+__BRYTHON__.compiled_date = "2016-01-30 11:57:09.522049"
 __BRYTHON__.builtin_module_names = ["posix","sys","errno", "time","_ajax",
     "_browser",
     "_html",
