e57612a9313a2faf2235a0a7823f4d5f22e5e27f
Pierre Quentel <pierre.quentel@orange.com>
2016-02-11 16:46:10 +0100

    Fixes #373 : ajax gallery example broken in Safari

--- www/src/brython.js
+++ www/src/brython.js
@@ -60,7 +60,7 @@ return $B.frames_stack[$B.frames_stack.length-1][3]}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,2,5,'alpha',0]
 __BRYTHON__.__MAGIC__="3.2.5"
 __BRYTHON__.version_info=[3,3,0,'alpha',0]
-__BRYTHON__.compiled_date="2016-02-10 22:42:08.003689"
+__BRYTHON__.compiled_date="2016-02-11 16:43:53.006020"
 __BRYTHON__.builtin_module_names=["posix","sys","errno","time","_ajax","_browser","_html","_jsre","_multiprocessing","_posixsubprocess","_svg","_sys","builtins","dis","hashlib","javascript","json","long_int","math","modulefinder","random","_abcoll","_codecs","_collections","_csv","_functools","_imp","_io","_random","_socket","_sre","_string","_struct","_sysconfigdata","_testcapi","_thread","_warnings","_weakref"]
 
 ;(function($B){var js,$pos,res,$op
