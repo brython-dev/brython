9c3739cd04431660f8a0d7565d4d0296541845b7
Pierre Quentel <pierre.quentel@orange.com>
2016-02-14 16:04:47 +0100

    Add a boolean "supported" to browser.websocket. Fix bug in documentation for this module

--- www/src/Lib/browser/websocket.py
+++ www/src/Lib/browser/websocket.py
@@ -1,4 +1,10 @@
 from browser import window
 import javascript
 
-WebSocket = javascript.JSConstructor(window.WebSocket)
+if hasattr(window, 'WebSocket'):
+    supported = True
+    WebSocket = javascript.JSConstructor(window.WebSocket)
+else:
+    supported = False
+    def WebSocket(*args,**kw):
+        throw NotImplementedError
