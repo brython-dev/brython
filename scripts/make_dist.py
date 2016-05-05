34f40d85a6cc5a096eab78be5b5c3ba0f23eee1b
Pierre Quentel <pierre.quentel@orange.com>
2016-05-05 18:33:36 +0200

    Change version to 3.2.7 alpha

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -20,7 +20,7 @@ if(sys.version_info[0]!=3):
 pdir = os.path.dirname(os.getcwd())
 # version info
 version = [3, 3, 0, "alpha", 0]
-implementation = [3, 2, 6, 'alpha', 0]
+implementation = [3, 2, 7, 'alpha', 0]
 
 # version name
 vname = '.'.join(str(x) for x in implementation[:3])
