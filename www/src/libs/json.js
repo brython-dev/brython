25ca91a75bfd962409307cdb277e9f9086d2e85e
Quentin Santos <quentin.santos@worldonline.fr>
2016-07-27 20:39:21 +0200

    Fix #466: add method `json.load()`

--- www/src/libs/json.js
+++ www/src/libs/json.js
@@ -4,6 +4,9 @@ return  {
     loads : function(json_obj){
         return $B.jsobject2pyobject(JSON.parse(json_obj))
     },
+    load : function(file_obj){
+        return $module.loads(file_obj.$content);
+    },
     dumps : function(obj){return JSON.stringify($B.pyobject2jsobject(obj))},
 }
 
