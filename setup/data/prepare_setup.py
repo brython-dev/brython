85af89ab9592795b49437eb23ebd408e544c55b9
Pierre Quentel <pierre.quentel@orange.com>
2016-11-11 13:34:46 +0100

    Fix bug in prepare_setup.py

--- setup/data/prepare_setup.py
+++ setup/data/prepare_setup.py
@@ -5,7 +5,7 @@ import json
 import make_bundle
 
 # prepare script with standard distribution
-www = os.path.join(os.path.dirname(os.getcwd()), 'www')
+www = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'www')
 folders = [
     os.path.join(www, 'src', 'Lib'),
     os.path.join(www, 'src', 'libs')
@@ -16,6 +16,9 @@ for folder in folders:
 
 # copy stdlib in core/lib/brython_stdlib.js
 lib_dir = os.path.join(os.getcwd(), 'lib')
+if not os.path.exists(lib_dir):
+    os.mkdir(lib_dir)
+
 bundle_path = os.path.join(lib_dir, 'brython_stdlib.js')
 with open(bundle_path, 'w', encoding='utf-8') as out:
     out.write('__BRYTHON__.use_VFS = true;\n')
