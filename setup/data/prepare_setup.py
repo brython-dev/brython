631b3592508d7478e16609918a6114c8dd9571b4
Pierre Quentel <pierre.quentel@orange.com>
2016-12-07 20:22:12 +0100

    Restore locations from previous commit

--- setup/data/prepare_setup.py
+++ setup/data/prepare_setup.py
@@ -2,11 +2,10 @@ import os
 import shutil
 import json
 
-import make_dist
-import make_bundle
+from tools import make_bundle
 
 # prepare script with standard distribution
-www = os.path.join(os.path.dirname(os.getcwd()), 'www')
+www = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'www')
 folders = [
     os.path.join(www, 'src', 'Lib'),
     os.path.join(www, 'src', 'libs')
@@ -18,23 +17,19 @@ for folder in folders:
 # copy stdlib in brython_stdlib.js
 # and initialise brython_modules.js with the same content
 
-setup_dir = os.path.join(os.path.dirname(os.getcwd()), 'setup', 'data')
 bundle_names = ['brython_stdlib.js', 'brython_modules.js']
 for name in bundle_names:
-    with open(os.path.join(setup_dir, name), 'w', encoding='utf-8') as out:
+    with open(name, 'w', encoding='utf-8') as out:
         out.write('__BRYTHON__.use_VFS = true;\n')
         out.write('__BRYTHON__.VFS = {}\n'.format(json.dumps(res)))
 
 # copy brython.js
-shutil.copyfile(os.path.join(www, 'src', 'brython.js'), 
-    os.path.join(setup_dir, 'brython.js'))
+shutil.copyfile(os.path.join(www, 'src', 'brython.js'), 'brython.js')
 
 # create zip file
-import tarfile
-import zipfile
-
-name = 'Brython-{}'.format('.'.join(str(x) for x in make_dist.version[:3]))
-dest_path = os.path.join(setup_dir, name)
+version = "0.0.3"
+name = 'Brython-%s' %version
+dest_path = name
 dist1 = tarfile.open(dest_path + '.tar.gz', mode='w:gz')
 dist2 = tarfile.open(dest_path+'.tar.bz2', mode='w:bz2')
 dist3 = zipfile.ZipFile(dest_path + '.zip', mode='w',
@@ -44,6 +39,6 @@ paths = ['index.html', 'brython.js', 'brython_stdlib.js', 'brython_modules.js']
 
 for arc, wfunc in (dist1, dist1.add), (dist2, dist2.add), (dist3, dist3.write):
     for path in paths:
-        wfunc(os.path.join(setup_dir, path), arcname=os.path.join(name, path))
+        wfunc(os.path.join(pdir, path), arcname=os.path.join(name, path))
 
     arc.close()
