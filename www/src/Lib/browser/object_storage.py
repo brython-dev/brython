bccda7b336f3a7aa149e6f0d0ff4ba3216456b0e
Pierre Quentel <pierre.quentel@orange.com>
2017-05-04 22:05:42 +0200

    Fixes #581 : pickle.dumps returns a str object instead of bytes

--- www/src/Lib/browser/object_storage.py
+++ www/src/Lib/browser/object_storage.py
@@ -1,4 +1,4 @@
-import pickle
+import json
 
 class __UnProvided():
     pass
@@ -10,25 +10,25 @@ class ObjectStorage():
         self.storage = storage
 
     def __delitem__(self, key):
-        del self.storage[pickle.dumps(key)]
+        del self.storage[json.dumps(key)]
 
     def __getitem__(self, key):
-        return pickle.loads(self.storage[pickle.dumps(key)])
+        return json.loads(self.storage[json.dumps(key)])
 
     def __setitem__(self, key, value):
-        self.storage[pickle.dumps(key)] = pickle.dumps(value)
+        self.storage[json.dumps(key)] = json.dumps(value)
 
     def __contains__(self, key):
-        return pickle.dumps(key) in self.storage
+        return json.dumps(key) in self.storage
 
     def get(self, key, default=None):
-        if pickle.dumps(key) in self.storage:
-            return self.storage[pickle.dumps(key)]
+        if json.dumps(key) in self.storage:
+            return self.storage[json.dumps(key)]
         return default
 
     def pop(self, key, default=__UnProvided()):
-        if type(default) is __UnProvided or pickle.dumps(key) in self.storage:
-            return pickle.loads(self.storage.pop(pickle.dumps(key)))
+        if type(default) is __UnProvided or json.dumps(key) in self.storage:
+            return json.loads(self.storage.pop(json.dumps(key)))
         return default
 
     def __iter__(self):
@@ -36,10 +36,10 @@ class ObjectStorage():
         return keys.__iter__()
 
     def keys(self):
-        return [pickle.loads(key) for key in self.storage.keys()]
+        return [json.loads(key) for key in self.storage.keys()]
 
     def values(self):
-        return [pickle.loads(val) for val in self.storage.values()]
+        return [json.loads(val) for val in self.storage.values()]
 
     def items(self):
         return list(zip(self.keys(), self.values()))
