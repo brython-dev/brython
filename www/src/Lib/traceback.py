1efa4f29a9897b1c48bbedcd3bc24f04d49e31b9
Pierre Quentel <pierre.quentel@orange.com>
2017-05-15 15:25:13 +0200

    Fixes #587 : print_exc and format_exc differs

--- www/src/Lib/traceback.py
+++ www/src/Lib/traceback.py
@@ -1,6 +1,12 @@
 import sys
 from browser import console
 
+def _restore_current(exc):
+    """Restore internal attribute current_exception, it may have been modified
+    by the code inside functions of this module.
+    """
+    __BRYTHON__.current_exception = exc
+    
 def print_exc(file=sys.stderr):
     exc = __BRYTHON__.current_exception
     if isinstance(exc, SyntaxError):
@@ -10,16 +16,18 @@ def print_exc(file=sys.stderr):
         file.write('\n  '+offset*' '+'^')
     else:
         file.write(exc.info)
-    file.write('\n'+exc.__name__)
+    msg = exc.__name__
     if exc.args:
-        file.write(': %s' %exc.args[0])
-    file.write('\n')
+        msg += ': %s' %exc.args[0]
+    file.write(msg+'\n')
+    _restore_current(exc)
 
-def format_exc(limit=None,chain=True):
+def format_exc(limit=None, chain=True):
     exc = __BRYTHON__.current_exception
     res = exc.info+'\n'+exc.__name__
     if exc.args:
         res += ': '+exc.args[0]
+    _restore_current(exc)
     return res+'\n'
 
 def format_exception(_type, value, tb, limit=None, chain=True):
