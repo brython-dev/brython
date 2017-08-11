e20298ca0c81f3675d6a2d0bfa6668fcaca4e9b6
Pierre Quentel <pierre.quentel@orange.com>
2017-08-11 15:04:42 +0200

    Fixes #638 : internal Javascript error when parsing invalid python code

--- www/src/brython_dist.js
+++ www/src/brython_dist.js
@@ -62,7 +62,7 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,3,3,'dev',0]
 __BRYTHON__.__MAGIC__="3.3.3"
 __BRYTHON__.version_info=[3,3,0,'alpha',0]
-__BRYTHON__.compiled_date="2017-08-11 14:49:55.598241"
+__BRYTHON__.compiled_date="2017-08-11 15:03:47.005794"
 __BRYTHON__.builtin_module_names=["posix","sys","errno","time","_ajax","_base64","_jsre","_multiprocessing","_posixsubprocess","_profile","_svg","_sys","builtins","dis","hashlib","json","long_int","math","modulefinder","random","_abcoll","_codecs","_collections","_csv","_functools","_imp","_io","_random","_socket","_sre","_string","_struct","_sysconfigdata","_testcapi","_thread","_warnings","_weakref"]
 
 ;(function($B){var js,$pos,res,$op
@@ -3154,6 +3154,8 @@ case 'for':
 switch(token){case 'in':
 return new $AbstractExprCtx(new $ExprCtx(C,'target list',true),false)
 case ':':
+if(C.tree.length<2){
+$_SyntaxError(C,'token '+token+' after '+C)}
 return $BodyCtx(C)}
 $_SyntaxError(C,'token '+token+' after '+C)
 case 'from':
