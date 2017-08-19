brython-dev/brython

commit e7a15aa8ee7739bb7d60de5dfe7352c191a2fe72

Fix bug in method attributes (__func__, __self__, etc)

www/src/py_type.js
@@ -119,7 +119,7 @@ $B.$class_constructor = function(class_name,class_obj,parents,parents_names,kwar
         for(var i=1;i<mro.length;i++){
             if(mro[i].__class__ !== $B.$type){
                 metaclass = mro[i].__class__.$factory
-		break
+        break
             }
         }
     }
@@ -680,9 +680,9 @@ $B.$MethodDict.__ne__ = function(self, other){
 }
 
 $B.$MethodDict.__getattribute__ = function(self, attr){
-    // Internal attributes __name__, __module__, __doc__ etc.
-    // are stored in self.$infos.__func__.$infos
-    var infos = self.$infos.__func__.$infos
+    // Internal attributes __name__, __func__, __self__ etc.
+    // are stored in self.$infos
+    var infos = self.$infos
     if(infos && infos[attr]){
         if(attr=='__code__'){
             var res = {__class__:$B.$CodeDict}
@@ -697,6 +697,7 @@ $B.$MethodDict.__getattribute__ = function(self, attr){
         return _b_.object.$dict.__getattribute__(self, attr)
     }
 }
+
 $B.$MethodDict.__mro__=[_b_.object.$dict]
 $B.$MethodDict.__repr__ = $B.$MethodDict.__str__ = function(self){
     return '<bound method '+self.$infos.__class__.$dict.__name__+'.'+

commit 8a34dd3fff0c59488c80cd3b1ba88501b42ddb94

Fix bug with method attributes introduced by commit e7a15aa

www/src/py_type.js
@@ -35,12 +35,12 @@ $B.$class_constructor = function(class_name,class_obj,parents,parents_names,kwar
      * We can't use __getattribute__ since it must be defined directly on a parent,
      * not further up the mro.
      */
-    var init_subclass = function init_subclass(){};
+    function init_subclass(){};
     for (var i=0;i<bases.length;i++) {
         if (bases[i].$dict.$methods) {
             var __init_subclass__ = bases[i].$dict.$methods.__init_subclass__;
             if (__init_subclass__) {
-                init_subclass = function init_subclass(cls) {
+                function init_subclass(cls) {
                     var kw = {
                         $nat:true,
                         kw:{}
@@ -683,6 +683,12 @@ $B.$MethodDict.__getattribute__ = function(self, attr){
     // Internal attributes __name__, __func__, __self__ etc.
     // are stored in self.$infos
     var infos = self.$infos
+    switch(attr){
+        case "__func__":
+        case "__self__":
+            return infos[attr]
+    }
+    infos = infos.__func__.$infos
     if(infos && infos[attr]){
         if(attr=='__code__'){
             var res = {__class__:$B.$CodeDict}