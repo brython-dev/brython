7dfb5cb349b7bc8a3fd1961a8881b7b7e35c2eda
Pierre Quentel <pierre.quentel@orange.com>
2017-08-29 09:14:25 +0200

    Restore missing attributes in module os

--- www/src/Lib/os.py
+++ www/src/Lib/os.py
@@ -15,8 +15,15 @@ sys.modules['os.path'] = path
 from os.path import (curdir, pardir, sep, pathsep, defpath, extsep, altsep,
     devnull)
 
-# unicode environ
-environ = {'HOME': __BRYTHON__.curdir}
+environ = {'HOME': __BRYTHON__.curdir,
+    'PYTHONPATH': __BRYTHON__.brython_path
+}
+
+def _get_exports_list(module):
+    try:
+        return list(module.__all__)
+    except AttributeError:
+        return [n for n in dir(module) if n[0] != '_']
 
 def getenv(key, default=None):
     """Get an environment variable, return None if it doesn't exist.
@@ -29,6 +36,45 @@ supports_bytes_environ = True
 def chdir(path):
     __BRYTHON__.curdir = path
 
+def fsencode(filename):
+    """
+    Encode filename to the filesystem encoding with 'surrogateescape' error
+    handler, return bytes unchanged. On Windows, use 'strict' error handler if
+    the file system encoding is 'mbcs' (which is the default encoding).
+    """
+    encoding = sys.getfilesystemencoding()
+    errors = 'surrogateescape'
+    if isinstance(filename, bytes):
+        return filename
+    elif isinstance(filename, str):
+        return filename.encode(encoding, errors)
+    else:
+        raise TypeError("expect bytes or str, not %s" % type(filename).__name__)
+
+def fsdecode(filename):
+    """
+    Decode filename from the filesystem encoding with 'surrogateescape' error
+    handler, return str unchanged. On Windows, use 'strict' error handler if
+    the file system encoding is 'mbcs' (which is the default encoding).
+    """
+    encoding = sys.getfilesystemencoding()
+    errors = 'surrogateescape'
+    if isinstance(filename, str):
+        return filename
+    elif isinstance(filename, bytes):
+        return filename.decode(encoding, errors)
+    else:
+        raise TypeError("expect bytes or str, not %s" % type(filename).__name__)
 def getcwd():
     return __BRYTHON__.curdir
-    
+
+_set = set()
+
+supports_dir_fd = _set
+
+supports_effective_ids = _set
+
+supports_fd = _set
+
+supports_follow_symlinks = _set
+
