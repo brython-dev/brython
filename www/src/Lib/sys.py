250cbc897c73911352d94b67af51aff9ae36ddfb
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2017-09-10 18:22:24 +0200

    Small bugfix in the name of the repr method of hash_info

--- www/src/Lib/sys.py
+++ www/src/Lib/sys.py
@@ -172,7 +172,7 @@ class _hash_info:
       self.seed_bits=128
       cutoff=0
 
-  def __repr(self):
+  def __repr__(self):
       #fix me
       return "sys.hash_info(width=32, modulus=2147483647, inf=314159, nan=0, imag=1000003, algorithm='siphash24', hash_bits=64, seed_bits=128, cutoff=0)"
 
