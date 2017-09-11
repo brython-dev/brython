8a2a3d0e276ef1a48345e28b5855c9384f343d84
Jonathan Verner <jonathan.verner@matfyz.cz>
2017-09-11 15:43:40 +0200

    Small bugfixes to the asyncio library.

--- www/src/Lib/asyncio/coroutines.py
+++ www/src/Lib/asyncio/coroutines.py
@@ -49,12 +49,16 @@ def coroutine(func):
             first_result = next(generator)
             _cb = get_continuation(generator, final_result)
             first_result.add_done_callback(_cb)
-        except StopIteration:
-            final_result.set_result(None)
+        except StopIteration as ex:
+            if hasattr(ex, 'value'):
+                final_result.set_result(ex.value)
+            else:
+                final_result.set_result(None)
         except Exception as ex:
             final_result.set_exception(ex)
         return final_result
     run.__coroutine = True
+    run.__name__ = func.__name__
     return run
 
 def iscoroutine(obj):
