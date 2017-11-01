395ee15b3c7d8688ca7516b418ec5abb6730bfd4
Pierre Quentel <pierre.quentel@orange.com>
2017-11-01 09:43:46 +0100

    In ajax.py, replace argument "async" by "asynchr", to avoid conflicts when "async" becomes a keyword

--- www/src/Lib/browser/ajax.py
+++ www/src/Lib/browser/ajax.py
@@ -3,9 +3,9 @@ from _ajax import *
 class Request:
 
     def __init__(self, url, method="GET", headers={},
-            data=None, async=True, timeout=None):
+            data=None, asynchr=True, timeout=None):
         self.req = ajax1()
-        self.req.open(method, url, async)
+        self.req.open(method, url, asynchr)
         self.method = method
         self.headers = headers
         self.req.timeout_secs = timeout
