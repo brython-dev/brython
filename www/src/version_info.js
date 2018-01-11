d4be1696ba171eaf7468435d02fc63bed6f19ec1
Pierre Quentel <quentel.pierre@orange.fr>
2018-01-11 09:09:48 +0100

    Update generated files

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 4, 1, 'dev', 0]
 __BRYTHON__.__MAGIC__ = "3.4.1"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2018-01-11 09:02:35.257809"
+__BRYTHON__.compiled_date = "2018-01-11 09:09:12.681609"
 __BRYTHON__.builtin_module_names = ["posix","sys", "errno", "time","_ajax",
     "_base64",
     "_jsre",
