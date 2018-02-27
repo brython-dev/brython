c644fa5492ed934a3db765641b75bc6a3e8b13a8
Pierre Quentel <quentel.pierre@orange.fr>
2018-02-27 09:33:45 +0100

    Update generated scripts

--- www/src/brython_dist.js
+++ www/src/brython_dist.js
@@ -71,8 +71,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,5,0,'rc',0]
 __BRYTHON__.__MAGIC__="3.5.0"
 __BRYTHON__.version_info=[3,3,0,'alpha',0]
-__BRYTHON__.compiled_date="2018-02-27 09:26:32.084689"
-__BRYTHON__.timestamp=1519719992084
+__BRYTHON__.compiled_date="2018-02-27 09:33:10.956408"
+__BRYTHON__.timestamp=1519720390956
 __BRYTHON__.builtin_module_names=["posix","sys","errno","time","_ajax","_base64","_jsre","_multiprocessing","_posixsubprocess","_profile","_svg","_sys","builtins","dis","hashlib","json","long_int","math","modulefinder","random","_abcoll","_codecs","_collections","_csv","_functools","_imp","_io","_random","_socket","_sre","_string","_struct","_sysconfigdata","_testcapi","_thread","_warnings","_weakref"]
 
 ;(function($B){Number.isInteger=Number.isInteger ||function(value){return typeof value==='number' &&
