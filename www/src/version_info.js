531649c2d7696e5473c386ce7d97eb12883de50f
Pierre Quentel <pierre@brython.info>
2018-06-06 16:19:16 +0200

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
-__BRYTHON__.implementation = [3, 5, 2, 'dev', 0]
-__BRYTHON__.__MAGIC__ = "3.5.2"
+__BRYTHON__.implementation = [3, 6, 0, 'dev', 0]
+__BRYTHON__.__MAGIC__ = "3.6.0"
 __BRYTHON__.version_info = [3, 3, 0, 'alpha', 0]
-__BRYTHON__.compiled_date = "2018-06-06 14:57:55.560431"
-__BRYTHON__.timestamp = 1528289875576
+__BRYTHON__.compiled_date = "2018-06-06 16:18:12.369996"
+__BRYTHON__.timestamp = 1528294692369
 __BRYTHON__.builtin_module_names = ["posix","sys", "errno", "time","_ajax",
     "_base64",
     "_jsre",
