1aa8d4002378b39bda0c0dbf05c613777b2722fd
Evan Mallory <evanmall@andrew.cmu.edu>
2018-08-06 11:50:29 -0400

    One more copy_to_af.sh change

--- copy_to_af.sh
+++ copy_to_af.sh
@@ -6,7 +6,7 @@ DEST=../cs1-v2/frontend/src/brython
 
 rm -rf "$DEST"
 mkdir "$DEST"
-cp -r www/src/libs www/src/Lib setup "$DEST"
+cp -r www/src/brython.js www/src/brython_stdlib.js www/src/libs www/src/Lib setup "$DEST"
 
 # Remove files that we don't need
 rm -rf "$DEST/setup/data" "$DEST/Lib/test" "$DEST/setup/changelog.txt"
