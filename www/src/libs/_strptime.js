8526d67c5e4a42dd2721760f53c75903265125d7
Pierre Quentel <pierre@brython.info>
2018-10-11 10:59:43 +0200

    Replace Lib/_strptime.py by libs/_strptime.js

---
+++ www/src/libs/_strptime.js
@@ -0,0 +1,506 @@
+var dt_formats = {
+    "am_pm": {
+        "Fri Nov %d %H:%M:%S %Y": [
+            "C"
+        ],
+        "%m/%d/%Y %H:%M:%S": [
+            "ab",
+            "ae",
+            "an",
+            "av",
+            "ay",
+            "bh",
+            "bi",
+            "ch",
+            "cr",
+            "cv",
+            "fj",
+            "ho",
+            "ht",
+            "hz",
+            "ie",
+            "ik",
+            "io",
+            "kg",
+            "kj",
+            "kv",
+            "li",
+            "mh",
+            "mo",
+            "na",
+            "ng",
+            "nv",
+            "ny",
+            "oj",
+            "pi",
+            "rc",
+            "sc",
+            "sh",
+            "sm",
+            "su",
+            "tl",
+            "tw",
+            "ty",
+            "wa",
+            "za",
+            "zu"
+        ],
+        "%Y-%m-%d %H:%M:%S": [
+            "af",
+            "br",
+            "ce",
+            "eo",
+            "lt",
+            "mg",
+            "nr",
+            "rw",
+            "se",
+            "sn",
+            "ss",
+            "st",
+            "sv",
+            "tn",
+            "ts",
+            "ug",
+            "ve",
+            "vo",
+            "xh"
+        ],
+        "%d.%m.%Y %H:%M:%S": [
+            "az",
+            "cs",
+            "de",
+            "et",
+            "fo",
+            "hy",
+            "is",
+            "ka",
+            "kk",
+            "lv",
+            "mk",
+            "nb",
+            "nn",
+            "no",
+            "os",
+            "pl",
+            "ro",
+            "ru",
+            "tg",
+            "tr",
+            "tt"
+        ],
+        "%d.%m.%y %H:%M:%S": [
+            "ba",
+            "be",
+            "lb"
+        ],
+        "%d.%m.%Y \u0433. %H:%M:%S": [
+            "bg"
+        ],
+        "%d/%m/%Y %H:%M:%S": [
+            "bm",
+            "ca",
+            "co",
+            "cy",
+            "es",
+            "ff",
+            "fr",
+            "ga",
+            "gd",
+            "gl",
+            "gn",
+            "gv",
+            "ha",
+            "he",
+            "ig",
+            "it",
+            "ki",
+            "kw",
+            "la",
+            "lg",
+            "ln",
+            "lo",
+            "lu",
+            "mt",
+            "nd",
+            "pt",
+            "rn",
+            "sg",
+            "sw",
+            "uk",
+            "uz",
+            "wo",
+            "yo"
+        ],
+        "%Y/%m/%d %H:%M:%S": [
+            "bo",
+            "eu",
+            "ia",
+            "ja",
+            "zh"
+        ],
+        "%d.%m.%Y. %H:%M:%S": [
+            "bs",
+            "hr",
+            "sr"
+        ],
+        "%Y.%m.%d %H:%M:%S": [
+            "cu",
+            "mn"
+        ],
+        "%d-%m-%Y %H:%M:%S": [
+            "da",
+            "fy",
+            "hi",
+            "kl",
+            "mr",
+            "my",
+            "nl",
+            "rm",
+            "sa",
+            "ta"
+        ],
+        "%d/%m/%y %H:%M:%S": [
+            "dv",
+            "km"
+        ],
+        "%d.%m.%Y %H.%M.%S": [
+            "fi"
+        ],
+        "%d-%m-%y %H:%M:%S": [
+            "gu",
+            "kn",
+            "or",
+            "te"
+        ],
+        "%Y. %m. %d. %H:%M:%S": [
+            "hu"
+        ],
+        "%d/%m/%Y %H.%M.%S": [
+            "id",
+            "jv",
+            "oc"
+        ],
+        "%d-\u043d\u043e\u044f %y %H:%M:%S": [
+            "ky"
+        ],
+        "%Y-%m-%d %H.%M.%S": [
+            "si"
+        ],
+        "%d. %m. %Y %H:%M:%S": [
+            "sk",
+            "sl"
+        ],
+        "%d.%m.%y \u00fd. %H:%M:%S": [
+            "tk"
+        ]
+    },
+    "am": {
+        "%d/%m/%Y %H:%M:%S saaku": [
+            "aa"
+        ],
+        "%Y/%m/%d %H:%M:%S AN": [
+            "ak"
+        ],
+        "%d/%m/%Y %H:%M:%S \u1325\u12cb\u1275": [
+            "am"
+        ],
+        "%d-%m-%Y \u09f0\u09be\u09a4\u09bf\u09aa\u09c1 %H:%M:%S": [
+            "as"
+        ],
+        "%d/%m/%Y %H:%M:%S AM": [
+            "bn",
+            "iu",
+            "kr",
+            "ml",
+            "sd",
+            "to",
+            "ur"
+        ],
+        "%Y-%m-%d \u0f46\u0f74\u0f0b\u0f5a\u0f7c\u0f51\u0f0b%H:%M:%S \u0f66\u0f94\u0f0b\u0f46\u0f0b": [
+            "dz"
+        ],
+        "%m/%d/%Y \u014bdi ga %H:%M:%S": [
+            "ee"
+        ],
+        "%d/%m/%Y %H:%M:%S \u03c0\u03bc": [
+            "el"
+        ],
+        "%m/%d/%Y %H:%M:%S AM": [
+            "en",
+            "ks"
+        ],
+        "%Y/%m/%d \ua0b5\ua1aa\ua20c\ua210 %H:%M:%S": [
+            "ii",
+            "yi"
+        ],
+        "%Y-%m-%d \uc624\uc804 %H:%M:%S": [
+            "ko"
+        ],
+        "%Y/%m/%d %H:%M:%S \u067e.\u0646": [
+            "ku"
+        ],
+        "%d/%m/%Y %H:%M:%S a.m.": [
+            "mi",
+            "qu"
+        ],
+        "%d/%m/%Y %H:%M:%S PG": [
+            "ms"
+        ],
+        "%m/%d/%Y %H:%M:%S \u092a\u0942\u0930\u094d\u0935\u093e\u0939\u094d\u0928": [
+            "ne"
+        ],
+        "%d/%m/%Y %H:%M:%S WD": [
+            "om"
+        ],
+        "%d-%m-%y \u0a38\u0a35\u0a47\u0a30 %H:%M:%S": [
+            "pa"
+        ],
+        "%d/%m/%Y %H:%M:%S sn.": [
+            "so"
+        ],
+        "%d.%m.%Y %H:%M:%S e paradites": [
+            "sq"
+        ],
+        "%d/%m/%Y %H:%M:%S \u1295\u1309\u1206 \u1230\u12d3\u1270": [
+            "ti"
+        ],
+        "%d/%m/%Y %H:%M:%S SA": [
+            "vi"
+        ]
+    },
+    "pm": {
+        "%d/%m/%Y %H:%M:%S carra": [
+            "aa"
+        ],
+        "%Y/%m/%d %H:%M:%S EW": [
+            "ak"
+        ],
+        "%d/%m/%Y %H:%M:%S \u12a8\u1230\u12d3\u1275": [
+            "am"
+        ],
+        "%d-%m-%Y \u0986\u09ac\u09c7\u09b2\u09bf %H:%M:%S": [
+            "as"
+        ],
+        "%d/%m/%Y %H:%M:%S PM": [
+            "bn",
+            "iu",
+            "kr",
+            "ml",
+            "sd",
+            "to",
+            "ur"
+        ],
+        "%Y-%m-%d \u0f46\u0f74\u0f0b\u0f5a\u0f7c\u0f51\u0f0b%H:%M:%S \u0f55\u0fb1\u0f72\u0f0b\u0f46\u0f0b": [
+            "dz"
+        ],
+        "%m/%d/%Y \u0263etr\u0254 ga %H:%M:%S": [
+            "ee"
+        ],
+        "%d/%m/%Y %H:%M:%S \u03bc\u03bc": [
+            "el"
+        ],
+        "%m/%d/%Y %H:%M:%S PM": [
+            "en",
+            "ks"
+        ],
+        "%Y/%m/%d \ua0b5\ua1aa\ua20c\ua248 %H:%M:%S": [
+            "ii",
+            "yi"
+        ],
+        "%Y-%m-%d \uc624\ud6c4 %H:%M:%S": [
+            "ko"
+        ],
+        "%Y/%m/%d %H:%M:%S \u062f.\u0646": [
+            "ku"
+        ],
+        "%d/%m/%Y %H:%M:%S p.m.": [
+            "mi",
+            "qu"
+        ],
+        "%d/%m/%Y %H:%M:%S PTG": [
+            "ms"
+        ],
+        "%m/%d/%Y %H:%M:%S \u0905\u092a\u0930\u093e\u0939\u094d\u0928": [
+            "ne"
+        ],
+        "%d/%m/%Y %H:%M:%S WB": [
+            "om"
+        ],
+        "%d-%m-%y \u0a36\u0a3e\u0a2e %H:%M:%S": [
+            "pa"
+        ],
+        "%d/%m/%Y %H:%M:%S gn.": [
+            "so"
+        ],
+        "%d.%m.%Y %H:%M:%S e pasdites": [
+            "sq"
+        ],
+        "%d/%m/%Y %H:%M:%S \u12f5\u1215\u122d \u1230\u12d3\u1275": [
+            "ti"
+        ],
+        "%d/%m/%Y %H:%M:%S CH": [
+            "vi"
+        ]
+    }
+}
+
+var locale = __BRYTHON__.locale
+
+var shortdays = [],
+    longdays = []
+
+for(var day = 16; day < 23; day++){
+    var d = new Date(Date.UTC(2012, 11, day, 3, 0, 0))
+    shortdays.push(d.toLocaleDateString(locale, {weekday: 'short'}))
+    longdays.push(d.toLocaleDateString(locale, {weekday: 'long'}))
+}
+
+var shortmonths = [],
+    longmonths = []
+
+for(var month = 0; month < 12; month++){
+    var d = new Date(Date.UTC(2012, month, 1, 3, 0, 0))
+    shortmonths.push(d.toLocaleDateString(locale, {month: 'short'}))
+    longmonths.push(d.toLocaleDateString(locale, {month: 'long'}))
+}
+
+var shortdays_re = new RegExp(shortdays.join("|").replace(".", "\\.")),
+    longdays_re = new RegExp(longdays.join("|")),
+    shortmonths_re = new RegExp(shortmonths.join("|").replace(".", "\\.")),
+    longmonths_re = new RegExp(longmonths.join("|"))
+
+var regexps = {
+    d: ["day", new RegExp("0[1-9]|[123][0-9]")],
+    H: ["hour", new RegExp("[01][0-9]|2[0-3]|\\d")],
+    I: ["hour", new RegExp("0[0-9]|1[0-2]")],
+    m: ["month", new RegExp("0[1-9]|1[012]")],
+    M: ["minute", new RegExp("[0-5][0-9]")],
+    S: ["second", new RegExp("([1-5]\\d)|(0?\\d)")],
+    y: ["year", new RegExp("0{0,2}\\d{2}")],
+    Y: ["year", new RegExp("\\d{4}")]
+}
+
+function _locale_c_format(){
+    var lang = __BRYTHON__.locale
+    for(var fmt in dt_formats["am_pm"]){
+        if(dt_formats["am_pm"][fmt].indexOf(lang) > -1){
+            return fmt
+        }
+    }
+    var res = [];
+    ["am", "pm"].forEach(function(item){
+        for(var fmt in dt_formats[item]){
+            if(dt_formats[item][fmt].indexOf(lang) > -1){
+                res.push(fmt)
+            }
+        }
+    });
+    return res
+}
+
+var $module=(function($B){
+    return {
+        _locale_c_format: _locale_c_format,
+        _strptime_datetime: function(cls, s, fmt){
+            var datetime
+            try{
+                datetime = $B.imported.datetime.datetime
+            }catch(err){
+            }
+
+            var pos_s = 0,
+                pos_fmt = 0,
+                dt = {}
+            function error(){
+                throw Error("no match " + pos_s + " " + s.charAt(pos_s) + " "+
+                    pos_fmt + " " + fmt.charAt(pos_fmt))
+            }
+            while(pos_fmt < fmt.length){
+                var car = fmt.charAt(pos_fmt)
+                if(car == "%"){
+                    var spec = fmt.charAt(pos_fmt + 1),
+                        regexp = regexps[spec]
+                    if(regexp !== undefined){
+                        var re = regexp[1],
+                            attr = regexp[0],
+                            res = re.exec(s.substr(pos_s))
+                        if(res === null){
+                            error()
+                        }else{
+                            if(dt[attr] !== undefined){
+                                throw Error(attr + " is defined more than once")
+                            }else{
+                                dt[attr] = parseInt(res[0])
+                                pos_fmt += 2
+                                pos_s += res[0].length
+                            }
+                        }
+                    }else if(spec == "a" || spec == "A"){
+                        // Locale's abbreviated (a) or full (A) weekday name
+                        var attr = "weekday",
+                            re = spec == "a" ? shortdays_re : longdays_re,
+                            t = spec == "a" ? shortdays : longdays
+                            res = re.exec(s.substr(pos_s))
+                        if(res === null){
+                            error()
+                        }else{
+                            var match = res[0],
+                                ix = t.indexOf(match)
+                        }
+                        if(dt.weekday !== undefined){
+                            throw Error(attr + " is defined more than once")
+                        }else{
+                            dt.weekday = ix
+                        }
+                        pos_fmt += 2
+                        pos_s += match.length
+                    }else if(spec == "b" || spec == "B"){
+                        // Locales's abbreviated (b) or full (B) month
+                        var attr = "month",
+                            re = spec == "b" ? shortmonths_re : longmonths_re,
+                            t = spec == "b" ? shortmonths : longmonths,
+                            res = re.exec(s.substr(pos_s))
+                        if(res === null){
+                            error()
+                        }else{
+                            var match = res[0],
+                                ix = t.indexOf(match)
+                        }
+                        if(dt.month !== undefined){
+                            throw Error(attr + " is defined more than once")
+                        }else{
+                            dt.month = ix + 1
+                        }
+                        pos_fmt += 2
+                        pos_s += match.length
+                    }else if(spec == "c"){
+                        // Locale's appropriate date and time representation
+                        var fmt1 = fmt.substr(0, pos_fmt - 1) + _locale_c_format() +
+                            fmt.substr(pos_fmt + 2)
+                        console.log("fmt1", fmt1)
+                        fmt = fmt1
+                    }else if(spec == "%"){
+                        if(s.charAt(pos_s) == "%"){
+                            pos_fmt++
+                            pos_s++
+                        }else{
+                            error()
+                        }
+                    }else{
+                        pos_fmt++
+                    }
+                }else{
+                    if(car == s.charAt(pos_s)){
+                        pos_fmt++
+                        pos_s++
+                    }else{
+                        error()
+                    }
+                }
+            }
+            return $B.$call(cls)(dt.year, dt.month, dt.day,
+                dt.hour || 0, dt.minute || 0, dt.second || 0)
+        }
+    }
+})(__BRYTHON__)
