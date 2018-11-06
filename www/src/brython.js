b63eb92e8a6f7620b05a5cdae189395da9d056fc
Pierre Quentel <pierre@brython.info>
2018-11-06 23:16:43 +0100

    Fixes #964 : Template .render() function not working in Edge and IE11

--- www/src/brython.js
+++ www/src/brython.js
@@ -73,8 +73,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,0,'rc',2]
 __BRYTHON__.__MAGIC__="3.7.0"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2018-11-06 11:02:33.826314"
-__BRYTHON__.timestamp=1541498553826
+__BRYTHON__.compiled_date="2018-11-06 23:15:42.614896"
+__BRYTHON__.timestamp=1541542542614
 __BRYTHON__.builtin_module_names=["_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_strptime","_svg","_sys","_warnings","array","builtins","dis","hashlib","json","long_int","marshal","math","modulefinder","posix","random","zlib"]
 
 ;(function($B){Number.isInteger=Number.isInteger ||function(value){return typeof value==='number' &&
@@ -7902,7 +7902,7 @@ JSObject.__iter__=function(self){var items=[]
 if(_window.Symbol && self.js[Symbol.iterator]!==undefined){
 if(self.js.length !==undefined && self.js.item !==undefined){for(var i=0;i < self.js.length ;i++){items.push(JSObject.$factory(self.js[i]))}}else{for(var item in self.js){if(self.js.hasOwnProperty(item)){items.push(jsobj2pyobj(item))}}}
 return $B.$iterator(items,$JSObject_iterator)}else if(self.js.length !==undefined && self.js.item !==undefined){
-self.js.forEach(function(item){items.push(JSObject.$factory(item))})
+for(var i=0;i < self.js.length;i++){items.push(JSObject.$factory(self.js.item(i)))}
 return $B.$iterator(items,$JSObject_iterator)}
 var _dict=JSObject.to_dict(self)
 return _b_.dict.__iter__(_dict)}
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 7, 0, 'rc', 2]
 __BRYTHON__.__MAGIC__ = "3.7.0"
 __BRYTHON__.version_info = [3, 7, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2018-11-06 11:02:33.826314"
-__BRYTHON__.timestamp = 1541498553826
+__BRYTHON__.compiled_date = "2018-11-06 23:15:42.614896"
+__BRYTHON__.timestamp = 1541542542614
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_base64",
     "_binascii",
