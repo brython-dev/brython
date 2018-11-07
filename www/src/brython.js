ebce5adbb1d4b01313dfa8431f57c82028ddc3f1
Pierre Quentel <pierre@brython.info>
2018-11-07 08:41:36 +0100

    Update generated scripts

--- www/src/brython.js
+++ www/src/brython.js
@@ -73,8 +73,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,0,'rc',2]
 __BRYTHON__.__MAGIC__="3.7.0"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2018-11-06 23:15:42.614896"
-__BRYTHON__.timestamp=1541542542614
+__BRYTHON__.compiled_date="2018-11-07 08:40:51.865597"
+__BRYTHON__.timestamp=1541576451865
 __BRYTHON__.builtin_module_names=["_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_strptime","_svg","_sys","_warnings","array","builtins","dis","hashlib","json","long_int","marshal","math","modulefinder","posix","random","zlib"]
 
 ;(function($B){Number.isInteger=Number.isInteger ||function(value){return typeof value==='number' &&
@@ -7609,7 +7609,7 @@ throw _b_.TypeError.$factory("endswith first arg must be bytes " +
 "or a tuple of bytes, not " + $B.get_class($.suffix).__name__)}}
 bytes.expandtabs=function(){var $=$B.args('expandtabs',2,{self: null,tabsize: null},['self','tabsize'],arguments,{tabsize: 8},null,null)
 var tab_spaces=[]
-for(var i=0;i < $.tabsize;++i)
+for(let i=0;i < $.tabsize;++i)
 tab_spaces.push(32)
 var buffer=$.self.source.slice()
 for(let i=0;i < buffer.length;++i)
