7a53855faef66bcef9ae6618975a390efb799f90
Pierre Quentel <pierre@brython.info>
2018-11-12 22:07:26 +0100

    Replace Lib/_string.py by libs/_string.js

---
+++ www/src/libs/_string.js
@@ -0,0 +1,70 @@
+var $module=(function($B){
+
+var _b_ = $B.builtins
+
+function parts(format_string){
+    var result = [],
+        _parts = $B.split_format(format_string) // defined in py_string.js
+    for(var i = 0; i < _parts.length; i+= 2){
+        result.push({pre: _parts[i], fmt: _parts[i + 1]})
+    }
+    return result
+}
+
+function Tuple(){
+    var args = []
+    for(var i=0, len=arguments.length; i < len; i++){
+        args.push(arguments[i])
+    }
+    return _b_.tuple.$factory(args)
+}
+
+return{
+
+    formatter_field_name_split: function(fieldname){
+        // Split the argument as a field name
+        var parsed = $B.parse_format(fieldname),
+            first = parsed.name,
+            rest = []
+        if(first.match(/\d+/)){first = parseInt(first)}
+        parsed.name_ext.forEach(function(ext){
+            if(ext.startsWith("[")){
+                var item = ext.substr(1, ext.length - 2)
+                if(item.match(/\d+/)){
+                    rest.push(Tuple(false, parseInt(item)))
+                }else{
+                    rest.push(Tuple(false, item))
+                }
+            }else{
+                rest.push(Tuple(true, ext.substr(1)))
+            }
+        })
+        return Tuple(first, _b_.iter(rest))
+    },
+    formatter_parser: function(format_string){
+        // Parse the argument as a format string
+
+        if(! _b_.isinstance(format_string, _b_.str)){
+            throw _b_.ValueError.$factory("Invalid format string type: " +
+                $B.get_class(format_string).__name__)
+        }
+
+        var result  = []
+        parts(format_string).forEach(function(item){
+            var pre = item.pre === undefined ? "" : item.pre,
+                fmt = item.fmt
+            if(fmt === undefined){
+               result.push(Tuple(pre, _b_.None, _b_.None, _b_.None))
+            }else if(fmt.string == ''){
+               result.push(Tuple(pre, '', '', _b_.None))
+            }else{
+               result.push(Tuple(pre,
+                   fmt.raw_name + fmt.name_ext.join(""),
+                   fmt.raw_spec,
+                   fmt.conv || _b_.None))
+           }
+        })
+        return result
+    }
+}
+})(__BRYTHON__)
