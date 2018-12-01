brython-dev/brython

commit 30a59c62d8c5a75a77180685b658555f5d0f096e

Optimise JS code for names bound to a literal integer

www/src/py2js.js
@@ -846,8 +846,17 @@ var $AssignCtx = $B.parser.$AssignCtx = function(context){
             case 'list_or_tuple':
                 left_items = left.tree
         }
-        if(left_items === null){return}
+
         var right = this.tree[1]
+        if(left_items === null){
+            if(left.tree[0].bound){
+                if(right.type == "expr" && right.name == "int"){
+                    node.bindings = node.bindings || {}
+                    node.bindings[left.tree[0].value] = "int"
+                }
+            }
+            return
+        }
 
         var right_items = null
         if(right.type == 'list' || right.type == 'tuple'||
@@ -1183,7 +1192,6 @@ var $AugmentedAssignCtx = $B.parser.$AugmentedAssignCtx = function(context, op){
     this.toString = function(){return '(augm assign) ' + this.tree}
 
     this.transform = function(node, rank){
-
         var func = '__' + $operators[op] + '__',
             offset = 0,
             parent = node.parent,
@@ -1197,6 +1205,8 @@ var $AugmentedAssignCtx = $B.parser.$AugmentedAssignCtx = function(context, op){
             this.tree[0].tree[0].type == 'id')
 
         if(left_is_id){
+            var left_bound_to_int =
+                this.tree[0].tree[0].boundBefore(this.scope) == "int"
             // Set attribute "augm_assign" of $IdCtx instance, so that
             // the id will not be resolved with $B.$check_undef()
             this.tree[0].tree[0].augm_assign = true
@@ -1293,6 +1303,11 @@ var $AugmentedAssignCtx = $B.parser.$AugmentedAssignCtx = function(context, op){
         }
 
         var left = context.tree[0].to_js()
+
+        if(left_bound_to_int && right_is_int){
+            parent.insert(rank + offset, $NodeJS(left + " "+ op + " " + right))
+            return offset++
+        }
 
         // Generate code to use Javascript operator if the object type is
         // str, int or float
@@ -3804,13 +3819,13 @@ var $IdCtx = $B.parser.$IdCtx = function(context,value){
         while(!found && node.parent && nb++ < 100){
             var pnode = node.parent
             if(pnode.bindings && pnode.bindings[this.value]){
-                return true
+                return pnode.bindings[this.value]
             }
             for(var i = 0; i < pnode.children.length; i++){
                 var child = pnode.children[i]
                 if(child === node){break}
                 if(child.bindings && child.bindings[this.value]){
-                    return true
+                    return child.bindings[this.value]
                 }
             }
             if(pnode === scope){

commit 960a4b358ce6e8be1c002415eb1c88b4e1600166

Fixes bug introduced by commit 30a59c62d8c5a75a77180685b658555f5d0f096e

www/src/brython.js
@@ -73,8 +73,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,0,'rc',2]
 __BRYTHON__.__MAGIC__="3.7.0"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2018-12-01 08:43:47.607747"
-__BRYTHON__.timestamp=1543650227607
+__BRYTHON__.compiled_date="2018-12-01 08:55:54.381976"
+__BRYTHON__.timestamp=1543650954381
 __BRYTHON__.builtin_module_names=["_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_sys","_warnings","array","builtins","dis","hashlib","json","long_int","marshal","math","modulefinder","posix","random","zlib"]
 
 ;(function($B){Number.isInteger=Number.isInteger ||function(value){return typeof value==='number' &&
@@ -673,7 +673,8 @@ parent.insert(rank + offset,new_node)
 in_class=true
 offset++}}}
 var left=C.tree[0].to_js()
-if(left_bound_to_int && right_is_int){parent.insert(rank + offset,$NodeJS(left + " "+ op + " " + right))
+if(false){
+parent.insert(rank + offset,$NodeJS(left + " "+ op + " " + right))
 return offset++}
 prefix=prefix && !C.tree[0].unknown_binding && !left_id_unbound
 var op1=op.charAt(0)

www/src/py2js.js
@@ -1304,7 +1304,7 @@ var $AugmentedAssignCtx = $B.parser.$AugmentedAssignCtx = function(context, op){
 
         var left = context.tree[0].to_js()
 
-        if(left_bound_to_int && right_is_int){
+        if(false){ //left_bound_to_int && right_is_int){
             parent.insert(rank + offset, $NodeJS(left + " "+ op + " " + right))
             return offset++
         }
@@ -3914,7 +3914,7 @@ var $IdCtx = $B.parser.$IdCtx = function(context,value){
         // Build the list of scopes where the variable name is bound
         while(1){
             if($B._globals[scope.id] !== undefined &&
-                $B._globals[scope.id][val] !== undefined){
+                    $B._globals[scope.id][val] !== undefined){
                 // Variable is declared as global. If the name is bound in the
                 // global scope, use it ; if the name is being bound, bind it
                 // in the global namespace.

www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 7, 0, 'rc', 2]
 __BRYTHON__.__MAGIC__ = "3.7.0"
 __BRYTHON__.version_info = [3, 7, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2018-12-01 08:43:47.607747"
-__BRYTHON__.timestamp = 1543650227607
+__BRYTHON__.compiled_date = "2018-12-01 08:55:54.381976"
+__BRYTHON__.timestamp = 1543650954381
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_base64",
     "_binascii",