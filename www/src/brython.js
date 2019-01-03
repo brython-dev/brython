93afc497f98d09907896d0e101e0281131ca6f8a
Pierre Quentel <pierre@brython.info>
2019-01-03 09:52:22 +0100

    Set version to 3.7.0

--- www/src/brython.js
+++ www/src/brython.js
@@ -77,8 +77,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,0,'final',0]
 __BRYTHON__.__MAGIC__="3.7.0"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2019-01-03 08:39:17.741090"
-__BRYTHON__.timestamp=1546501157741
+__BRYTHON__.compiled_date="2019-01-03 09:50:27.935495"
+__BRYTHON__.timestamp=1546505427935
 __BRYTHON__.builtin_module_names=["_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_sys","_warnings","array","builtins","dis","hashlib","json","long_int","marshal","math","modulefinder","posix","random","zlib"]
 
 ;(function($B){Number.isInteger=Number.isInteger ||function(value){return typeof value==='number' &&
