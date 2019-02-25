3cde955a87d0dbe37f701a0db03c0ffd21c84f76
Pierre Quentel <pierre@brython.info>
2019-02-25 09:02:24 +0100

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 7, 1, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.7.1"
 __BRYTHON__.version_info = [3, 7, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2019-02-24 15:58:03.118563"
-__BRYTHON__.timestamp = 1551020283118
+__BRYTHON__.compiled_date = "2019-02-25 08:57:12.843730"
+__BRYTHON__.timestamp = 1551081432843
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
