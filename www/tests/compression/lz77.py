6a0242bb0250690c3dce74d1d0db4a04f7a059eb
PierreQuentel <pierre.quentel@gmail.com>
2019-05-11 15:41:09 +0200

    Implement "lazy matching" in lz77.py

--- www/tests/compression/lz77.py
+++ www/tests/compression/lz77.py
@@ -1,11 +1,15 @@
 class LZ77:
 
     def compress(self, text, size, min_len=3):
-        buf = bytearray()
-
+        
         pos = 0
         while pos < len(text):
             sequence = text[pos:pos + min_len]
+            if len(sequence) < 3:
+                for char in text[pos:]:
+                    yield char
+                break
+            buf = text[pos - size:pos]
             buf_pos = buf.rfind(sequence)
             if buf_pos > -1:
                 length = 1
@@ -14,26 +18,32 @@ class LZ77:
                         and pos + length < len(text) \
                         and text[pos + length] == buf[buf_pos + length]:
                     length += 1
-
-                distance = len(buf) - buf_pos
-                if distance == 66:
-                    print("distance", distance, "copied",
-                        buf[buf_pos : buf_pos + length],
-                        len(buf[buf_pos : buf_pos + length]))
-                buf += text[pos:pos + length + 1]
-                yield (length, distance)
-                if pos + length == len(text):
-                    break
-                else:
-                    pos += length + 1
-                    yield text[pos - 1]
+                match = text[pos : pos + length]
+                # "lazy matching": search longer match starting at next
+                # position
+                longer_match = False
+                if pos + length < len(text) - 2:
+                    match2 = text[pos + 1 : pos + length + 2]
+                    longer_buf_pos = buf.rfind(match2)
+                    if longer_buf_pos > -1:
+                        # found longer match : emit current byte as literal 
+                        # and move 1 byte forward
+                        longer_match = True
+                        char = text[pos]
+                        yield char
+                        pos += 1
+                if not longer_match:
+                    distance = len(buf) - buf_pos
+                    yield (length, distance)
+                    if pos + length == len(text):
+                        break
+                    else:
+                        pos += length
+                        yield text[pos]
             else:
                 char = text[pos]
                 yield char
-                buf.append(char)
                 pos += 1
-            if len(buf) > size:
-                buf = buf[len(buf) - size:]
 
     def decompress(self, data):
         res = bytearray()
