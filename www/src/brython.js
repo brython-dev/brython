f68067f98400aa8b1b73d42b11e152f584a5b304
Pierre Quentel <pierre@brython.info>
2019-06-11 12:46:59 +0200

    Fixes #1134 : struct format does not support spaces

--- www/src/brython.js
+++ www/src/brython.js
@@ -85,8 +85,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,4,'dev',0]
 __BRYTHON__.__MAGIC__="3.7.4"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2019-06-10 19:44:46.228474"
-__BRYTHON__.timestamp=1560188686228
+__BRYTHON__.compiled_date="2019-06-11 12:43:19.851417"
+__BRYTHON__.timestamp=1560249799851
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webworker","_zlib","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","modulefinder","posix","random","unicodedata"]
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 7, 4, 'dev', 0]
 __BRYTHON__.__MAGIC__ = "3.7.4"
 __BRYTHON__.version_info = [3, 7, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2019-06-10 19:44:46.228474"
-__BRYTHON__.timestamp = 1560188686228
+__BRYTHON__.compiled_date = "2019-06-11 12:43:19.851417"
+__BRYTHON__.timestamp = 1560249799851
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
