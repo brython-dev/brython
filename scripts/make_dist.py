184e45208c806427f8f19acaadfe67a9780de4e6
Pierre Quentel <pierre@brython.info>
2019-07-11 14:52:15 +0200

    Set version to 3.7.4

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -21,7 +21,7 @@ if sys.version_info[0] != 3:
 pdir = os.path.dirname(os.getcwd())
 # version info
 version = [3, 7, 0, "final", 0]
-implementation = [3, 7, 4, "dev", 0]
+implementation = [3, 7, 4, "final", 0]
 
 # version name
 vname = '.'.join(str(x) for x in implementation[:3])
