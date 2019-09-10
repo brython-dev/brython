5e7b1d5f13ad65312d62eb6d1d219f56532f6d84
Pierre Quentel <pierre@brython.info>
2019-09-10 08:09:04 +0200

    Update generated scripts

--- www/src/brython.js
+++ www/src/brython.js
@@ -92,8 +92,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,5,'final',0]
 __BRYTHON__.__MAGIC__="3.7.5"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2019-09-09 13:48:16.851626"
-__BRYTHON__.timestamp=1568029696851
+__BRYTHON__.compiled_date="2019-09-09 22:47:03.265910"
+__BRYTHON__.timestamp=1568062023265
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","modulefinder","posix","random","unicodedata"]
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 7, 5, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.7.5"
 __BRYTHON__.version_info = [3, 7, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2019-09-09 13:48:16.851626"
-__BRYTHON__.timestamp = 1568029696851
+__BRYTHON__.compiled_date = "2019-09-09 22:47:03.265910"
+__BRYTHON__.timestamp = 1568062023265
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
