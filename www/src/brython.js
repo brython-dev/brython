28f275f618173530b5e6908c73b8e16e5e3f9567
Pierre Quentel <pierre@brython.info>
2019-10-10 18:58:29 +0200

    Update browser/__init__.py

--- www/src/brython.js
+++ www/src/brython.js
@@ -91,8 +91,8 @@ $B.regexIdentifier=/^(?:[\$A-Z_a-z\xAA\xB5\xBA\xC0-\xD6\xD8-\xF6\xF8-\u02C1\u02C
 __BRYTHON__.implementation=[3,7,6,'dev',0]
 __BRYTHON__.__MAGIC__="3.7.6"
 __BRYTHON__.version_info=[3,7,0,'final',0]
-__BRYTHON__.compiled_date="2019-10-10 18:09:37.135197"
-__BRYTHON__.timestamp=1570723777135
+__BRYTHON__.compiled_date="2019-10-10 18:54:41.748668"
+__BRYTHON__.timestamp=1570726481748
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","modulefinder","posix","random","unicodedata"]
 ;
 
