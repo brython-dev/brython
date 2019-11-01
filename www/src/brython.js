cec3eecdf096a4daa512ab45d24afd543fc8b22c
PierreQuentel <quentel.pierre@orange.fr>
2019-11-01 16:05:21 +0100

    Update generated scripts after merge

--- www/src/brython.js
+++ www/src/brython.js
@@ -90,8 +90,8 @@ return js}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,8,0,'dev',0]
 __BRYTHON__.__MAGIC__="3.8.0"
 __BRYTHON__.version_info=[3,8,0,'final',0]
-__BRYTHON__.compiled_date="2019-11-01 16:00:21.807600"
-__BRYTHON__.timestamp=1572620421807
+__BRYTHON__.compiled_date="2019-11-01 16:04:42.549701"
+__BRYTHON__.timestamp=1572620682549
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","modulefinder","posix","random","unicodedata"]
 ;
 
