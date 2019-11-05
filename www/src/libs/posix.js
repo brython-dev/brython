b716b4f13d165a70432260b99e3e0a5255c5177d
PierreQuentel <quentel.pierre@orange.fr>
2019-11-05 14:38:43 +0100

    Use $B.files in built-in module posix.js

--- www/src/libs/posix.js
+++ www/src/libs/posix.js
@@ -13,21 +13,29 @@ function _randint(a, b){
 }
 
 var stat_result = $B.make_class("stat_result",
-    function(){
-        var res = {
-            __class__: stat_result,
-            st_atime: new Date(),
-            st_uid: -1,
-            st_gid: -1,
-            st_ino: -1,
-            st_mode: 0,
-            st_size: 1
-        };
-        ["mtime", "ctime", "atime_ns", "mtime_ns", "ctime_ns"].
-            forEach(function(item){
-                res["st_" + item] = res.st_atime
-            });
-        return res
+    function(filename){
+        // Use $B.files, created by "python -m brython --make_file_system"
+        if($B.files && $B.files.hasOwnProperty(filename)){
+            var f = $B.files[filename],
+                res = {
+                    __class__: stat_result,
+                    st_atime: new Date().getTime(),
+                    st_ctime: f.ctime,
+                    st_mtime: f.mtime,
+                    st_uid: -1,
+                    st_gid: -1,
+                    st_ino: -1,
+                    st_mode: 0,
+                    st_size: 1
+                };
+            ["atime", "mtime", "ctime"].
+                forEach(function(item){
+                    res["st_" + item + "_ns"] = res["st_" + item] *
+                        1000000
+                });
+            return res
+        }
+        throw _b_.FileNotFoundError.$factory(filename)
     }
 )
 $B.set_func_names(stat_result, "posix")
@@ -66,8 +74,12 @@ var $module = {
     getpid: function(){return 0},
     lstat: function(){return stat_result.$factory()},
     open: function(path, flags){return _b_.open(path, flags)},
-    stat: function(){return stat_result.$factory()},
-    stat_result: function(){return stat_result.$factory()},
+    stat: function(filename){
+        return stat_result.$factory(filename)
+    },
+    stat_result: function(filename){
+        return stat_result.$factory(filename)
+    },
     urandom: function(n){
         var randbytes = []
         for(var i = 0; i < n; i++){
