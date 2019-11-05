ea73ca53c6faa86e1b573ab883b1d70d56b98071
PierreQuentel <quentel.pierre@orange.fr>
2019-11-05 14:35:59 +0100

    More documentation in make_file_system.py

--- setup/brython/make_file_system.py
+++ setup/brython/make_file_system.py
@@ -5,12 +5,26 @@ import sys
 import binascii
 
 def make(vfs_name, prefix=None):
-    """Create a Virtual File System : a Javascript file with the files in
+    """Called by
+
+        python -m brython --make_file_system <vfs_name> <prefix>
+
+    Creates a Virtual File System : a Javascript file with the files in
     current directory and its children.
+
     The file is stored in current directory as "<vfs_name>.vfs.js".
-    A dictionary files is created. Keys are the file names, relative to the
-    current directory (ie a file "data.txt" in subdirectory "subdir" has
-    the key "subdir/data.txt"
+
+    A dictionary "files" is created. Keys are the file names, relative to the
+    current directory (ie a file "data.txt" in current directory has the key
+    "data.txt", and a file "names.txt" in the subdirectory "address" has key
+    "address/names.txt").
+
+    If <prefix> was specified, it is prepended to the keys, followed by a /.
+    For instance, if prefix is "info", the files above will have keys
+    "info/data.txt" and "info/adress/names.txt"
+
+    Python files can be included in such files, but *programs will not be
+    able to import them*; for this, use --modules instead.
     """
 
     files = {}
@@ -27,7 +41,6 @@ def make(vfs_name, prefix=None):
             path = dirpath[len(this_dir) + len(os.sep):].split(os.sep)
         for filename in filenames:
             if filename.endswith(".vfs.js"):
-                # avoid recursion
                 continue
             rel_path = "/".join(virtual_dir + path + [filename])
             with open(os.path.join(dirpath, filename), "rb") as f:
