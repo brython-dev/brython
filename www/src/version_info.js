e2ae92a9bf0e74a8a2cb9d415cf124cb724aa831
PierreQuentel <quentel.pierre@orange.fr>
2019-11-06 17:47:15 +0100

    Add stdlib module pkgutil.py

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 8, 1, 'dev', 0]
 __BRYTHON__.__MAGIC__ = "3.8.1"
 __BRYTHON__.version_info = [3, 8, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2019-11-06 13:27:02.750687"
-__BRYTHON__.timestamp = 1573043222750
+__BRYTHON__.compiled_date = "2019-11-06 17:46:19.550762"
+__BRYTHON__.timestamp = 1573058779550
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
