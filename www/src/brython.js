ce7c62a8ac669b80806420bfefbf65b04235903a
PierreQuentel <quentel.pierre@orange.fr>
2019-11-14 08:40:21 +0100

    Restrict "_operator" module to a single function _compare_digest, directly used by the "hmac" module. Other functions must not override those in module "operator"

--- www/src/brython.js
+++ www/src/brython.js
@@ -95,8 +95,8 @@ return js}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,8,1,'dev',0]
 __BRYTHON__.__MAGIC__="3.8.1"
 __BRYTHON__.version_info=[3,8,0,'final',0]
-__BRYTHON__.compiled_date="2019-11-13 22:27:26.282290"
-__BRYTHON__.timestamp=1573680446266
+__BRYTHON__.compiled_date="2019-11-14 08:37:24.108247"
+__BRYTHON__.timestamp=1573717044108
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","modulefinder","posix","random","unicodedata"]
 ;
 
