410b7decf38abadd394d069960d0ff1dffcbea54
PierreQuentel <quentel.pierre@orange.fr>
2019-12-05 21:46:05 +0100

    Change version to 3.8.1

--- setup/setup.py
+++ setup/setup.py
@@ -89,7 +89,7 @@ if command == "sdist":
 setup(
     name='brython',
 
-    version='3.8.0',
+    version='3.8.1',
     description='Brython is an implementation of Python 3 running in the browser',
 
     long_description=LONG_DESCRIPTION,
