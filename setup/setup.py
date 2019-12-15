5c9ab3d35ad04e4b048cd65a0704ca79469bf937
PierreQuentel <quentel.pierre@orange.fr>
2019-12-15 09:36:02 +0100

    Change site colors. For Brython logo, replace image by text in Libra_bt font

--- setup/setup.py
+++ setup/setup.py
@@ -111,7 +111,7 @@ if command == "sdist":
 setup(
     name='brython',
 
-    version='3.8.1',
+    version='3.8.4',
     description='Brython is an implementation of Python 3 running in the browser',
 
     long_description=LONG_DESCRIPTION,
