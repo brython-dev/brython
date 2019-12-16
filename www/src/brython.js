c82af7cec2dded4cac90ffea7e806bcd92298898
PierreQuentel <quentel.pierre@orange.fr>
2019-12-16 11:12:02 +0100

    Update generated scripts

--- www/src/brython.js
+++ www/src/brython.js
@@ -99,8 +99,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,8,5,'dev',0]
 __BRYTHON__.__MAGIC__="3.8.5"
 __BRYTHON__.version_info=[3,8,0,'final',0]
-__BRYTHON__.compiled_date="2019-12-14 18:51:17.367765"
-__BRYTHON__.timestamp=1576345877367
+__BRYTHON__.compiled_date="2019-12-16 08:59:10.018216"
+__BRYTHON__.timestamp=1576483150018
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","math1","math_kozh","modulefinder","posix","random","unicodedata"]
 ;
 
