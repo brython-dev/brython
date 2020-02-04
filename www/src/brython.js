abb137abdefdaa359466f8785f49546800558662
PierreQuentel <quentel.pierre@orange.fr>
2020-02-04 12:26:10 +0100

    In index.html, use time instead of datetime, to avoid importing datetime and the dozens of associated modules

--- www/src/brython.js
+++ www/src/brython.js
@@ -99,8 +99,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,8,6,'final',0]
 __BRYTHON__.__MAGIC__="3.8.6"
 __BRYTHON__.version_info=[3,8,0,'final',0]
-__BRYTHON__.compiled_date="2020-02-04 10:57:06.014358"
-__BRYTHON__.timestamp=1580810226014
+__BRYTHON__.compiled_date="2020-02-04 12:24:38.464694"
+__BRYTHON__.timestamp=1580815478464
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_io_classes","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_warnings","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","hashlib","long_int","marshal","math","math1","math_kozh","modulefinder","posix","random","unicodedata"]
 ;
 
