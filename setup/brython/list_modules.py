4c0c5945aec8c8cb146dfeafc0240af353026447
PierreQuentel <quentel.pierre@orange.fr>
2020-02-20 15:25:50 +0100

    Remove debug trace in list_modules.py

--- setup/brython/list_modules.py
+++ setup/brython/list_modules.py
@@ -302,7 +302,6 @@ class ModulesFinder:
         """Build brython_modules.js from the list of modules needed by the
         application.
         """
-        print("modules", self.modules, "http" in self.modules)
         vfs = {"$timestamp": int(1000 * time.time())}
         for module in self.modules:
             dico = stdlib if module in stdlib else user_modules
