7db2347b9761851219476d4c094983e8f258ed6b
PierreQuentel <quentel.pierre@orange.fr>
2020-02-21 21:33:36 +0100

    Fixes #1282 : saving a lot of data to local object storage freezes everything

--- www/src/Lib/browser/object_storage.py
+++ www/src/Lib/browser/object_storage.py
@@ -1,4 +1,4 @@
-import json
+from javascript import JSON
 
 class _UnProvided():
     pass
@@ -10,25 +10,25 @@ class ObjectStorage():
         self.storage = storage
 
     def __delitem__(self, key):
-        del self.storage[json.dumps(key)]
+        del self.storage[JSON.stringify(key)]
 
     def __getitem__(self, key):
-        return json.loads(self.storage[json.dumps(key)])
+        return JSON.parse(self.storage[JSON.stringify(key)])
 
     def __setitem__(self, key, value):
-        self.storage[json.dumps(key)] = json.dumps(value)
+        self.storage[JSON.stringify(key)] = JSON.stringify(value)
 
     def __contains__(self, key):
-        return json.dumps(key) in self.storage
+        return JSON.stringify(key) in self.storage
 
     def get(self, key, default=None):
-        if json.dumps(key) in self.storage:
-            return self.storage[json.dumps(key)]
+        if JSON.stringify(key) in self.storage:
+            return self.storage[JSON.stringify(key)]
         return default
 
     def pop(self, key, default=_UnProvided()):
-        if type(default) is _UnProvided or json.dumps(key) in self.storage:
-            return json.loads(self.storage.pop(json.dumps(key)))
+        if type(default) is _UnProvided or JSON.stringify(key) in self.storage:
+            return JSON.parse(self.storage.pop(JSON.stringify(key)))
         return default
 
     def __iter__(self):
@@ -36,10 +36,10 @@ class ObjectStorage():
         return keys.__iter__()
 
     def keys(self):
-        return [json.loads(key) for key in self.storage.keys()]
+        return [JSON.parse(key) for key in self.storage.keys()]
 
     def values(self):
-        return [json.loads(val) for val in self.storage.values()]
+        return [JSON.parse(val) for val in self.storage.values()]
 
     def items(self):
         return list(zip(self.keys(), self.values()))
