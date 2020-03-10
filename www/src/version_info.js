e41d15cef6733f05cfc3150ce814339fa9d8d407
PierreQuentel <quentel.pierre@orange.fr>
2020-03-10 11:32:24 +0100

    Update generated scripts after merge

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,13 +1,8 @@
 __BRYTHON__.implementation = [3, 8, 8, 'dev', 0]
 __BRYTHON__.__MAGIC__ = "3.8.8"
 __BRYTHON__.version_info = [3, 8, 0, 'final', 0]
-<<<<<<< HEAD
-__BRYTHON__.compiled_date = "2020-03-10 11:26:07.074861"
-__BRYTHON__.timestamp = 1583835967059
-=======
-__BRYTHON__.compiled_date = "2020-03-09 11:34:51.403166"
-__BRYTHON__.timestamp = 1583750091402
->>>>>>> 5ad4b09fbccd03ef3dbdaf54387607bbf6481b02
+__BRYTHON__.compiled_date = "2020-03-10 11:31:49.751939"
+__BRYTHON__.timestamp = 1583836309751
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
@@ -33,6 +28,8 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "long_int",
     "marshal",
     "math",
+    "math1",
+    "math_kozh",
     "modulefinder",
     "posix",
     "random",
