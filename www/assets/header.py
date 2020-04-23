c84c47309b519eb95e191c1380bd7d219ebccd1e
PierreQuentel <quentel.pierre@orange.fr>
2020-04-23 17:29:26 +0200

    header.py : redirect http://brython.info to https; less links in the header bar with submenus for better rendering on smartphones; add language selection to all pages. Related to issue #1360

--- www/assets/header.py
+++ www/assets/header.py
@@ -1,5 +1,17 @@
-from browser import window, document as doc
-from browser.html import *
+from browser import bind, console, html, window, document, alert
+import browser.widgets.menu as menu
+
+href = document.location.href
+protocol, rest = href.split("://")
+host, addr = rest.split("/", 1)
+
+# automatic redirection from http to https for brython.info
+if protocol == "http" and host.endswith("brython.info"):
+    document.location.href = f"https://{rest}"
+
+Menu = menu.Menu
+menu.css["menu-item-top"] = "menu-item-top"
+menu.css["menu-item-sub"] = "menu-item-sub"
 
 trans_menu = {
     "menu_console": {"en": "Console", "es": "Consola", "fr": "Console"},
@@ -9,20 +21,23 @@ trans_menu = {
     "menu_doc": {"en": "Documentation", "es": "Documentación", "fr": "Documentation"},
     "menu_download": {"en": "Download", "es": "Descargas", "fr": "Téléchargement"},
     "menu_dev": {"en": "Development", "es": "Desarrollo", "fr": "Développement"},
+    "menu_ex": {"en": "Examples", "es": "Ejemplos", "fr": "Exemples"},
     "menu_groups": {"en": "Community", "es": "Comunidad", "fr": "Communauté"},
+    "menu_ref": {"en": "Reference", "es": "Referencia", "fr": "Référence"},
+    "menu_resources": {"en": "Resources", "es": "Recursos", "fr": "Ressources"},
     "menu_tutorial": {"en": "Tutorial", "es": "Tutorial", "fr": "Tutoriel"}
 }
 links = {
-    "home": "index.html",
-    "console": "tests/console.html",
-    "demo": "demo.html",
-    "editor": "tests/editor.html",
-    "gallery": "gallery/gallery_{language}.html",
-    "doc": "static_doc/{language}/intro.html",
+    "home": "/index.html",
+    "console": "/tests/console.html",
+    "demo": "/demo.html",
+    "editor": "/tests/editor.html",
+    "gallery": "/gallery/gallery_{language}.html",
+    "ref": "/static_doc/{language}/intro.html",
     "download": "https://github.com/brython-dev/brython/releases",
     "dev": "https://github.com/brython-dev/brython",
-    "groups": "groups.html",
-    "tutorial": "static_tutorial/{language}/index.html"
+    "groups": "/groups.html",
+    "tutorial": "/static_tutorial/{language}/index.html"
 }
 
 def show(language=None):
@@ -34,7 +49,7 @@ def show(language=None):
     prefix = "/"
 
     if language is None:
-        qs_lang = doc.query.getfirst("lang") # query string
+        qs_lang = document.query.getfirst("lang") # query string
         if qs_lang and qs_lang in ["en", "fr", "es"]:
             has_req = True
             language = qs_lang
@@ -45,7 +60,7 @@ def show(language=None):
 
     language = language or "en"
 
-    _banner = doc["banner_row"]
+    _banner = document["banner_row"]
 
     loc = window.location.href
     current = None
@@ -60,29 +75,73 @@ def show(language=None):
         elif "static_doc" in loc:
             current = "doc"
 
-    for key in ["tutorial", "demo", "doc", "console", "editor", "gallery",
-            "download", "dev", "groups"]:
-        if key in ["download","dev"]:
-            href = links[key]
-        else:
-            href = prefix + links[key]
-        if key in ["doc", "tutorial", "gallery"]:
-            href = href.format(language=language)
-        if key not in ["download", "dev"]:
-            # add lang to href
-            href += f"?lang={language}"
-        if key == "home":
-            img = IMG(src="/brython.svg", Class="logo")
-            link = A(img, href=href)
-            cell = TD(link, Class="logo")
-        else:
-            link = A(trans_menu[f"menu_{key}"][language], href=href,
-                Class="banner")
-            cell = TD(link)
-            if key == current:
-                link.classList.add("selected_header")
-        if key in ["download", "dev"]:
-            link.attrs["target"] = "_blank"
-        _banner <= cell
+    def load_page(key):
+        def f(e):
+            href = links[key].format(language=language)
+            window.location.href = href + f"?lang={language}"
+        return f
+
+    menu = Menu(_banner)
+
+    home = menu.add_item("brython", callback=load_page("home"))
+    home.attrs["class"] = "logo"
+
+    menu.add_item(trans_menu["menu_console"][language],
+        callback=load_page("console"))
+
+    menu.add_item(trans_menu["menu_editor"][language],
+        callback=load_page("editor"))
+
+    docs_menu = menu.add_menu(trans_menu["menu_doc"][language])
+    docs_menu.add_item(trans_menu["menu_tutorial"][language],
+        callback=load_page("tutorial"))
+    docs_menu.add_item(trans_menu["menu_ref"][language],
+        callback=load_page("ref"))
+
+    ex_menu = menu.add_menu(trans_menu["menu_ex"][language])
+    ex_menu.add_item(trans_menu["menu_demo"][language],
+        callback=load_page("demo"))
+    ex_menu.add_item(trans_menu["menu_gallery"][language],
+        callback=load_page("gallery"))
+
+    ex_resources = menu.add_menu(trans_menu["menu_resources"][language])
+    ex_resources.add_item(trans_menu["menu_download"][language],
+        callback=load_page("download"))
+    ex_resources.add_item(trans_menu["menu_dev"][language],
+        callback=load_page("dev"))
+    ex_resources.add_item(trans_menu["menu_groups"][language],
+        callback=load_page("groups"))
+
+    # insert language selection menu
+    sel_lang = html.DIV(Class="sel_lang")
+
+    document.body.insertBefore(sel_lang, _banner.nextElementSibling)
+    select = html.SELECT(Class="language")
+    sel_lang <= select
+    for lang1, lang2 in [["en", "English"],
+                         ["fr", "Français"],
+                         ["es", "Español"]]:
+        select <= html.OPTION(lang2, value=lang1, selected=lang1==language)
+
+    @bind(select, "change")
+    # If user changes the language in the select box, reload the page.
+    def change_language(ev):
+        sel = ev.target
+        new_lang = sel.options[sel.selectedIndex].value
+        head = f"{protocol}://{host}"
+        new_href = href
+        if addr.startswith("index.html"):
+            new_href = f"{head}/index.html?lang={new_lang}"
+        elif addr.startswith(("static_tutorial", "static_doc")):
+            elts = addr.split("/")
+            page = elts[-1].split("?")[0]
+            elts[1] = new_lang
+            new_href = f"{head}/{elts[0]}/{new_lang}/{page}"
+        elif addr.startswith(("demo.html",
+                              "tests/console.html",
+                              "tests/editor.html")):
+            elts = addr.split("?")
+            new_href = f"{head}/{elts[0]}?lang={new_lang}"
+        document.location.href = new_href
 
     return qs_lang, language
