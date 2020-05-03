d12b99c98544a285eab431e3a8944fef8e3b99fd
PierreQuentel <quentel.pierre@orange.fr>
2020-05-03 09:41:53 +0200

    Change CSS management in browser.widgets.menu

--- www/src/Lib/browser/widgets/menu.py
+++ www/src/Lib/browser/widgets/menu.py
@@ -1,58 +1,89 @@
-from browser import console, document, html, window
+from browser import console, document, html, window, alert
 
-color = "CadetBlue"
+style_sheet = """
+/* Classes for brython.widgets.menu */
 
-st1 = {
-    "listStyleType": "none",
-    "margin": 0,
-    "padding": 0,
-    "font-family": "sans-serif"
+:root {
+  --brython-menu-color: CadetBlue;
+  --brython-menu-color-selected: SkyBlue;
+  --brython-menu-submenu-background-color: #fff;
+  --brython-menu-font-family: Arial;
 }
 
-table = {
-    "border-spacing": 0
+/* Item in the main horizontal navigation bar */
+.brython-menu-navbar-item {
+    font-family: var(--brython-menu-font-family);
+    background-color: var(--brython-menu-color);
+    color: #fff;
+    padding: 0.5em 1em 0.5em 1em;
+    cursor: default;
 }
 
-li_style1 = {
-    "backgroundColor": color,
-    "color": "#fff",
-    "padding": "0.5em 1em 0.5em 1em",
-    "cursor": "default"
+.brython-menu-navbar-item:hover {
+    background-color: var(--brython-menu-color-selected);
 }
 
-li_style2 = {
-    "backgroundColor": "#fff",
-    "color": color,
-    "padding": "0.25em 1em 0.25em 1em",
-    "cursor": "default"
+.brython-menu-navbar-item-selected {
+    background-color: var(--brython-menu-color-selected);
 }
 
-div_style = {
-    "position": "absolute",
-    "borderStyle": "solid",
-    "borderWidth": "1px",
-    "borderColor": color
+/* Table for a submenu, opened by a click on an item */
+.brython-menu-submenu {
+    font-family: var(--brython-menu-font-family);
+    background-color: var(--brython-menu-submenu-background-color);
+    position: absolute;
+    border-style: solid;
+    border-width: 1px;
+    border-color: var(--brython-menu-color);
+    border-spacing: 0;
 }
 
-css = {}
+/* TR for a submenu item row */
+.brython-menu-submenu-row:hover {
+    color: #fff;
+    background-color: var(--brython-menu-color-selected);
+}
+
+.brython-menu-submenu-row-selected {
+    color: #fff;
+    background-color: var(--brython-menu-color-selected);
+}
+
+/*
+   TD for a cell in a submenu row
+   Each row has two cells, one for the item label, the other one
+   filled with a > if the item has a submenu
+*/
+.brython-menu-submenu-item {
+    font-family: var(--brython-menu-font-family);
+    padding: 0.3em 0.3em 0.3em 1em;
+    cursor: default;
+}
+"""
+
 
 class Menu:
 
-    def __init__(self, container=document.body, parent=None, level=0):
+    def __init__(self, container=document.body, parent=None, default_css=True):
         """Create a new menu, inserted inside the container. For the top level
-        menu, parent is None, otherwise it is a SPAN item."""
+        menu, parent is None, otherwise it is a SPAN element (top menu) or a
+        TR element (submenu)."""
+        if default_css:
+            for stylesheet in document.styleSheets:
+                if stylesheet.ownerNode.id == "brython-menu":
+                    break
+            else:
+                document <= html.STYLE(style_sheet, id="brython-menu")
+
+        self.default_css = default_css
         self.container = container
-        self.level = level
         self.parent = parent
-        self.submenus = []
 
         if parent:
-            self.submenu = html.DIV(Class="sub")
-            if "menu-submenu" in css:
-                self.submenu.classList.add(css["menu-submenu"])
-            self.submenu.style.position = "absolute"
-            self.submenu.style.display = "none"
-            self.container <= self.submenu
+            parent.submenu = html.TABLE(Class="brython-menu-submenu")
+            parent.submenu.style.position = "absolute"
+            parent.submenu.style.display = "none"
+            self.container <= parent.submenu
 
             parent.bind("click", self.unfold)
 
@@ -60,20 +91,20 @@ class Menu:
             document.bind("click", self.hide_menus)
             self.container.bind_document = True
 
-    def add_item(self, name, callback=None):
+    def add_item(self, name, menu=False, callback=None):
         if self.parent is None:
             # First level
-            item = html.SPAN(name)
-            if "menu-item-top" in css:
-                item.attrs["class"] = css["menu-item-top"]
+            item = html.SPAN(name, Class="brython-menu-navbar-item")
             self.container <= item
             item.bind("click", self.hide_menus)
         else:
             # Next levels
-            item = html.DIV(name)
-            self.submenu <= item
-            if "menu-item-sub" in css:
-                item.attrs["class"] = css["menu-item-sub"]
+            item = html.TR(Class="brython-menu-submenu-row")
+            self.parent.submenu <= item
+            item <= html.TD(name, Class="brython-menu-submenu-item")
+            item <= html.TD(">" if menu else "&nbsp;",
+                Class="brython-menu-submenu-item",
+                paddingLeft="2em")
 
         if callback is not None:
             item.bind("click", callback)
@@ -82,45 +113,85 @@ class Menu:
 
     def add_menu(self, name):
         """Add a new submenu in the current menu."""
-        if self.parent is not None:
-            name += "..."
-        item = self.add_item(name)
-        # create a DIV for the submenu
-        span = html.SPAN(Class="sub")
-        span.style.position = "absolute"
-        if "submenu" in css:
-            span.classList.add(css["submenu"])
+        # add an item
+        item = self.add_item(name, menu=True)
 
         if self.parent is None:
-            self.container <= span
+            # create a SPAN for the submenu
+            span = html.SPAN(Class="brython-menu-submenu")
+            span.style.position = "absolute"
 
-        submenu = Menu(self.container, item, self.level + 1)
-        self.submenus.append(submenu)
-        submenu.parent_menu = self
-        return submenu
+        return Menu(self.container, item, default_css=self.default_css)
 
     def hide_menus(self, *args):
         """When user clicks outside of open submenus, close them all."""
-        for div in document.select(".sub"):
-            div.style.display = "none"
+        for div in document.select(".brython-menu-submenu"):
+            if div.style.display != "none":
+                div.style.display = "none"
+
+    def hide_submenus(self, table):
+        """Hide all submenus of specified table."""
+        for row in table.select("TR"):
+            if hasattr(row, "submenu"):
+                row.submenu.style.display = "none"
+                self.hide_submenus(row.submenu)
 
     def unfold(self, ev):
         """Called when a label with a submenu is clicked."""
-        parent = self.parent
+        target = ev.target
+        if target.nodeName == "SPAN":
+            # click on a navbar item
+            selected = document.select(".brython-menu-navbar-item-selected")
 
-        if self.level == 1:
-            # Click on an item in the main menu bar
-            self.submenu.style.left = f"{parent.abs_left}px"
+            if selected:
+                self.hide_menus()
+
+            for item in selected:
+                item.classList.remove("brython-menu-navbar-item-selected")
+
+            submenu = target.submenu
+
+            target.classList.add("brython-menu-navbar-item-selected")
+            submenu.style.left = f"{target.abs_left}px"
+            submenu.style.top = f"{target.abs_top + target.offsetHeight}px"
+
+            # Once an item has been selected, mouseenter on the other items
+            # unfolds them
+            if not selected:
+                for item in document.select(".brython-menu-navbar-item"):
+                    item.bind("mouseenter", self.unfold)
+
+            # Display menu
+            submenu.style.display = "block"
+
         else:
-            # remove other submenus of parent
-            for menu in self.parent_menu.submenus:
-                menu.submenu.style.display = "none"
-            # set top and left
-            self.submenu.style.top = f"{parent.abs_top}px"
-            self.submenu.style.left = f"{parent.abs_left + parent.offsetWidth}px"
+            target = target.closest("TR")
 
-        # display menu
-        self.submenu.style.display = "block"
+            # Remove other submenus
+            table = target.closest("TABLE")
+            self.hide_submenus(table)
+
+            # If another item in the table was selected, unselect it
+            selected = table.select(".brython-menu-submenu-row-selected")
+            for row in selected:
+                row.classList.remove("brython-menu-submenu-row-selected")
+
+            # Mark target as selected
+            target.classList.add("brython-menu-submenu-row-selected")
+
+            if hasattr(target, "submenu"):
+                # Set top and left of submenu and display it
+                target.submenu.style.top = f"{target.abs_top}px"
+                target.submenu.style.left = \
+                    f"{target.abs_left + target.offsetWidth}px"
+                target.submenu.style.display = "block"
+
+            if not selected:
+                # Once an item has been selected, mouseenter on the other
+                # items unfolds them
+                for row in table.select("TR"):
+                    row.bind("mouseenter", self.unfold)
+
 
         # stop propagation, otherwise "click" is triggered on document,
         # which removes all menus...
