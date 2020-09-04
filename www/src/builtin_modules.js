7d1b08fa447189cfe1c9bf3ee29a749139a9cb11
PierreQuentel <quentel.pierre@orange.fr>
2020-09-04 21:41:24 +0200

    Experimental - add decorator javascript.extends (related to issue #1439)

--- www/src/builtin_modules.js
+++ www/src/builtin_modules.js
@@ -369,6 +369,58 @@
             return $B.JSObj.$factory($B.js_this)
         },
         $$Date: self.Date && $B.JSObj.$factory(self.Date),
+        $$extends: function(js_constr){
+            return function(obj){
+                if(typeof obj == "function"){
+                    var res = function(){
+                        js_constr.call(this, ...arguments)
+                        obj.apply(this, arguments)
+                    }
+                    res.prototype = Object.create(js_constr.prototype)
+                    res.prototype.constructor = res
+                    res.$is_js_func = true
+                    return res
+                }else if(obj.$is_class){
+                    console.log("obj", obj)
+                    if(js_constr.$js_func.name == "Named"){
+                        console.log("-- Named")
+                    }
+                    var res = function(){
+                        console.log("call parent class", obj.$parent_class)
+                        obj.$parent_class.call(this, ...arguments)
+                        if(obj.$$constructor){
+                            var args = [this]
+                            for(var i = 0, len = arguments.length; i < len; i++){
+                                args.push(arguments[i])
+                            }
+                            obj.$$constructor.apply(this, args)
+                        }
+                    }
+                    res.prototype = Object.create(js_constr.prototype)
+                    res.prototype.constructor = res
+                    res.$is_js_func = true
+                    res.$class = obj
+                    obj.$parent_class = js_constr
+                    for(var attr in obj.__dict__.$string_dict){
+                        var value = obj.__dict__.$string_dict[attr][0]
+                        if(typeof value == "function"){
+                            res.prototype[attr] = (function(x){
+                                return function(){
+                                    var args = [this]
+                                    for(var i = 0, len = arguments.length; i < len; i++){
+                                        args.push($B.pyobj2jsobj(arguments[i]))
+                                    }
+                                    return x.apply(this, args)
+                                }
+                            })(value)
+                        }else{
+                            res.prototype[attr] = $B.pyobj2jsobj(value)
+                        }
+                    }
+                    return res
+                }
+            }
+        },
         JSConstructor: {
             __get__: function(){
                 console.warn('"javascript.JSConstructor" is deprecrated. ' +
