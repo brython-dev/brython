13ad2cb11827935bdd4e0cee3a8444bb0fa78735
PierreQuentel <quentel.pierre@orange.fr>
2020-11-04 16:52:37 +0100

    Remove py_import_hooks.js (now included inside py_import.js)

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -64,7 +64,7 @@ def run():
         'stdlib_paths', 'py_import', 'py_float', 'py_int', 'py_long_int',
         'py_complex', 'py_sort', 'py_list', 'unicode_data', 'py_string',
         'py_dict', 'py_dom', 'py_generator', 'builtin_modules',
-        'builtins_docstrings', 'py_import_hooks', 'async'
+        'builtins_docstrings', 'async'
     ]
 
     res = """// brython.js brython.info
