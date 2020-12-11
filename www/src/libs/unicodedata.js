c8384d87f2dbb3802ed319b4e84fd6779c8a7149
PierreQuentel <quentel.pierre@orange.fr>
2020-12-11 14:51:52 +0100

    Adapt unicodedata.js to support strings with surrogate pairs

--- www/src/libs/unicodedata.js
+++ www/src/libs/unicodedata.js
@@ -23,7 +23,7 @@ var $module = (function($B){
     }
 
     function _info(chr){
-        var ord = chr.codePointAt(0),
+        var ord = _b_.ord(chr),
             hex = ord.toString(16).toUpperCase()
         while(hex.length < 4){hex = "0" + hex}
         var re = new RegExp("^" + hex +";(.+?);(.*?);(.*?);(.*?);(.*?);(.*);(.*);(.*)$",
