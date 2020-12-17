acab25170422c3aef9ade9ab82fec3d5b677e831
PierreQuentel <quentel.pierre@orange.fr>
2020-12-17 11:43:52 +0100

    Update generated scripts after merge

--- www/src/brython.js
+++ www/src/brython.js
@@ -103,8 +103,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,9,0,'final',0]
 __BRYTHON__.__MAGIC__="3.9.0"
 __BRYTHON__.version_info=[3,9,0,'final',0]
-__BRYTHON__.compiled_date="2020-12-17 10:50:57.437143"
-__BRYTHON__.timestamp=1608198657437
+__BRYTHON__.compiled_date="2020-12-17 11:42:24.215277"
+__BRYTHON__.timestamp=1608201744215
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_cmath","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","math1","modulefinder","posix","python_re","random","unicodedata"]
 ;
 
@@ -13884,6 +13884,7 @@ if(key.toLowerCase()=="float"){self.style.cssFloat=value
 self.style.styleFloat=value}else{switch(key){case "top":
 case "left":
 case "width":
+case "height":
 case "borderWidth":
 if(_b_.isinstance(value,_b_.int)){value=value+"px"}}
 self.style[key]=value}}}
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 0, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.0"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2020-12-17 10:50:57.437143"
-__BRYTHON__.timestamp = 1608198657437
+__BRYTHON__.compiled_date = "2020-12-17 11:42:24.215277"
+__BRYTHON__.timestamp = 1608201744215
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
