dc5a768bc00a9fa2aca1ed28a383de20b5037aa4
PierreQuentel <quentel.pierre@orange.fr>
2021-01-19 16:04:13 +0100

    Add assertWarns to tester.py

--- www/tests/tester.py
+++ www/tests/tester.py
@@ -135,6 +135,56 @@ class Tester:
         if item:
             raise AssertionError(msg or '%s is not False' %item)
 
+    def assertWarns(self, exc):
+      import warnings
+      class Manager:
+    
+          def __init__(self, expected, expected_regex=None):
+              self.expected = expected
+              self.expected_regex = expected_regex
+              self.obj_name = None
+    
+          def __enter__(self):
+              self.warnings_manager = warnings.catch_warnings(record=True)
+              self.warnings = self.warnings_manager.__enter__()
+              warnings.simplefilter("always", self.expected)
+              return self
+    
+          def __exit__(self, exc_type, exc_value, tb):
+              self.warnings_manager.__exit__(exc_type, exc_value, tb)
+              if exc_type is not None:
+                  # let unexpected exceptions pass through
+                  return
+              try:
+                  exc_name = self.expected.__name__
+              except AttributeError:
+                  exc_name = str(self.expected)
+              first_matching = None
+              for m in self.warnings:
+                  w = m.message
+                  if not isinstance(w, self.expected):
+                      continue
+                  if first_matching is None:
+                      first_matching = w
+                  if (self.expected_regex is not None and
+                      not self.expected_regex.search(str(w))):
+                      continue
+                  # store warning for later retrieval
+                  self.warning = w
+                  self.filename = m.filename
+                  self.lineno = m.lineno
+                  return
+              # Now we simply try to choose a helpful failure message
+              if first_matching is not None:
+                  self._raiseFailure('"{}" does not match "{}"'.format(
+                           self.expected_regex.pattern, str(first_matching)))
+              if self.obj_name:
+                  self._raiseFailure("{} not triggered by {}".format(exc_name,
+                                                                     self.obj_name))
+              else:
+                  self._raiseFailure("{} not triggered".format(exc_name))
+    
+      return Manager(exc)
     def fail(self, *args):
         raise Exception(str(args))
 
