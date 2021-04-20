beaf8e6418ba90bc71e580e7b88dbffa1a272fec
PierreQuentel <pierre.quentel@gmail.com>
2021-04-20 09:36:53 +0200

    Replace the re module by the JS module libs/python_re.js

--- www/src/brython.js
+++ www/src/brython.js
@@ -116,8 +116,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,9,1,'final',0]
 __BRYTHON__.__MAGIC__="3.9.1"
 __BRYTHON__.version_info=[3,9,0,'final',0]
-__BRYTHON__.compiled_date="2021-04-17 18:46:30.423912"
-__BRYTHON__.timestamp=1618677990423
+__BRYTHON__.compiled_date="2021-04-20 09:33:41.764643"
+__BRYTHON__.timestamp=1618904021763
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sreXXX","_sre_utils","_string","_strptime","_svg","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","modulefinder","posix","python_re","python_re_backtrack_choice","python_re_v5","random","unicodedata"]
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 1, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.1"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-04-17 18:46:30.423912"
-__BRYTHON__.timestamp = 1618677990423
+__BRYTHON__.compiled_date = "2021-04-20 09:33:41.764643"
+__BRYTHON__.timestamp = 1618904021763
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
