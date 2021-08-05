4734529a30fde6fb0c4f0455d1012e45e37c33d8
PierreQuentel <pierre.quentel@gmail.com>
2021-08-05 11:29:17 +0200

    Update generated scripts after merge.

--- www/src/brython.js
+++ www/src/brython.js
@@ -110,8 +110,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,9,5,'final',0]
 __BRYTHON__.__MAGIC__="3.9.5"
 __BRYTHON__.version_info=[3,9,0,'final',0]
-__BRYTHON__.compiled_date="2021-08-05 11:01:18.239898"
-__BRYTHON__.timestamp=1628154078239
+__BRYTHON__.compiled_date="2021-08-05 11:28:44.784580"
+__BRYTHON__.timestamp=1628155724784
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre_utils","_string","_strptime","_svg","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","modulefinder","posix","python_re","random","unicodedata"]
 ;
 ;(function($B){function ord(char){if(char.length==1){return char.charCodeAt(0)}
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 5, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.5"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-08-05 11:01:18.239898"
-__BRYTHON__.timestamp = 1628154078239
+__BRYTHON__.compiled_date = "2021-08-05 11:28:44.784580"
+__BRYTHON__.timestamp = 1628155724784
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
