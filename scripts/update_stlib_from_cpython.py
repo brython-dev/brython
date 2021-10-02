a1ed328f015ed301818df1672bc7942f6aa51fc3
PierreQuentel <pierre.quentel@gmail.com>
2021-10-02 20:42:00 +0200

    Improve script that updates stdlib to latest CPython version

--- scripts/update_stlib_from_cpython.py
+++ scripts/update_stlib_from_cpython.py
@@ -6,8 +6,8 @@ import shutil
 bdir = os.path.join(os.path.dirname(os.getcwd()),
     "www", "src", "Lib")
 
-pdir = r'c:\python37\Lib'
-p38dir = r'c:\python38\Lib'
+p_old_dir = r'c:\Python39\Lib'
+p_new_dir = r'c:\Python310\Lib'
 
 for dirpath, dirnames, filenames in os.walk(bdir):
     if "site-packages" in dirnames:
@@ -17,21 +17,34 @@ for dirpath, dirnames, filenames in os.walk(bdir):
     for filename in filenames:
         if not filename.endswith(".py"):
             continue
-        ppath = pdir + "\\" + prefix + "\\" + filename
+        ppath = p_old_dir + "\\" + prefix + "\\" + filename
         if os.path.exists(ppath):
-            if filecmp.cmp(os.path.join(dirpath, filename),
-                    ppath, shallow=False):
-                print(filename, "same as CPython")
-                p38path = p38dir + "\\" + prefix + "\\" + filename
-                if os.path.exists(p38path):
-                    print("exists in Python 3.8")
-                    shutil.copyfile(p38path,
-                        os.path.join(dirpath, filename))
+            brython_path = os.path.join(dirpath, filename)
+            brython_short = brython_path[len(bdir) + 1:]
+            if filecmp.cmp(brython_path, ppath, shallow=False):
+                print(brython_short, "same as CPython 3.9")
+                p_new_path = p_new_dir + "\\" + prefix + "\\" + filename
+                if os.path.exists(p_new_path):
+                    if filecmp.cmp(brython_path, p_new_path, shallow=False):
+                        print("    not changed in Python 3.10")
+                    else:
+                        print("    replace by 3.10 version")
+                        shutil.copyfile(p_new_path, brython_path)
                 else:
-                    print("--- not in Python 3.8 !!!")
-                    input()
-
+                    print("    not in Python 3.10")
             else:
-                print(filename, "not the same")
+                p_new_path = p_new_dir + "\\" + prefix + "\\" + filename
+                if os.path.exists(p_new_path):
+                    if filecmp.cmp(brython_path, p_new_path, shallow=False):
+                        print(brython_short, "already changed to Python 3.10")
+                    else:
+                        print(brython_short, 'not the same as CPython 3.9')
+                else:
+                    print(brython_short, "not in Python 3.10")
         else:
-            print(ppath, "not in cpython")
+            p_new_path = p_new_dir + "\\" + prefix + "\\" + filename
+            if os.path.exists(p_new_path):
+                print(ppath, "not in CPython 3.9, but present in 3.10")
+            else:
+                print(ppath, "not in CPython 3.9 and 3.10")
+                
