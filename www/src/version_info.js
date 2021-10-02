e49aeee931b4a496c46673ee57e7ce37f4eb0d40
PierreQuentel <pierre.quentel@gmail.com>
2021-10-02 21:27:05 +0200

    Changes in traceback.py. Anticipation of Python 3.10.

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 6, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.6"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-10-02 20:54:43.266835"
-__BRYTHON__.timestamp = 1633200883266
+__BRYTHON__.compiled_date = "2021-10-02 21:26:17.166339"
+__BRYTHON__.timestamp = 1633202777166
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
