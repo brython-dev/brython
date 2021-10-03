35f7a3a8f0342495b7e2879b230a68c59afb1ebb
PierreQuentel <pierre.quentel@gmail.com>
2021-10-03 11:29:59 +0200

    Upgrade threading.py to CPython 3.9 version

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 6, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.6"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-10-03 11:25:47.151174"
-__BRYTHON__.timestamp = 1633253147151
+__BRYTHON__.compiled_date = "2021-10-03 11:28:29.445867"
+__BRYTHON__.timestamp = 1633253309445
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
