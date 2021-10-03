ffa9c51c52d3039817ecf8661b4a5388b8a9a895
PierreQuentel <pierre.quentel@gmail.com>
2021-10-03 09:48:10 +0200

    Upgrade enum.py to CPython 3.9 version

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 6, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.6"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-10-03 08:13:12.201482"
-__BRYTHON__.timestamp = 1633241592201
+__BRYTHON__.compiled_date = "2021-10-03 09:44:58.917293"
+__BRYTHON__.timestamp = 1633247098917
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
