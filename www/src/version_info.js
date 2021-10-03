9a0c15c588781c4bc0c81c120f43f4ad5e69c2c1
PierreQuentel <pierre.quentel@gmail.com>
2021-10-03 11:02:48 +0200

    Upgrade pathlib.py to CPython 3.9 version

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 9, 6, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.9.6"
 __BRYTHON__.version_info = [3, 9, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-10-03 10:56:25.333303"
-__BRYTHON__.timestamp = 1633251385333
+__BRYTHON__.compiled_date = "2021-10-03 10:59:28.227895"
+__BRYTHON__.timestamp = 1633251568227
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
