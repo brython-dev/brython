690a12f2569cd1ff83ef763b198b51b605084c12
PierreQuentel <quentel.pierre@orange.fr>
2021-10-31 08:21:50 +0100

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 10, 0, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.10.0"
 __BRYTHON__.version_info = [3, 10, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2021-10-30 21:48:25.095866"
-__BRYTHON__.timestamp = 1635623305095
+__BRYTHON__.compiled_date = "2021-10-31 08:20:40.898557"
+__BRYTHON__.timestamp = 1635664840898
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
