7ed61bd0991e041b410f7768e8e910bd9a200a1e
PierreQuentel <pierre.quentel@gmail.com>
2021-12-31 10:41:23 +0100

    In make_minified.py, restore current directory at the end of the script (made make_release.py fail)

--- scripts/make_minified.py
+++ scripts/make_minified.py
@@ -4,9 +4,11 @@
 
 import os
 
+this_dir = os.getcwd()
 # path of parent directory
-pdir = os.path.dirname(os.getcwd())
+pdir = os.path.dirname(this_dir)
 src_dir = os.path.join(pdir, 'www', 'src')
 os.chdir(src_dir)
 if os.system('terser brython.js -o brython.min.js'):
     raise SystemError('could not create brython.min.js')
+os.chdir(this_dir)
