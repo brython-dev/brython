4837a540ba560776e8b86de68dec28b9470c36b6
PierreQuentel <pierre.quentel@gmail.com>
2022-01-17 15:12:12 +0100

    Make bytearray unhashable (set bytearray.__hash__ to None)

--- www/src/py_bytes.js
+++ www/src/py_bytes.js
@@ -77,10 +77,13 @@ mutable_methods.forEach(function(method){
     })(method)
 })
 
+bytearray.__hash__ = _b_.None
+
 var bytearray_iterator = $B.make_iterator_class('bytearray_iterator')
 bytearray.__iter__ = function(self){
     return bytearray_iterator.$factory(self.source)
 }
+
 bytearray.__mro__ = [_b_.object]
 
 bytearray.__repr__ = bytearray.__str__ = function(self){
@@ -1275,7 +1278,7 @@ function _int(hex){return parseInt(hex, 16)}
 function normalise(encoding){
     var enc = encoding.toLowerCase()
     if(enc.substr(0, 7) == "windows"){enc = "cp" + enc.substr(7)}
-    if(enc.startsWith("cp") || enc.startsWith("iso")){
+    if(enc.startsWith("cp-") || enc.startsWith("iso-")){
         enc = enc.replace("-", "") // first hyphen, like in cp-1250
     }
     enc = enc.replace(/-/g, "_") // second, like in iso-8859-1
