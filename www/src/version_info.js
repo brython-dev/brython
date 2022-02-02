878a6541460bc36b1bbeaf61e2229ea1f94ec4d6
PierreQuentel <quentel.pierre@orange.fr>
2022-02-02 08:19:00 +0100

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,13 +1,14 @@
 __BRYTHON__.implementation = [3, 10, 4, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.10.4"
 __BRYTHON__.version_info = [3, 10, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2022-01-31 09:48:22.284072"
-__BRYTHON__.timestamp = 1643618902284
+__BRYTHON__.compiled_date = "2022-02-02 08:18:24.434802"
+__BRYTHON__.timestamp = 1643786304434
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
     "_base64",
     "_binascii",
+    "_cmath",
     "_io_classes",
     "_json",
     "_jsre",
@@ -15,6 +16,7 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "_multiprocessing",
     "_posixsubprocess",
     "_profile",
+    "_sre1",
     "_sre_utils",
     "_string",
     "_strptime",
@@ -33,8 +35,11 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "long_int",
     "marshal",
     "math",
+    "module1",
     "modulefinder",
     "posix",
     "python_re",
+    "python_re1",
+    "python_re2",
     "random",
     "unicodedata"]
