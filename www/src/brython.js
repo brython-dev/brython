8c7838c4322cf3808d6005d01fdb9f3f6d2061e6
PierreQuentel <quentel.pierre@orange.fr>
2022-02-21 11:56:01 +0100

    Add support of aio.Queue. Related to issue #1898.

--- www/src/brython.js
+++ www/src/brython.js
@@ -113,8 +113,8 @@ new Function("$locals_script",js)({})}})(__BRYTHON__)
 __BRYTHON__.implementation=[3,10,4,'final',0]
 __BRYTHON__.__MAGIC__="3.10.4"
 __BRYTHON__.version_info=[3,10,0,'final',0]
-__BRYTHON__.compiled_date="2022-02-21 11:24:27.943229"
-__BRYTHON__.timestamp=1645439067943
+__BRYTHON__.compiled_date="2022-02-21 11:55:03.290388"
+__BRYTHON__.timestamp=1645440903290
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_cmath","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_sre1","_sre_utils","_string","_strptime","_svg","_symtable","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","module1","modulefinder","posix","python_re","python_re1","python_re2","random","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins
