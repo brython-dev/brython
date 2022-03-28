93e35f49b607431d970091e6ca3f386f0f0d24d1
PierreQuentel <quentel.pierre@orange.fr>
2022-03-28 18:30:10 +0200

    Fixes #1936 : json.JSONEncoder is missing

--- www/tests/test_json.py
+++ www/tests/test_json.py
@@ -66,4 +66,16 @@ assert json.loads('"one \'two\' three"') == "one 'two' three"
 assert json.loads('"one \\"two\\" three"') == 'one "two" three'
 assert json.loads('"one\\\\\\ntwo\\nthree"') == 'one\\\ntwo\nthree'
 
+# issue 1936
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, complex):
+            return [obj.real, obj.imag]
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+assert json.dumps(2 + 1j, cls=ComplexEncoder) == '[2.0, 1.0]'
+assert ComplexEncoder().encode(2 + 1j) == '[2.0, 1.0]'
+assert list(ComplexEncoder().iterencode(2 + 1j)) == ['[2.0', ', 1.0', ']']
+
 print('all tests ok..')
