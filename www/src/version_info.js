69f79d0114845a37de8d29ef86ed33da4d7658f3
PierreQuentel <pierre.quentel@gmail.com>
2022-10-14 14:15:42 +0200

    Update generated scripts after merge

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 __BRYTHON__.implementation = [3, 10, 7, 'final', 0]
 __BRYTHON__.__MAGIC__ = "3.10.7"
 __BRYTHON__.version_info = [3, 10, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2022-10-13 22:13:49.300180"
-__BRYTHON__.timestamp = 1665692029300
+__BRYTHON__.compiled_date = "2022-10-14 14:15:14.886851"
+__BRYTHON__.timestamp = 1665749714886
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
