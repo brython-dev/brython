4094d28d2a836be5a5912d9ddc6b3725e72343f4
PierreQuentel <pierre.quentel@gmail.com>
2022-11-29 16:29:42 +0100

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2022-11-28 21:32:26.478773"
-__BRYTHON__.timestamp = 1669667546478
+__BRYTHON__.compiled_date = "2022-11-29 16:28:08.728342"
+__BRYTHON__.timestamp = 1669735688728
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
