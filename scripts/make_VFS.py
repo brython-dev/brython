fff8f5cd6095a616387e7eefa3fb79af6462dba6
PierreQuentel <quentel.pierre@orange.fr>
2022-12-06 00:04:47 +0100

    In make_VFS.py, handle case when brython_stdlib.js is absent

--- scripts/make_VFS.py
+++ scripts/make_VFS.py
@@ -79,6 +79,7 @@ def process(filename, exclude_dirs=['test','site-packages']):
     main_root = os.path.dirname(filename)
     VFS = {"$timestamp": timestamp}
     print("generated VFS timestamp", timestamp)
+    old_vfs = None
     if os.path.exists(filename):
         with open(filename, encoding='utf-8') as f:
             vfs = f.readlines()[1:-1]
@@ -151,7 +152,7 @@ def process(filename, exclude_dirs=['test','site-packages']):
                    mod_name = mod_name[:-9]
 
                 tail = file_name[len(main_root) + 1:]
-                if not check_changed(main_root, tail):
+                if old_vfs and not check_changed(main_root, tail):
                     # if last modif time did not change, use the previous
                     # version stored in brython_stdlib.js
                     nb_unchanged += 1
