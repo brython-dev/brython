1c39b6e1b585039809e174e44ac8d15388d3e435
PierreQuentel <pierre.quentel@gmail.com>
2022-12-11 10:33:33 +0100

    Move make_ast_classes, make_uniocde_tables, make_unicode_file and make_class_flags from make_dist to make_release

--- scripts/make_release.py
+++ scripts/make_release.py
@@ -14,6 +14,11 @@ if brython_version != cpython_version:
     print('Brython version is', brython_version,
         'but Cpython version is', cpython_version)
 
+import make_ast_classes       # generates /src/py_ast_classes.js
+import make_class_flags       # generate __flags__ for builtin classes
+import make_unicode_tables    # generates /src/unicode_data.js
+import make_unicode_file      # generates /src/unicode.txt
+
 # generate html files that compare Brython and CPython distributions
 import make_stdlib_list
 
@@ -96,7 +101,7 @@ content = re.sub('^implementation = "(.*)?"$',
 
 with open(br_script, 'w', encoding="utf-8") as out:
     out.write(content)
-    
+
 # copy files in folder /npm
 print("Udpate npm folder...")
 npmdir = os.path.join(pdir, 'npm')
