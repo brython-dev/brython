brython-dev/brython

commit 7fabbbd3955f72d87d2b3dd29454b593681f6048

In all scripts that use dicts, replace reference to internal attributes of dict objects (string_dict etc.) to calls to dict methods. Create fast methods for dicts that have only string keys.

www/src/libs/_ajax.js
@@ -66,11 +66,11 @@ function _read(req){
 }
 
 function stringify(d){
-    var params = d.string_dict,
-        items = []
-    for(var key in params){
+    var items = []
+    for(var key in _b_.dict.$fast_iter_keys(d)){
+        var value = _b_.dict.$getitem(d, key)
         items.push(encodeURIComponent(key) + "=" +
-                   encodeURIComponent(params[key][0]))
+                   encodeURIComponent(value))
     }
     return items.join("&")
 }
@@ -82,15 +82,18 @@ function handle_kwargs(self, kw, method){
         cache,
         mode = "text",
         timeout = {}
-    for(var key in kw.string_dict){
+    for(var key of _b_.dict.$keys_string(kw)){
         if(key == "data"){
-            var params = kw.string_dict[key][0]
+            var params = _b_.dict.$getitem_string(kw, key)
             if(typeof params == "string" || params instanceof FormData){
                 data = params
             }else if(params.__class__ === _b_.dict){
-                for(var key in params.int_dict){
-                    throw _b_.ValueError.$factory(
-                        'data only supports string keys, got ' + key)
+                for(var key of _b_.dict.$fast_iter_keys(params)){
+                    if(typeof key !== 'string'){
+                        throw _b_.ValueError.$factory(
+                            'data only supports string keys, got ' +
+                            `'${$B.class_name(key)}' object`)
+                    }
                 }
                 data = params
             }else{
@@ -98,30 +101,31 @@ function handle_kwargs(self, kw, method){
                     $B.class_name(params))
             }
         }else if(key == "encoding"){
-            encoding = kw.string_dict[key][0]
+            encoding = _b_.dict.$getitem_string(kw, key)
         }else if(key == "headers"){
-            var value = kw.string_dict[key][0]
+            var value = _b_.dict.$getitem_string(kw, key)
             if(! _b_.isinstance(value, _b_.dict)){
                 throw _b_.ValueError.$factory(
                     "headers must be a dict, not " + $B.class_name(value))
             }
-            for(key in value.string_dict){
-                headers[key.toLowerCase()] = [key, value.string_dict[key][0]]
+            for(key of _b_.dict.$keys_string(value)){
+                headers[key.toLowerCase()] = [key,
+                    _b_.dict.$getitem_string(value, key)]
             }
         }else if(key.startsWith("on")){
             var event = key.substr(2)
             if(event == "timeout"){
-                timeout.func = kw.string_dict[key][0]
+                timeout.func = _b_.dict.$getitem_string(kw, key)
             }else{
-                var f = kw.string_dict[key][0]
+                var f = _b_.dict.$getitem_string(kw, key)
                 ajax.bind(self, event, f)
             }
         }else if(key == "mode"){
-            var mode = kw.string_dict[key][0]
+            var mode = _b_.dict.$getitem_string(kw, key)
         }else if(key == "timeout"){
-            timeout.seconds = kw.string_dict[key][0]
+            timeout.seconds = _b_.dict.$getitem_string(kw, key)
         }else if(key == "cache"){
-            cache = kw.string_dict[key][0]
+            cache = _b_.dict.$getitem_string(kw, key)
         }
     }
     if(encoding && mode != "text"){
@@ -498,12 +502,12 @@ function file_upload(){
     }
     set_timeout(self, timeout)
 
-    if(kw.string_dict.method !== undefined){
-        method = kw.string_dict.method[0]
+    if(_b_.dict.$contains_string(kw, 'method')){
+        method = _b_.dict.$getitem_string(kw, 'method')
     }
 
-    if(kw.string_dict.field_name !== undefined){
-        field_name = kw.string_dict.field_name[0]
+    if(_b_.dict.$contains_string(kw, 'field_name')){
+        field_name = _b_.dict.$getitem_string(kw, 'field_name')
     }
 
     var formdata = new FormData()
@@ -528,9 +532,10 @@ function file_upload(){
     self.js.open(method, url, _b_.True)
     self.js.send(formdata)
 
-    for(key in kw.string_dict){
+    for(key of _b_.dict.$keys_string(kw)){
         if(key.startsWith("on")){
-            ajax.bind(self, key.substr(2), kw.string_dict[key][0])
+            ajax.bind(self, key.substr(2), 
+                _b_.dict.$getitem_string(kw, key))
         }
     }
 }

www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-01-11 09:42:42.626979"
-__BRYTHON__.timestamp = 1673426562626
+__BRYTHON__.compiled_date = "2023-01-11 22:21:30.032011"
+__BRYTHON__.timestamp = 1673472090032
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",

www/src/ast_to_js.js
@@ -268,7 +268,7 @@ function name_scope(name, scopes){
         console.log('symtable', scopes.symtable)
     }
     try{
-        flags = block.symbols.string_dict[name][0]
+        flags = _b_.dict.$getitem_string(block.symbols, name)
     }catch(err){
         console.log('name', name, 'not in symbols of block', block)
         return {found: false, resolve: 'all'}
@@ -318,8 +318,8 @@ function name_scope(name, scopes){
         // Search name in the surrounding scopes, using symtable
         for(var i = scopes.length - 2; i >= 0; i--){
             block = scopes.symtable.table.blocks.get(fast_id(scopes[i].ast))
-            if(block && block.symbols.string_dict[name]){
-                var fl = block.symbols.string_dict[name],
+            if(block && _b_.dict.$contains_string(block.symbols, name)){
+                var fl = _b_.dict.$getitem_string(block.symbols, name),
                     local_to_block =
                         [LOCAL, CELL].indexOf((fl >> SCOPE_OFF) & SCOPE_MASK) > -1
                 if(! local_to_block){
@@ -347,8 +347,8 @@ function name_scope(name, scopes){
         }
         if(scopes[i].locals.has(name) && scopes[i].type != 'class'){
             return {found: scopes[i]} // reference(scopes, scopes[i], name)
-        }else if(block && block.symbols.string_dict[name]){
-            flags = block.symbols.string_dict[name][0]
+        }else if(block && _b_.dict.$contains_string(block.symbols, name)){
+            flags = _b_.dict.$getitem_string(block.symbols, name)
             var __scope = (flags >> SCOPE_OFF) & SCOPE_MASK
             if([LOCAL, CELL].indexOf(__scope) > -1){
                 /* name is local to a surrounding scope but not yet bound
@@ -1841,11 +1841,11 @@ $B.ast.FunctionDef.prototype.to_js = function(scopes){
 
     var parameters = [],
         locals = [],
-        identifiers = Object.keys(symtable_block.symbols.string_dict)
+        identifiers = _b_.dict.$keys_string(symtable_block.symbols)
 
     var free_vars = []
     for(var ident of identifiers){
-        var flag = symtable_block.symbols.string_dict[ident][0],
+        var flag = _b_.dict.$getitem_string(symtable_block.symbols, ident),
             _scope = (flag >> SCOPE_OFF) & SCOPE_MASK
         if(_scope == FREE){
             free_vars.push(`'${ident}'`)

www/src/builtin_modules.js
@@ -21,9 +21,14 @@
                     ["elt", "evt", "options"], arguments,
                     {options: _b_.None}, null, null)
             var options = $.options
-            if(typeof options == "boolean"){}
-            else if(options.__class__ === _b_.dict){
-                options = options.$string_dict
+            if(typeof options == "boolean"){
+                // ignore
+            }else if(options.__class__ === _b_.dict){
+                var _options = {}
+                for(var key of _b_.dict.$keys_string(options)){
+                    _options[key] = _b_.dict.$getitem_string(options, key)
+                }
+                options = _options
             }else{
                 options == false
             }

www/src/js_objects.js
@@ -49,10 +49,11 @@ $B.pyobj2structuredclone = function(obj, strict){
         return res
     }else if(_b_.isinstance(obj, _b_.dict)){
         if(strict){
-            if(Object.keys(obj.int_dict).length > 0 ||
-                    Object.keys(obj.object_dict).length > 0){
-                throw _b_.TypeError.$factory("a dictionary with non-string " +
-                    "keys does not support structured clone")
+            for(var key of _b_.dict.$fast_iter_keys(obj)){
+                if(typeof key !== 'string'){
+                    throw _b_.TypeError.$factory("a dictionary with non-string " +
+                        "keys does not support structured clone")
+                }
             }
         }
         var items = $B.dict_to_list(obj),
@@ -663,12 +664,12 @@ $B.JSMeta.__new__ = function(metaclass, class_name, bases, cl_dict){
     // Creating a class that inherits a Javascript class A must return
     // another Javascript class B that extends A
     eval("var " + class_name + ` = function(){
-        if(cl_dict.string_dict.__init__){
+        if(_b_.dict.$contains_string(cl_dict, '__init__')){
             var args = [this]
             for(var i = 0, len = arguments.length; i < len; i++){
                 args.push(arguments[i])
             }
-            cl_dict.string_dict.__init__[0].apply(this, args)
+            _b_.dict.$getitem_string(cl_dict, '__init__').apply(this, args)
         }else{
             return new bases[0].$js_func(...arguments)
         }

www/src/libs/_aio.js
@@ -24,9 +24,9 @@ function handle_kwargs(kw, method){
         format = "text",
         headers = {},
         timeout = {}
-    for(var key in kw.string_dict){
+    for(var key of _b_.dict.$keys_string(kw)){
         if(key == "data"){
-            var params = kw.string_dict[key][0]
+            var params = _b_.dict.$getitem_string(kw, key)
             if(typeof params == "string"){
                 data = params
             }else if(_b_.isinstance(params, _b_.bytes)){
@@ -41,29 +41,29 @@ function handle_kwargs(kw, method){
                         "expected dict, bytes or str, got " +
                         $B.class_name(params))
                 }
-                params = params.string_dict
                 var items = []
-                for(var key in params){
+                for(var key of _b_.dict.$keys_string(params)){
+                    var value = _b_.dict.$getitem_string(params, key)
                     items.push(encodeURIComponent(key) + "=" +
-                               encodeURIComponent($B.pyobj2jsobj(params[key][0])))
+                               encodeURIComponent($B.pyobj2jsobj(value)))
                 }
                 data = items.join("&")
             }
         }else if(key == "headers"){
-            headers = _b_.dict.$to_obj(kw.string_dict[key][0])
+            headers = _b_.dict.$to_obj(_b_.dict.$getitem_string(kw, key))
         }else if(key.startsWith("on")){
             var event = key.substr(2)
             if(event == "timeout"){
-                timeout.func = kw.string_dict[key][0]
+                timeout.func = _b_.dict.$getitem_string(kw, key)
             }else{
-                ajax.bind(self, event, kw.string_dict[key][0])
+                ajax.bind(self, event, _b_.dict.getitem_string(kw, key))
             }
         }else if(key == "timeout"){
-            timeout.seconds = kw.string_dict[key][0]
+            timeout.seconds = _b_.dict.$getitem_string(kw, key)
         }else if(key == "cache"){
-            cache = kw.string_dict[key][0]
+            cache = _b_.dict.$getitem_string(kw, key)
         }else if(key == "format"){
-            format = kw.string_dict[key][0]
+            format = _b_.dict.$getitem_string(kw, key)
         }
     }
     if(method == "post"){

www/src/libs/_binascii.js
@@ -123,8 +123,8 @@ var module = {
         var $ = $B.args("b2a_base64", 1, {data: null}, ['data'],
                 arguments, {}, null, "kw")
         var newline = false
-        if($.kw && $.kw.string_dict.newline){
-            newline = $.kw.string_dict.newline[0]
+        if($.kw && _b_.dict.$contains_string($.kw, 'newline')){
+            newline = _b_.dict.$getitem_string($.kw, 'newline')
         }
 
         var string = $B.to_bytes($.data),

commit 581bbaa1677ac4b081e4e7dfdac944c0be71f8d9

Fix regression in ajax.post introduced by commit 7fabbbd3955f72d87d2b3dd29454b593681f6048

www/speed/make_report.html
@@ -48,13 +48,13 @@
 <script type="text/javascript" src="/src/ast_to_js.js"></script>
 <script type="text/javascript" src="/src/symtable.js"></script>
 
-<script type="text/javascript" src="../src/brython_stdlib.js"></script>
+<!-- script type="text/javascript" src="../src/brython_stdlib.js"></script -->
 
 <script type="text/python3" src="make_report.py">
 </script>
 
 </head>
-<body onload="brython({debug:1, cache:'none', static_stdlib_import:true})">
+<body onload="brython({debug:1, static_stdlib_import:true})">
 
 <select name="filename" id="files" autocomplete="off">
     <option value="benchmarks/assignment.py">simple assignment</option>

www/src/brython.js
@@ -155,8 +155,8 @@ $B.stdlib_module_names=Object.keys($B.stdlib)})(__BRYTHON__)
 ;
 __BRYTHON__.implementation=[3,11,0,'dev',0]
 __BRYTHON__.version_info=[3,11,0,'final',0]
-__BRYTHON__.compiled_date="2023-01-11 22:21:30.032011"
-__BRYTHON__.timestamp=1673472090032
+__BRYTHON__.compiled_date="2023-01-18 22:07:34.895906"
+__BRYTHON__.timestamp=1674076054895
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","modulefinder","posix","python_re","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins

www/src/libs/_ajax.js
@@ -67,7 +67,7 @@ function _read(req){
 
 function stringify(d){
     var items = []
-    for(var key in _b_.dict.$fast_iter_keys(d)){
+    for(var key of _b_.dict.$fast_iter_keys(d)){
         var value = _b_.dict.$getitem(d, key)
         items.push(encodeURIComponent(key) + "=" +
                    encodeURIComponent(value))

www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-01-11 22:21:30.032011"
-__BRYTHON__.timestamp = 1673472090032
+__BRYTHON__.compiled_date = "2023-01-18 22:07:34.895906"
+__BRYTHON__.timestamp = 1674076054895
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",