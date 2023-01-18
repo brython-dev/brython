2db5a8ca698d3650ffc3a0c57e0de61c4ff54cea
PierreQuentel <pierre.quentel@gmail.com>
2023-01-18 21:33:35 +0100

    Fix regression in ajax introduced by recent commits

--- www/src/brython.js
+++ www/src/brython.js
@@ -155,8 +155,8 @@ $B.stdlib_module_names=Object.keys($B.stdlib)})(__BRYTHON__)
 ;
 __BRYTHON__.implementation=[3,11,0,'dev',0]
 __BRYTHON__.version_info=[3,11,0,'final',0]
-__BRYTHON__.compiled_date="2023-01-18 19:22:09.979704"
-__BRYTHON__.timestamp=1674066129979
+__BRYTHON__.compiled_date="2023-01-18 21:33:13.250899"
+__BRYTHON__.timestamp=1674073993250
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_webcomponent","_webworker","_zlib_utils","array","bry_re","builtins","dis","encoding_cp932","hashlib","html_parser","long_int","marshal","math","modulefinder","posix","python_re","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins
