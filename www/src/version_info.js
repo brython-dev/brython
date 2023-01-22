70e8fbc4c4902f36304c66f246603106e55a7d3f
PierreQuentel <pierre.quentel@gmail.com>
2023-01-22 12:47:18 +0100

    Remove eval() from module _svg.js. Related to issue #2146.

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-01-22 12:41:18.633638"
-__BRYTHON__.timestamp = 1674387678632
+__BRYTHON__.compiled_date = "2023-01-22 12:46:46.505749"
+__BRYTHON__.timestamp = 1674388006505
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
