fef1ee3e600a0fa23698c18b81d506085ab90a5f
PierreQuentel <pierre.quentel@gmail.com>
2023-01-22 17:01:04 +0100

    Fix bug in error reporting for browser.timer

--- www/src/brython.js
+++ www/src/brython.js
@@ -155,8 +155,8 @@ $B.stdlib_module_names=Object.keys($B.stdlib)})(__BRYTHON__)
 ;
 __BRYTHON__.implementation=[3,11,0,'dev',0]
 __BRYTHON__.version_info=[3,11,0,'final',0]
-__BRYTHON__.compiled_date="2023-01-22 12:46:46.505749"
-__BRYTHON__.timestamp=1674388006505
+__BRYTHON__.compiled_date="2023-01-22 17:00:27.881473"
+__BRYTHON__.timestamp=1674403227881
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","python_re","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins
