91b3f0fecdc4fee720308dab959a270db1d9b70f
PierreQuentel <quentel.pierre@orange.fr>
2023-02-24 21:38:50 +0100

    Adapt svg to change in kwarg parsing

--- www/src/libs/_svg.js
+++ www/src/libs/_svg.js
@@ -32,9 +32,9 @@ function makeTagDict(tagName){
 
         // attributes
         var items = _b_.list.$factory(_b_.dict.items($ns['kw']))
-        for(var arg in $ns.kw.obj){
+        for(var arg in $ns.kw.$jsobj){
             // keyword arguments
-            var value = $B.py_immutable_to_js($ns.kw.obj[arg])
+            var value = $B.py_immutable_to_js($ns.kw.$jsobj[arg])
             if(arg.toLowerCase().substr(0,2) == "on"){
                 // Event binding passed as argument "onclick", "onfocus"...
                 // Better use method bind of DOMNode objects
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,12 +1,13 @@
 __BRYTHON__.implementation = [3, 11, 1, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-02-24 18:54:44.159182"
-__BRYTHON__.timestamp = 1677261284159
+__BRYTHON__.compiled_date = "2023-02-24 21:37:42.517033"
+__BRYTHON__.timestamp = 1677271062517
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
     "_base64",
     "_binascii",
+    "_cmath",
     "_io_classes",
     "_json",
     "_jsre",
@@ -16,6 +17,7 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "_profile",
     "_random",
     "_sre",
+    "_sre1",
     "_sre_utils",
     "_string",
     "_strptime",
@@ -32,7 +34,10 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "html_parser",
     "marshal",
     "math",
+    "module1",
     "modulefinder",
     "posix",
     "python_re",
+    "python_re1",
+    "python_re2",
     "unicodedata"]
