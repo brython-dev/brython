a94a7bcce7477abc7af4bf8492207d68c5d98f0d
PierreQuentel <pierre.quentel@gmail.com>
2023-03-03 19:09:06 +0100

    Update test_unicodedata.py

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 2, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-03-03 14:08:07.978493"
-__BRYTHON__.timestamp = 1677848887978
+__BRYTHON__.compiled_date = "2023-03-03 18:39:53.484212"
+__BRYTHON__.timestamp = 1677865193480
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
