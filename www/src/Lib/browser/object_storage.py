452457b783f890f7e3631aaa6fc24f58727d5902
PierreQuentel <pierre.quentel@gmail.com>
2023-05-29 14:46:01 +0200

    In browser.object_storage.py, replace javascript.JSON by Python json

--- www/src/Lib/browser/object_storage.py
+++ www/src/Lib/browser/object_storage.py
@@ -1,4 +1,4 @@
-from javascript import JSON
+import json
 
 class _UnProvided():
     pass
@@ -10,25 +10,25 @@ class ObjectStorage():
         self.storage = storage
 
     def __delitem__(self, key):
-        del self.storage[JSON.stringify(key)]
+        del self.storage[json.dumps(key)]
 
     def __getitem__(self, key):
-        return JSON.parse(self.storage[JSON.stringify(key)])
+        return json.loads(self.storage[json.dumps(key)])
 
     def __setitem__(self, key, value):
-        self.storage[JSON.stringify(key)] = JSON.stringify(value)
+        self.storage[json.dumps(key)] = json.dumps(value)
 
     def __contains__(self, key):
-        return JSON.stringify(key) in self.storage
+        return json.dumps(key) in self.storage
 
     def get(self, key, default=None):
-        if JSON.stringify(key) in self.storage:
-            return self.storage[JSON.stringify(key)]
+        if json.dumps(key) in self.storage:
+            return self.storage[json.dumps(key)]
         return default
 
     def pop(self, key, default=_UnProvided()):
-        if type(default) is _UnProvided or JSON.stringify(key) in self.storage:
-            return JSON.parse(self.storage.pop(JSON.stringify(key)))
+        if type(default) is _UnProvided or json.dumps(key) in self.storage:
+            return json.loads(self.storage.pop(json.dumps(key)))
         return default
 
     def __iter__(self):
@@ -36,10 +36,10 @@ class ObjectStorage():
         return keys.__iter__()
 
     def keys(self):
-        return [JSON.parse(key) for key in self.storage.keys()]
+        return [json.loads(key) for key in self.storage.keys()]
 
     def values(self):
-        return [JSON.parse(val) for val in self.storage.values()]
+        return [json.loads(val) for val in self.storage.values()]
 
     def items(self):
         return list(zip(self.keys(), self.values()))
