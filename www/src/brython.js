98c6dde585329e4bbb189e7741e21c3156023032
PierreQuentel <pierre.quentel@gmail.com>
2023-07-10 18:56:46 +0200

    Update generated files with Python 3.11

--- www/src/brython.js
+++ www/src/brython.js
@@ -158,8 +158,8 @@ $B.stdlib_module_names=Object.keys($B.stdlib)})(__BRYTHON__)
 ;
 __BRYTHON__.implementation=[3,11,3,'dev',0]
 __BRYTHON__.version_info=[3,11,0,'final',0]
-__BRYTHON__.compiled_date="2023-07-10 18:46:28.651139"
-__BRYTHON__.timestamp=1689007588651
+__BRYTHON__.compiled_date="2023-07-10 18:56:05.007040"
+__BRYTHON__.timestamp=1689008165007
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","python_re","python_re_new","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 11, 3, 'dev', 0]
 __BRYTHON__.version_info = [3, 11, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-07-10 18:46:28.651139"
-__BRYTHON__.timestamp = 1689007588651
+__BRYTHON__.compiled_date = "2023-07-10 18:56:05.007040"
+__BRYTHON__.timestamp = 1689008165007
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
