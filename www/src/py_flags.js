16d49c938ce4a2f9286f0df86af0123bbe13de0f
PierreQuentel <quentel.pierre@orange.fr>
2023-10-02 15:25:09 +0200

    Update generated scripts

--- www/src/py_flags.js
+++ www/src/py_flags.js
@@ -1,15 +1,15 @@
 (function($B){
 $B.builtin_class_flags = {
     builtins: {
-        1074287874: ['ConnectionAbortedError', 'BlockingIOError', 'IsADirectoryError', 'RuntimeError', 'TabError', 'WindowsError', 'BaseExceptionGroup', 'SyntaxError', 'ConnectionRefusedError', 'FutureWarning', 'UnicodeError', 'ReferenceError', 'FloatingPointError', 'RuntimeWarning', 'ImportError', 'InterruptedError', 'IndentationError', 'SystemExit', 'AttributeError', 'ProcessLookupError', 'RecursionError', 'KeyError', 'UnicodeTranslateError', 'StopIteration', 'UnboundLocalError', 'ZeroDivisionError', 'EncodingWarning', 'KeyboardInterrupt', 'LookupError', 'BrokenPipeError', 'NameError', 'ArithmeticError', 'UnicodeEncodeError', 'NotImplementedError', 'Warning', 'FileExistsError', 'ResourceWarning', 'ConnectionResetError', 'MemoryError', 'NotADirectoryError', 'EnvironmentError', 'StopAsyncIteration', 'TypeError', 'SystemError', 'ConnectionError', 'OSError', 'AssertionError', 'EOFError', 'ImportWarning', 'SyntaxWarning', 'BaseException', 'OverflowError', 'ChildProcessError', 'PermissionError', 'BufferError', 'Exception', 'FileNotFoundError', 'UnicodeDecodeError', 'DeprecationWarning', 'ValueError', 'UserWarning', 'TimeoutError', 'ModuleNotFoundError', 'PendingDeprecationWarning', 'IndexError', 'UnicodeWarning', 'IOError', 'BytesWarning', 'GeneratorExit'],
+        1074287874: ['StopAsyncIteration', 'TypeError', 'ConnectionAbortedError', 'UnicodeWarning', 'IndentationError', 'MemoryError', 'KeyError', 'Exception', 'StopIteration', 'NotADirectoryError', 'BrokenPipeError', 'UnicodeEncodeError', 'IsADirectoryError', 'DeprecationWarning', 'KeyboardInterrupt', 'RuntimeWarning', 'ConnectionRefusedError', 'UnicodeError', 'UserWarning', 'SystemExit', 'AssertionError', 'ArithmeticError', 'WindowsError', 'OSError', 'ZeroDivisionError', 'BufferError', 'SyntaxError', 'EncodingWarning', 'BaseExceptionGroup', 'ImportWarning', 'BytesWarning', 'EOFError', 'AttributeError', 'GeneratorExit', 'NameError', 'TabError', 'ModuleNotFoundError', 'UnboundLocalError', 'ReferenceError', 'UnicodeDecodeError', 'LookupError', 'InterruptedError', 'BaseException', 'NotImplementedError', 'Warning', 'ProcessLookupError', 'EnvironmentError', 'ConnectionError', 'RecursionError', 'PermissionError', 'SystemError', 'ImportError', 'RuntimeError', 'UnicodeTranslateError', 'IndexError', 'SyntaxWarning', 'FloatingPointError', 'PendingDeprecationWarning', 'OverflowError', 'TimeoutError', 'ResourceWarning', 'IOError', 'FutureWarning', 'ValueError', 'FileExistsError', 'FileNotFoundError', 'ConnectionResetError', 'ChildProcessError', 'BlockingIOError'],
         1073763848: ['ExceptionGroup'],
         21500162: ['bool'],
-        4723970: ['bytearray', 'float'],
+        4723970: ['float', 'bytearray'],
         138941698: ['bytes'],
-        546050: ['zip', 'classmethod', 'property', 'super', 'map', 'filter', 'enumerate', 'reversed', 'staticmethod'],
+        546050: ['classmethod', 'enumerate', 'reversed', 'zip', 'map', 'filter', 'property', 'staticmethod', 'super'],
         529666: ['complex', 'object'],
         541611330: ['dict'],
-        4740354: ['frozenset', 'set'],
+        4740354: ['set', 'frozenset'],
         21501186: ['int'],
         38294818: ['list'],
         545058: ['memoryview'],
@@ -20,10 +20,10 @@ $B.builtin_class_flags = {
         2156420354: ['type'],
     },
     types: {
-        545154: ['classmethod_descriptor', 'generator', 'async_generator', 'getset_descriptor', 'frame', 'coroutine', 'member_descriptor', 'method-wrapper'],
+        545154: ['member_descriptor', 'getset_descriptor', 'generator', 'coroutine', 'async_generator', 'classmethod_descriptor', 'frame', 'method-wrapper'],
         547202: ['builtin_function_or_method'],
-        545026: ['cell', 'traceback'],
-        528642: ['NotImplementedType', 'code', 'NoneType', 'ellipsis'],
+        545026: ['traceback', 'cell'],
+        528642: ['ellipsis', 'NoneType', 'NotImplementedType', 'code'],
         678146: ['function'],
         545090: ['mappingproxy'],
         678274: ['method_descriptor'],
