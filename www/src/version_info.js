859827b623375581494fa1ebe7dc89da4c8e69d6
PierreQuentel <quentel.pierre@orange.fr>
2023-10-22 08:32:42 +0200

    Update after merge

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,12 +1,13 @@
 __BRYTHON__.implementation = [3, 12, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 12, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-10-20 16:49:57.579466"
-__BRYTHON__.timestamp = 1697813397579
+__BRYTHON__.compiled_date = "2023-10-22 08:32:22.988621"
+__BRYTHON__.timestamp = 1697956342988
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
     "_base64",
     "_binascii",
+    "_cmath",
     "_io_classes",
     "_json",
     "_jsre",
@@ -16,6 +17,7 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "_profile",
     "_random",
     "_sre",
+    "_sre1",
     "_sre_utils",
     "_string",
     "_strptime",
@@ -33,8 +35,10 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "html_parser",
     "marshal",
     "math",
+    "module1",
     "modulefinder",
     "posix",
     "python_re",
-    "python_re_new",
+    "python_re1",
+    "python_re2",
     "unicodedata"]
