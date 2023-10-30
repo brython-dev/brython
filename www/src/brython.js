9d6405bc446355cd1251a1043fd22caf0bacd653
PierreQuentel <pierre.quentel@gmail.com>
2023-10-30 17:52:16 +0100

    Push version of libs/_webcomponent.js provided by Denis Migdal in PR #2295. Add tests.

--- www/src/brython.js
+++ www/src/brython.js
@@ -148,8 +148,8 @@ $B.stdlib_module_names=Object.keys($B.stdlib)})(__BRYTHON__)
 ;
 __BRYTHON__.implementation=[3,12,0,'dev',0]
 __BRYTHON__.version_info=[3,12,0,'final',0]
-__BRYTHON__.compiled_date="2023-10-30 17:11:51.863373"
-__BRYTHON__.timestamp=1698682311863
+__BRYTHON__.compiled_date="2023-10-30 17:51:11.458681"
+__BRYTHON__.timestamp=1698684671458
 __BRYTHON__.builtin_module_names=["_aio","_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","python_re","python_re_new","unicodedata"]
 ;
 ;(function($B){var _b_=$B.builtins
