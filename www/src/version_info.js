4733fdc1454dd1be8b76c6812cba1801c6da6819
denmigda <denis.migdal@uca.fr>
2023-10-30 15:05:03 +0100

    Try to fix PR issue

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 12, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 12, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-10-27 20:25:02.671733"
-__BRYTHON__.timestamp = 1698431102671
+__BRYTHON__.compiled_date = "2023-10-29 21:46:13.187760"
+__BRYTHON__.timestamp = 1698612373187
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_ast",
@@ -36,4 +36,5 @@ __BRYTHON__.builtin_module_names = ["_aio",
     "modulefinder",
     "posix",
     "python_re",
+    "python_re_new",
     "unicodedata"]
