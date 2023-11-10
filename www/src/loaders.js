brython-dev/brython

commit 96401c307fac3f266e028c36895a70a271f5112f

Add HTMLElement, MutationObserver and custumElements defaults

www/src/py2js.js
@@ -5642,7 +5642,7 @@ PatternCaptureCtx.prototype.transition = function(token, value){
     return transition(context.parent, token, value)
 }
 
-PatternClassCtx = function(context){
+const PatternClassCtx = function(context){
     this.type = "class_pattern"
     this.tree = []
     this.parent = context.parent

www/src/brython_builtins.js
@@ -18,21 +18,27 @@ $B.isNode = (typeof process !=='undefined') && (process.release.name==='node')
     && (process.__nwjs!==1)
 
 
-var _window
-if($B.isNode){
-    _window = {
-        location: {
+var _window = globalThis;
+
+_window.location ||= {
             href:'',
             origin: '',
             pathname: ''
-        },
-        navigator: {
-            userLanguage: ''
-        }
-    }
-} else {
-    _window = globalThis
+        };
+        
+console.log(_window.location);
+
+_window.navigator ||= {userLanguage: ''}
+
+_window.document  ||= {
+	getElementsByTagName: () => [{src: "http://localhost/"}],  // TODO: maybe needs some adaptations
+	currentScript: {src: "http://localhost/"}, // TODO: maybe needs some adaptations
+	querySelectorAll: () => []
 }
+
+_window.HTMLElement = class HTMLElement {};
+_window.MutationObserver ||= function() { this.observe = () => {};  };
+_window.customElements   ||= {define: () => {} };
 
 var href = _window.location.href
 $B.protocol = href.split(':')[0]
@@ -53,8 +59,8 @@ if($B.brython_path === undefined){
     }else{
         this_url = document.currentScript.src
     }
-
-    var elts = this_url.split('/')
+    
+    var elts = this_url.split('/');
     elts.pop()
     // brython_path is the url of the directory holding brython core scripts
     // It is used to import modules of the standard library

commit ddf2ba52f9594c6dd03c6157d2d40fe27bd520ef

Fix bug that broke support for Web Workers that was introduced in commit 96401c3

www/src/loaders.js
@@ -421,7 +421,7 @@ var inImported = $B.inImported = function(module){
 }
 
 function report_precompile(mod){
-    if(typeof document !== 'undefined'){
+    if(!$B.isWebWorker){
         document.dispatchEvent(new CustomEvent('precompile',
             {detail: 'remove outdated ' + mod +
              ' from cache'}))
@@ -429,14 +429,14 @@ function report_precompile(mod){
 }
 
 function report_close(){
-    if(typeof document !== 'undefined'){
+    if(!$B.isWebWorker){
         document.dispatchEvent(new CustomEvent('precompile',
             {detail: "close"}))
     }
 }
 
 function report_done(mod){
-    if(typeof document !== 'undefined'){
+    if(!$B.isWebWorker){
         document.dispatchEvent(new CustomEvent("brython_done",
             {detail: $B.obj_dict($B.$options)}))
     }

www/src/py2js.js
@@ -9006,7 +9006,7 @@ var brython_options = {}
 
 var python_scripts = []
 
-if(typeof document !== 'undefined'){
+if(!$B.isWebWorker){
     // If this script is not called in a web worker by importScripts,
     // store Python scripts already loaded in the page before loading this
     // script