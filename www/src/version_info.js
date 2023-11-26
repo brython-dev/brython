b8b2abd240fdc8ccc250b4d67439a0f51783a71c
PierreQuentel <pierre.quentel@gmail.com>
2023-11-26 21:39:19 +0100

    Update generated scripts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 12, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 12, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2023-11-26 10:27:05.982526"
-__BRYTHON__.timestamp = 1700990825982
+__BRYTHON__.compiled_date = "2023-11-26 21:35:59.058155"
+__BRYTHON__.timestamp = 1701030959058
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
