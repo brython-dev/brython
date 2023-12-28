5b2aac80bc13fb7bdb3d210ddd524cf67f3c292a
Austin Schick <aschick@andrew.cmu.edu>
2023-12-28 18:23:01 -0500

    Uncomment test_error_parenthesis now that the tests can be parsed correctly

--- www/src/Lib/test/test_syntax.py
+++ www/src/Lib/test/test_syntax.py
@@ -2075,15 +2075,15 @@ def func2():
         self._check_error("A.\u03bc\\\n",
                           "unexpected EOF while parsing")
 
-    # def test_error_parenthesis(self):
-    #     for paren in "([{":
-    #         self._check_error(paren + "1 + 2", f"\\{paren}' was never closed")
+    def test_error_parenthesis(self):
+        for paren in "([{":
+            self._check_error(paren + "1 + 2", f"\\{paren}' was never closed")
 
-    #     for paren in "([{":
-    #         self._check_error(f"a = {paren} 1, 2, 3\nb=3", f"\\{paren}' was never closed")
+        for paren in "([{":
+            self._check_error(f"a = {paren} 1, 2, 3\nb=3", f"\\{paren}' was never closed")
 
-    #     for paren in ")]}":
-    #         self._check_error(paren + "1 + 2", f"unmatched '\\{paren}'")
+        for paren in ")]}":
+            self._check_error(paren + "1 + 2", f"unmatched '\\{paren}'")
 
     def test_invisible_characters(self):
         self._check_error('print\x17("Hello")', "invalid non-printable character")
