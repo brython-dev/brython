b87dcd420986606adad09d6b7d1c682a0f587ccf
PierreQuentel <pierre.quentel@gmail.com>
2024-02-09 12:18:58 +0100

    Remove pydoc_data package (limited use in browser context, heavy)

--- www/src/brython.js
+++ www/src/brython.js
@@ -169,8 +169,8 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 ;
 __BRYTHON__.implementation=[3,12,1,'dev',0]
 __BRYTHON__.version_info=[3,12,0,'final',0]
-__BRYTHON__.compiled_date="2024-02-09 12:08:48.147919"
-__BRYTHON__.timestamp=1707476928145
+__BRYTHON__.compiled_date="2024-02-09 12:18:01.935415"
+__BRYTHON__.timestamp=1707477481934
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","python_re_new","unicodedata"]
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,7 +1,7 @@
 __BRYTHON__.implementation = [3, 12, 1, 'dev', 0]
 __BRYTHON__.version_info = [3, 12, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2024-02-09 12:08:48.147919"
-__BRYTHON__.timestamp = 1707476928145
+__BRYTHON__.compiled_date = "2024-02-09 12:18:01.935415"
+__BRYTHON__.timestamp = 1707477481934
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
