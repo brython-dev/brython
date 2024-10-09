241cf8a6217163e40277190a6b2e8480904c5926
PierreQuentel <pierre.quentel@gmail.com>
2024-10-09 18:52:45 +0200

    Fixes #2495 : calling the closest method of an HTML element results in a strange error.

--- www/src/brython.js
+++ www/src/brython.js
@@ -209,8 +209,8 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 ;
 __BRYTHON__.implementation=[3,12,6,'dev',0]
 __BRYTHON__.version_info=[3,12,0,'final',0]
-__BRYTHON__.compiled_date="2024-10-09 18:33:31.828897"
-__BRYTHON__.timestamp=1728491611828
+__BRYTHON__.compiled_date="2024-10-09 18:52:09.784617"
+__BRYTHON__.timestamp=1728492729784
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","_zlib_utils1","_zlib_utils_kozh","array","builtins","dis","encoding_cp932","encoding_cp932_v2","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","python_re_new","unicodedata","xml_helpers","xml_parser","xml_parser_backup"]
 ;
 
@@ -10299,7 +10299,7 @@ return attr=="x" ? pos.left :pos.top}
 break
 case "clear":
 case "closest":
-if(! self.hasOwnProperty(attr)){throw $B.attr_error(self,attr)}
+if(! self[attr]){throw $B.attr_error(self,attr)}
 return function(){return DOMNode[attr].call(null,self,...arguments)}
 case "headers":
 if(self.nodeType==9){
