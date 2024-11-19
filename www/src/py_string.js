8023636b733fc433f54ac1d96d2004d94f125fe2
PierreQuentel <pierre.quentel@gmail.com>
2024-11-19 08:02:56 +0100

    Move code of str hash computation inside str.__hash__

--- www/src/py_string.js
+++ www/src/py_string.js
@@ -287,9 +287,7 @@ str.__getnewargs__ = function(){
     return str.$getnewargs($B.single_arg('__getnewargs__', 'self', arguments))
 }
 
-$B.nb_cy = 0
 function cyrb53(str){
-    $B.nb_cy++
     let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
     for(let i = 0, ch; i < str.length; i++) {
         ch = str.charCodeAt(i);
@@ -308,7 +306,18 @@ str.__hash__ = function(_self){
     // copied from
     // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
     var s = to_string(_self)
-    return cyrb53(s)
+    let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
+    for(let i = 0, ch; i < s.length; i++) {
+        ch = s.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 }
 
 str.__init__ = function(){
