c94eb15ecfd20e7f6c27764009e46083b10b234e
Christopher Lee <cjlee112@gmail.com>
2025-03-13 09:38:23 +0000

    ran make_dist.py to update js dist

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 "use strict";
 __BRYTHON__.implementation = [3, 13, 2, 'dev', 0]
 __BRYTHON__.version_info = [3, 13, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2025-03-13 09:01:31.819794"
-__BRYTHON__.timestamp = 1741852891819
+__BRYTHON__.compiled_date = "2025-03-13 09:20:28.634093"
+__BRYTHON__.timestamp = 1741857628633
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
@@ -25,13 +25,10 @@ __BRYTHON__.builtin_module_names = ["_ajax",
     "_webcomponent",
     "_webworker",
     "_zlib_utils",
-    "_zlib_utils1",
-    "_zlib_utils_kozh",
     "array",
     "builtins",
     "dis",
     "encoding_cp932",
-    "encoding_cp932_v2",
     "hashlib",
     "html_parser",
     "marshal",
@@ -40,8 +37,6 @@ __BRYTHON__.builtin_module_names = ["_ajax",
     "posix",
     "pyexpat",
     "python_re",
-    "python_re_new",
     "unicodedata",
     "xml_helpers",
-    "xml_parser",
-    "xml_parser_backup"];
+    "xml_parser"];
