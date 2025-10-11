dc557439b5d20c5a44bd3925db216a0f07d0a0a5
PierreQuentel <pierre.quentel@gmail.com>
2025-10-11 08:54:54 +0200

    Fix bugs in scripts directory

--- scripts/make_minified.py
+++ scripts/make_minified.py
@@ -6,7 +6,6 @@ import os
 from directories import src_dir
 
 this_dir = os.getcwd()
-src_dir = os.path.join(pdir, 'www', 'src')
 os.chdir(src_dir)
 if os.system('terser brython.js -o brython.min.js'):
     raise SystemError('could not create brython.min.js')
--- scripts/make_pyproject_toml.py
+++ scripts/make_pyproject_toml.py
@@ -4,13 +4,13 @@ import re
 from directories import root_dir
 import version
 
-brython_version = '.'.join(str(x) for x in version.version[:2])
+brython_version = '.'.join(str(x) for x in version.version[:3])
 pyproject = os.path.join(root_dir, 'setup', 'pyproject.toml')
 
 with open(pyproject, encoding="utf-8") as f:
     content = f.read()
 
-updated = re.sub(r'version\s*=\s*"(.*?)"', f'version = "{vname}"', content)
+updated = re.sub(r'version\s*=\s*"(.*?)"', f'version = "{brython_version}"', content)
 
 with open(pyproject, 'w', encoding="utf-8") as out:
     out.write(updated)
