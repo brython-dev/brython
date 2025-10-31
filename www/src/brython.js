8441ae2a6f7dc1201739c693114ec5fd8a7279e7
PierreQuentel <pierre.quentel@gmail.com>
2025-10-31 10:54:14 +0100

    Update generated scripts after merging PR #2605

--- www/src/brython.js
+++ www/src/brython.js
@@ -164,6 +164,8 @@ for(var script in scripts){if($B.VFS.hasOwnProperty(script)){console.warn("Virtu
 $B.VFS[script]=scripts[script]
 $B.VFS[script].timestamp=vfs_timestamp}
 $B.stdlib_module_names=Object.keys($B.VFS)}
+$B.loadBrythonPackage=function(brythonPackage){$B.use_VFS=true
+$B.update_VFS(brythonPackage)}
 $B.add_files=function(files){
 $B.files=$B.files ||{}
 for(var file in files){$B.files[file]=files[file]}}
@@ -220,8 +222,8 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 ;
 __BRYTHON__.implementation=[3,14,0,'dev',0]
 __BRYTHON__.version_info=[3,14,0,'final',0]
-__BRYTHON__.compiled_date="2025-10-19 19:09:13.792250"
-__BRYTHON__.timestamp=1760893753791
+__BRYTHON__.compiled_date="2025-10-31 10:52:20.128203"
+__BRYTHON__.timestamp=1761904340127
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","_zlib_utils1","_zlib_utils_kozh","array","builtins","dis","encoding_cp932","encoding_cp932_v2","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","python_re_new","unicodedata","xml_helpers","xml_parser","xml_parser_backup"];
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 "use strict";
 __BRYTHON__.implementation = [3, 14, 0, 'dev', 0]
 __BRYTHON__.version_info = [3, 14, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2025-10-19 19:09:13.792250"
-__BRYTHON__.timestamp = 1760893753791
+__BRYTHON__.compiled_date = "2025-10-31 10:52:20.128203"
+__BRYTHON__.timestamp = 1761904340127
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
