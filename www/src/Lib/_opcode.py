720794968f36e22dbf902da92dd5658c2c3b4f0b
PierreQuentel <pierre.quentel@gmail.com>
2025-11-09 08:38:09 +0100

    Add fake _opcode module (required by test.support)

---
+++ www/src/Lib/_opcode.py
@@ -0,0 +1,50 @@
+
+ENABLE_SPECIALIZATION = 1
+
+ENABLE_SPECIALIZATION_FT = 1
+
+
+def get_executor(*args,**kw):
+    pass
+
+def get_intrinsic1_descs(*args,**kw):
+    pass
+
+def get_intrinsic2_descs(*args,**kw):
+    pass
+
+def get_nb_ops(*args,**kw):
+    pass
+
+def get_special_method_names(*args,**kw):
+    pass
+
+def get_specialization_stats(*args,**kw):
+    pass
+
+def has_arg(*args,**kw):
+    pass
+
+def has_const(*args,**kw):
+    pass
+
+def has_exc(*args,**kw):
+    pass
+
+def has_free(*args,**kw):
+    pass
+
+def has_jump(*args,**kw):
+    pass
+
+def has_local(*args,**kw):
+    pass
+
+def has_name(*args,**kw):
+    pass
+
+def is_valid(*args,**kw):
+    pass
+
+def stack_effect(*args,**kw):
+    pass
