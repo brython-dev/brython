c113ab9131b4f8193bda4741966a1da3f56b47bd
PierreQuentel <pierre.quentel@gmail.com>
2026-03-02 14:55:54 +0100

    Set version to 3.14.1

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 "use strict";
-__BRYTHON__.implementation = [3, 14, 0, 'dev', 0]
+__BRYTHON__.implementation = [3, 14, 1, 'dev', 0]
 __BRYTHON__.version_info = [3, 14, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2026-03-01 23:01:20.721198"
-__BRYTHON__.timestamp = 1772402480720
+__BRYTHON__.compiled_date = "2026-03-02 14:54:30.707852"
+__BRYTHON__.timestamp = 1772459670707
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
