59fef159b06230a761f7a0375968504512ab9c07
PierreQuentel <pierre.quentel@gmail.com>
2026-06-02 11:07:56 +0200

    Make _operator._compare_digest() a staticmethod. Related to PR #2699

--- www/src/brython.js
+++ www/src/brython.js
@@ -709,8 +709,8 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 "use strict";
 __BRYTHON__.implementation=[3,14,1,'dev',0]
 __BRYTHON__.version_info=[3,14,0,'final',0]
-__BRYTHON__.compiled_date="2026-06-02 11:02:59.871837"
-__BRYTHON__.timestamp=1780390979871
+__BRYTHON__.compiled_date="2026-06-02 11:07:02.917735"
+__BRYTHON__.timestamp=1780391222917
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_kozh","_sre_utils","_string","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","_zlib_utils1","_zlib_utils_kozh","array","builtins","dis","encoding_cp932","encoding_cp932_v2","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","python_re_new","unicodedata","xml_helpers","xml_parser","xml_parser_backup"];
 ;
 
--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,8 @@
 "use strict";
 __BRYTHON__.implementation = [3, 14, 1, 'dev', 0]
 __BRYTHON__.version_info = [3, 14, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2026-06-02 11:02:59.871837"
-__BRYTHON__.timestamp = 1780390979871
+__BRYTHON__.compiled_date = "2026-06-02 11:07:02.917735"
+__BRYTHON__.timestamp = 1780391222917
 __BRYTHON__.builtin_module_names = ["_ajax",
     "_ast",
     "_base64",
