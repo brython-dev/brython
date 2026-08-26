90b78e298ad935646c8962cca4071cd0ed03e812
PierreQuentel <pierre.quentel@gmail.com>
2026-08-26 15:04:39 +0200

    Update generated scripts after merge

--- www/src/brython.js
+++ www/src/brython.js
@@ -720,8 +720,8 @@ $B.unicode_titles={"\u01c5":"\u01c5","\u01c6":"\u01c5","\u01c4":"\u01c5","\u01c8
 "use strict";
 __BRYTHON__.implementation=[3,14,3,'dev',0]
 __BRYTHON__.version_info=[3,14,0,'final',0]
-__BRYTHON__.compiled_date="2026-08-26 15:01:48.105723"
-__BRYTHON__.timestamp=1787749308105
+__BRYTHON__.compiled_date="2026-08-26 15:03:19.859890"
+__BRYTHON__.timestamp=1787749399859
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","encoding_cp932_v2","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","unicodedata","xml_helpers","xml_parser"];
 ;
 
